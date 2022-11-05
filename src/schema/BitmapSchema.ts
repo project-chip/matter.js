41613a425e0b440cfaeefbf0bdcda1b0759df02b
Marco Fucci di Napoli <mfucci@gmail.com>
2022-11-05 13:34:11 -1000

    Update schema as abstract to enforce validation

--- src/schema/BitmapSchema.ts
+++ src/schema/BitmapSchema.ts
@@ -14,21 +14,24 @@ type BitSchema = {[key: string]: BitPosition};
 type TypeFromBitSchema<T extends BitSchema> = {[K in keyof T]: boolean};
 
 /** Declares a bitmap schema by indicating the bit position and their names. */
-export interface BitmapSchema<T extends BitSchema> extends Schema<TypeFromBitSchema<T>, number> {}
-export const BitmapSchema = <T extends BitSchema>(bitSchemas: T) => ({
-    encode: (value: TypeFromBitSchema<T>) => {
-        let result = 0;
-        for (const name in bitSchemas) {
-            if (value[name]) result |= 1 << bitSchemas[name].position;
-        }
-        return result;
-    },
+export const BitmapSchema = (bitSchemas: BitSchema) => new class<T extends BitSchema> extends Schema<TypeFromBitSchema<T>, number>{
+    constructor(readonly bitSchemas: T) {
+        super();
+    }
 
-    decode: (bitmap: number) => {
-        const result = <TypeFromBitSchema<T>>{};
-        for (const name in bitSchemas) {
-            result[name] = (bitmap & (1 << bitSchemas[name].position)) !== 0;
+    encodeInternal(value: TypeFromBitSchema<T>) {
+        let result = 0;
+        for (const name in this.bitSchemas) {
+            if (value[name]) result |= 1 << this.bitSchemas[name].position;
         }
         return result;
-    },
-});
+    }
+
+    decodeInternal(bitmap: number) {
+        const result = <TypeFromBitSchema<T>>{};
+        for (const name in this.bitSchemas) {
+            result[name] = (bitmap & (1 << this.bitSchemas[name].position)) !== 0;
+        }
+        return result;
+    }
+}(bitSchemas);
