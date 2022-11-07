4c2d20079440c353a147a424a4f328a613ec469a
mfucci <mfucci@gmail.com>
2022-11-07 10:50:44 -1000

    Add Schema and BitmapSchema (#2)
    * Add schema base and BitmapSchema
    * Update schema as abstract to enforce validation

---
+++ src/schema/Schema.ts
@@ -0,0 +1,28 @@
+/**
+ * @license
+ * Copyright 2022 Project CHIP Authors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+/** Define a schema to encode / decode convert type T to type E. */
+export abstract class Schema<T, E> {
+    /** Encodes the value using the schema. */
+    encode(value: T): E {
+        this.validate(value);
+        return this.encodeInternal(value);
+    }
+
+    /** Decodes the encoded data using the schema. */
+    decode(encoded: E): T {
+        const result = this.decodeInternal(encoded);
+        this.validate(result);
+        return result;
+    }
+
+    protected abstract encodeInternal(value: T): E;
+    protected abstract decodeInternal(encoded: E): T;
+
+    /** Optional validator that can be used to enforce constraints on the data before encoding / after decoding. */
+    validate(value: T): void {
+    }
+}
