947543e0d80b55959acbeb23d0150bde22659929
Marco Fucci di Napoli <mfucci@gmail.com>
2022-11-10 15:06:16 -1000

    Use individual const for float min / max

--- src/tlv/TlvFloat.ts
+++ src/tlv/TlvFloat.ts
@@ -9,7 +9,8 @@ import { DataWriterLE } from "../util/DataWriterLE";
 import { TlvType, TlvCodec, TlvTag } from "./TlvCodec";
 import { TlvSchema } from "./TlvSchema";
 
-const FLOAT_RANGE = { min: -340282346638528859811704183484516925440.0, max: 340282346638528859811704183484516925440.0 };
+const FLOAT32_MIN = -340282346638528859811704183484516925440.0;
+const FLOAT32_MAX = 340282346638528859811704183484516925440.0;
 
 /**
  * Schema to encode a float in TLV.
@@ -52,7 +53,7 @@ const FLOAT_RANGE = { min: -340282346638528859811704183484516925440.0, max: 3402
 export const TlvBoundedDouble = ({ min, max }: { min?: number, max?: number } = {}, type?: TlvType.Float32 | TlvType.Float64) => new FloatSchema(min, max, type);
 
 /** Float TLV schema. */
-export const TlvFloat = TlvBoundedDouble(FLOAT_RANGE, TlvType.Float32);
+export const TlvFloat = TlvBoundedDouble({ min: FLOAT32_MIN, max: FLOAT32_MAX}, TlvType.Float32);
 
 /** Double TLV schema. */
 export const TlvDouble = TlvBoundedDouble(); // number type is a double internally so no need to specify a range
