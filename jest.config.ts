a7226080c5dd6cddc0563d94312b3f9a1f3a225c
Martin Turon <mturon@google.com>
2022-11-30 08:17:05 -0800

    [build] Add coverage report to 'npm test'

--- jest.config.ts
+++ jest.config.ts
@@ -4,6 +4,7 @@ const config: Config = {
   testMatch: [ "<rootDir>/test/**/*.ts" ],
   preset: "ts-jest",
   testEnvironment: "node",
+  coverageDirectory: "out/coverage",
   moduleNameMapper: {
     '^(\\.{1,2}/.*)\\.[jt]s$': '$1',
   },
