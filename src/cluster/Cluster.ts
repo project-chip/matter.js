37852ca4bfdc865c3370c9e2eb79a3b768cf364b
Marco <mfucci@gmail.com>
2022-12-03 08:58:03 -1000

    Add clusters from node-matter

---
+++ src/cluster/Cluster.ts
@@ -0,0 +1,104 @@
+/**
+ * @license
+ * Copyright 2022 Project CHIP Authors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import { BitSchema, TypeFromBitSchema } from "../schema/BitmapSchema.js";
+import { TlvBitmap, TlvUInt16, TlvUInt32 } from "../tlv/TlvNumber.js";
+import { TlvFields, TlvObject, TypeFromFields } from "../tlv/TlvObject.js";
+import { TlvSchema } from "../tlv/TlvSchema.js";
+import { TlvVoid } from "../tlv/TlvVoid.js";
+import { Merge } from "../util/Type.js";
+import { MatterCoreSpecificationV1_0 } from "../spec/Specifications.js";
+
+export const enum AccessLevel {
+    View,
+    Manage,
+    Administer,
+}
+
+/* Interfaces and helper methods to define a cluster attribute */
+export interface Attribute<T> { id: number, schema: TlvSchema<T>, optional: boolean, readAcl: AccessLevel, writable: boolean, writeAcl?: AccessLevel, default?: T }
+export interface OptionalAttribute<T> extends Attribute<T> { optional: true }
+export interface WritableAttribute<T> extends Attribute<T> { writable: true }
+export interface OptionalWritableAttribute<T> extends OptionalAttribute<T> { writable: true }
+export type AttributeJsType<T extends Attribute<any>> = T extends Attribute<infer JsType> ? JsType : never;
+interface AttributeOptions<T> { default?: T, readAcl?: AccessLevel, writeAcl?: AccessLevel };
+export const Attribute = <T, V extends T>(id: number, schema: TlvSchema<T>, { default: conformanceValue, readAcl = AccessLevel.View }: AttributeOptions<V> = {}): Attribute<T> => ({ id, schema, optional: false, writable: false, default: conformanceValue, readAcl });
+export const OptionalAttribute = <T, V extends T>(id: number, schema: TlvSchema<T>, { default: conformanceValue, readAcl = AccessLevel.View }: AttributeOptions<V> = {}): OptionalAttribute<T> => ({ id, schema, optional: true, writable: false, default: conformanceValue, readAcl });
+export const WritableAttribute = <T, V extends T>(id: number, schema: TlvSchema<T>, { default: conformanceValue, readAcl = AccessLevel.View, writeAcl = AccessLevel.View }: AttributeOptions<V> = {}): WritableAttribute<T> => ({ id, schema, optional: false, writable: true, default: conformanceValue, readAcl, writeAcl });
+export const OptionalWritableAttribute = <T, V extends T>(id: number, schema: TlvSchema<T>, { default: conformanceValue, readAcl = AccessLevel.View, writeAcl = AccessLevel.View }: AttributeOptions<V> = {}): WritableAttribute<T> => ({ id, schema, optional: true, writable: true, default: conformanceValue, readAcl, writeAcl });
+
+/* Interfaces and helper methods to define a cluster command */
+export const TlvNoArguments = TlvObject({});
+export const TlvNoResponse = TlvVoid;
+export interface Command<RequestT, ResponseT> { optional: boolean, requestId: number, requestSchema: TlvSchema<RequestT>, responseId: number, responseSchema: TlvSchema<ResponseT> };
+export interface OptionalCommand<RequestT, ResponseT> extends Command<RequestT, ResponseT> { optional: true };
+export type ResponseType<T extends Command<any, any>> = T extends OptionalCommand<any, infer ResponseT> ? ResponseT | undefined : (T extends Command<any, infer ResponseT> ? ResponseT : never);
+export type RequestType<T extends Command<any, any>> = T extends Command<infer RequestT, any> ? RequestT : never;
+export const Command = <RequestT, ResponseT>(requestId: number, requestSchema: TlvSchema<RequestT>, responseId: number, responseSchema: TlvSchema<ResponseT>): Command<RequestT, ResponseT> => ({ optional: false, requestId, requestSchema, responseId, responseSchema });
+export const OptionalCommand = <RequestT, ResponseT>(requestId: number, requestSchema: TlvSchema<RequestT>, responseId: number, responseSchema: TlvSchema<ResponseT>): OptionalCommand<RequestT, ResponseT> => ({ optional: true, requestId, requestSchema, responseId, responseSchema });
+
+/* Interfaces and helper methods to define a cluset event */
+export const enum EventPriority {
+    Critical,
+    Info,
+}
+export interface Event<T> { id: number, schema: TlvSchema<T>, priority: EventPriority, optional: boolean }
+export interface OptionalEvent<T> extends Event<T> { optional: true }
+export const Event = <FT extends TlvFields>(id: number, priority: EventPriority, data: FT = <FT>{}): Event<TypeFromFields<FT>> => ({ id, schema: TlvObject(data), priority, optional: false });
+export const OptionalEvent = <FT extends TlvFields>(id: number, priority: EventPriority, data: FT = <FT>{}): Event<TypeFromFields<FT>> => ({ id, schema: TlvObject(data), priority, optional: true });
+
+/* Interfaces and helper methods to define a cluster */
+export interface Attributes { [key: string]: Attribute<any> }
+export interface Commands { [key: string]: Command<any, any> }
+export interface Events { [key: string]: Event<any> }
+
+/** @see {@link MatterCoreSpecificationV1_0} § 7.13 */
+export type GlobalAttributes<F extends BitSchema> = {
+    /** Indicates the revision of the server cluster specification supported by the cluster instance. */
+    clusterRevision: Attribute<number>,
+
+    /** Indicates whether the server supports zero or more optional clus­ter features. */
+    featureMap: Attribute<TypeFromBitSchema<F>>,
+}
+export const GlobalAttributes = <F extends BitSchema>(features: F) => ({
+    clusterRevision: Attribute(0xFFFD, TlvUInt16),
+    featureMap: Attribute(0xFFFC, TlvBitmap(TlvUInt32, features)),
+} as GlobalAttributes<F>);
+
+export interface Cluster<F extends BitSchema, A extends Attributes, C extends Commands, E extends Events> {
+    id: number,
+    name: string,
+    revision: number,
+    features: BitSchema,
+    attributes: A,
+    commands: C,
+    events: E,
+}
+export const Cluster = <F extends BitSchema, A extends Attributes, C extends Commands, E extends Events>({
+    id,
+    name,
+    revision,
+    features = <F>{},
+    attributes = <A>{},
+    commands = <C>{},
+    events = <E>{},
+}: {
+    id: number,
+    name: string,
+    revision: number,
+    features?: F,
+    attributes?: A,
+    commands?: C,
+    events?: E,
+} ):Cluster<F, Merge<A, GlobalAttributes<F>>, C, E> => ({
+    id,
+    name,
+    revision,
+    features,
+    commands,
+    attributes: Merge(attributes, GlobalAttributes(features)),
+    events,
+});
---
+++ src/protocol/InteractionProtocol.ts
@@ -0,0 +1,152 @@
+/**
+ * @license
+ * Copyright 2022 Project CHIP Authors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import { TlvNodeId } from "../common/NodeId.js";
+import { MatterCoreSpecificationV1_0 } from "../spec/Specifications.js";
+import { TlvAny } from "../tlv/TlvAny.js";
+import { TlvArray } from "../tlv/TlvArray.js";
+import { TlvBoolean } from "../tlv/TlvBoolean.js";
+import { TlvEnum, TlvUInt16, TlvUInt32, TlvUInt64, TlvUInt8 } from "../tlv/TlvNumber.js";
+import { TlvField, TlvList, TlvObject, TlvOptionalField } from "../tlv/TlvObject.js";
+
+/**
+ * @see {@link MatterCoreSpecificationV1_0}, section 8.10
+ */
+export const enum StatusCode {
+    Success = 0x00,
+    Failure = 0x01,
+    InvalidSubscription = 0x7d,
+    UnsupportedAccess = 0x7e, // old name: NOT_AUTHORIZED
+    UnsupportedEndpoint = 0x7f,
+    InvalidAction = 0x80,
+    UnsupportedCommand = 0x81,// old name: UNSUP_COMMAND
+    InvalidCommand = 0x85, // old name INVALID_FIELD
+    UnsupportedAttribute = 0x86,
+    ConstraintError = 0x87, // old name INVALID_VALUE
+    UnsupportedWrite = 0x88, // old name READ_ONLY
+    ResourceExhausted = 0x89, // old name INSUFFICIENT_SPACE
+    NotFound = 0x8b,
+    UnreportableAttribute = 0x8c,
+    InvalidDataType = 0x8d,
+    UnsupportedRead = 0x8f,
+    DataVersionMismatch = 0x92,
+    Timeout = 0x94,
+    UnsupportedMode = 0x9b,
+    Busy = 0x9c,
+    UnsupportedCluster = 0xc3,
+    NoUpstreamSubscription = 0xc5,
+    NeedsTimedInteraction = 0xc6,
+    UnsupportedEvent = 0xc7,
+    PathsExhausted = 0xc8,
+    TimedRequestMismatch = 0xc9,
+    FailsafeRequired = 0xca,
+}
+
+export const TlvStatusResponse = TlvObject({
+    status: TlvField(0, TlvEnum<StatusCode>()),
+    interactionModelRevision: TlvField(0xFF, TlvUInt8),
+});
+
+const TlvAttributePath = TlvList({
+    endpointId: TlvOptionalField(2, TlvUInt16),
+    clusterId: TlvOptionalField(3, TlvUInt32),
+    id: TlvOptionalField(4, TlvUInt32),
+});
+
+export const TlvReadRequest = TlvObject({
+    attributes: TlvField(0, TlvArray(TlvAttributePath)),
+    isFabricFiltered: TlvField(3,  TlvBoolean),
+    interactionModelRevision: TlvField(0xFF, TlvUInt8),
+});
+
+export const TlvDataReport = TlvObject({
+    subscriptionId: TlvOptionalField(0, TlvUInt32),
+    values: TlvField(1, TlvArray(TlvObject({
+        value: TlvField(1, TlvObject({
+            version: TlvField(0, TlvUInt32),
+            path: TlvField(1, TlvList({
+                endpointId: TlvField(2, TlvUInt16),
+                clusterId: TlvField(3, TlvUInt32),
+                id: TlvField(4, TlvUInt32),
+            })),
+            value: TlvField(2, TlvAny),
+        })),
+    }))),
+    isFabricFiltered: TlvOptionalField(4,  TlvBoolean),
+    interactionModelRevision: TlvField(0xFF, TlvUInt8),
+});
+
+export const TlvSubscribeRequest = TlvObject({
+    keepSubscriptions: TlvField(0,  TlvBoolean),
+    minIntervalFloorSeconds: TlvField(1, TlvUInt16),
+    maxIntervalCeilingSeconds: TlvField(2, TlvUInt16),
+    attributeRequests: TlvOptionalField(3, TlvArray(TlvAttributePath)),
+    eventRequests: TlvOptionalField(4, TlvArray(TlvList({
+        node: TlvField(0, TlvNodeId),
+        endpoint: TlvField(1, TlvUInt16),
+        cluster: TlvField(2, TlvUInt32),
+        event: TlvField(3, TlvUInt32),
+        isUrgent: TlvField(4,  TlvBoolean),
+    }))),
+    eventFilters: TlvOptionalField(5, TlvArray(TlvList({
+        node: TlvField(0, TlvNodeId),
+        eventMin: TlvField(1, TlvUInt64),
+    }))),
+    isFabricFiltered: TlvField(7,  TlvBoolean),
+    dataVersionFilters: TlvOptionalField(8, TlvArray(TlvObject({
+        path: TlvField(0, TlvList({
+            node: TlvField(0, TlvNodeId),
+            endpoint: TlvField(1, TlvUInt16),
+            cluster: TlvField(2, TlvUInt32),
+        })),
+        dataVersion: TlvField(1, TlvUInt32),
+    }))),
+});
+
+export const TlvSubscribeResponse = TlvObject({
+    subscriptionId: TlvField(0, TlvUInt32),
+    maxIntervalCeilingSeconds: TlvField(2, TlvUInt16),
+    interactionModelRevision: TlvField(0xFF, TlvUInt8),
+});
+
+export const TlvInvokeRequest = TlvObject({
+    suppressResponse: TlvField(0,  TlvBoolean),
+    timedRequest: TlvField(1,  TlvBoolean),
+    invokes: TlvField(2, TlvArray(TlvObject({
+        path: TlvField(0, TlvList({
+            endpointId: TlvField(0, TlvUInt16),
+            clusterId: TlvField(1, TlvUInt32),
+            id: TlvField(2, TlvUInt32),
+        })),
+        args: TlvField(1, TlvAny),
+    }))),
+    interactionModelRevision: TlvField(0xFF, TlvUInt8),
+});
+
+export const TlvInvokeResponse = TlvObject({
+    suppressResponse: TlvField(0,  TlvBoolean),
+    responses: TlvField(1, TlvArray(TlvObject({
+        response: TlvOptionalField(0, TlvObject({
+            path: TlvField(0, TlvList({
+                endpointId: TlvField(0, TlvUInt16),
+                clusterId: TlvField(1, TlvUInt32),
+                id: TlvField(2, TlvUInt32),
+            })),
+            response: TlvField(1, TlvAny),
+        })),
+        result: TlvOptionalField(1, TlvObject({
+            path: TlvField(0, TlvList({
+                endpointId: TlvField(0, TlvUInt16),
+                clusterId: TlvField(1, TlvUInt32),
+                id: TlvField(2, TlvUInt32),
+            })),
+            result: TlvField(1, TlvObject({
+                code: TlvField(0, TlvUInt16),
+            })),
+        })),
+    }))),
+    interactionModelRevision: TlvField(0xFF, TlvUInt8),
+});
