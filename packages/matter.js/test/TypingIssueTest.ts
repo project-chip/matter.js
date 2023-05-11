2ff3b5741d0316764019834b2311074a207d2776
Ingo Fischer <github@fischer-ka.de>
2023-05-11 17:18:00 +0200

    enhance issue example

--- packages/matter.js/test/TypingIssueTest.ts
+++ packages/matter.js/test/TypingIssueTest.ts
@@ -11,12 +11,15 @@ import { ClusterServerHandlers } from "../src/cluster/server/ClusterServer.js";
 import { ClusterServer } from "../src/protocol/interaction/InteractionServer.js";
 import { DeviceTypes } from "../src/device/DeviceTypes.js";
 import { Endpoint } from "../src/device/Endpoint.js";
+import { GroupsCluster } from "../src/cluster/GroupsCluster.js";
+import { GroupsClusterHandler } from "../src/cluster/server/GroupsServer.js";
 
 describe('Typing issue Test', () => {
 
     it("typing issue test", async () => {
         const identifyServer = createDefaultIdentifyClusterServer({
-            identify: async ({ request: { identifyTime } }) => { console.log(identifyTime); /* */ }
+            identify: async ({ request: { identifyTime } }) => { console.log(identifyTime); /* */ },
+            triggerEffect: async () => { console.log("triggerEffect"); /* */ },
         })
 
         /* Issue 1: */
@@ -28,9 +31,13 @@ describe('Typing issue Test', () => {
                 { identify: async ({ request: { identifyTime } }) => { console.log(identifyTime); } } as ClusterServerHandlers<typeof IdentifyCluster>
             );
 
+        const groupsServer = ClusterServer(GroupsCluster, { nameSupport: { groupNames: true } }, GroupsClusterHandler());
+
         /* Issue 2: */
         // adding the cluster in the array do not work - uncomment it to see it ... seems to be something that get weird because of the optional command which is not implemented
-        const endpoint = new Endpoint([DeviceTypes.ON_OFF_LIGHT], [/*identifyServer, identifyServer2*/], 1);
+        // a cluster without Optionalcommands works ...
+        // as soon as there is an option command (irrelevant if defined or not) it has issues
+        const endpoint = new Endpoint([DeviceTypes.ON_OFF_LIGHT], [groupsServer, /*identifyServer, identifyServer2*/], 1);
 
         // But it works that way ...
         // Difference: above it is an array with ClusterServerObj<any, any>
