9378d49f5c7660f946f3899662604a2d14b0caaa
Ingo Fischer <github@fischer-ka.de>
2023-05-11 18:16:21 +0200

    enhance testing for generated cluster global attributes

--- packages/matter.js/test/cluster/ClusterServerTest.ts
+++ packages/matter.js/test/cluster/ClusterServerTest.ts
@@ -11,11 +11,12 @@ import { AttributeId } from "../../src/datatype/AttributeId.js";
 import { CommandId } from "../../src/datatype/CommandId.js";
 import { FabricIndex } from "../../src/datatype/FabricIndex.js";
 import { ClusterServer } from "../../src/protocol/interaction/InteractionServer.js";
+import {IdentifyCluster, IdentifyType} from "../../src/cluster/index.js";
 
 
 describe("ClusterServer structure", () => {
     describe("check Server and global attributes", () => {
-        it("AdminCommissioningCluster including optional commands", () => {
+        it("AdminCommissioningCluster without optional commands", () => {
             const server = ClusterServer(
                 AdminCommissioningCluster,
                 {
@@ -36,24 +37,62 @@ describe("ClusterServer structure", () => {
             assert.deepEqual((server.attributes as any).generatedCommandList.get(), []);
         });
 
-        it("AdminCommissioningCluster including optional commands", () => {
+        it("IdentifyCluster including optional commands", () => {
             const server = ClusterServer(
-                AdminCommissioningCluster,
+                IdentifyCluster,
                 {
-                    windowStatus: 0,
-                    adminFabricIndex: new FabricIndex(1),
-                    adminVendorId: null
+                    identifyTime: 100,
+                    identifyType: IdentifyType.None
                 },
                 {
-                    openCommissioningWindow: async () => { /* dummy */ },
-                    revokeCommissioning: async () => { /* dummy */ },
+                    identify: async () => { /* dummy */ },
+                    triggerEffect: async () => { /* dummy */ },
                 }
             );
             assert.ok(server);
             // as any is trick because these attributes are not officially exposed by typings
-            assert.deepEqual((server.attributes as any).featureMap.get(), { basic: false });
-            assert.deepEqual((server.attributes as any).attributeList.get(), [new AttributeId(0), new AttributeId(1), new AttributeId(2), new AttributeId(65533), new AttributeId(65532), new AttributeId(65531), new AttributeId(65529), new AttributeId(65528)]);
-            assert.deepEqual((server.attributes as any).acceptedCommandList.get(), [new CommandId(0), new CommandId(2)]);
+            assert.deepEqual((server.attributes as any).featureMap.get(), { });
+            assert.deepEqual((server.attributes as any).attributeList.get(), [new AttributeId(0), new AttributeId(1), new AttributeId(65533), new AttributeId(65532), new AttributeId(65531), new AttributeId(65529), new AttributeId(65528)]);
+            assert.deepEqual((server.attributes as any).acceptedCommandList.get(), [new CommandId(0), new CommandId(0x40)]);
+            assert.deepEqual((server.attributes as any).generatedCommandList.get(), []);
+        });
+
+        it("IdentifyCluster including optional commands", () => {
+            const server = ClusterServer(
+                IdentifyCluster,
+                {
+                    identifyTime: 100,
+                    identifyType: IdentifyType.None
+                },
+                {
+                    identify: async () => { /* dummy */ },
+                    triggerEffect: async () => { /* dummy */ },
+                }
+            );
+            assert.ok(server);
+            // as any is trick because these attributes are not officially exposed by typings
+            assert.deepEqual((server.attributes as any).featureMap.get(), { });
+            assert.deepEqual((server.attributes as any).attributeList.get(), [new AttributeId(0), new AttributeId(1), new AttributeId(65533), new AttributeId(65532), new AttributeId(65531), new AttributeId(65529), new AttributeId(65528)]);
+            assert.deepEqual((server.attributes as any).acceptedCommandList.get(), [new CommandId(0), new CommandId(0x40)]);
+            assert.deepEqual((server.attributes as any).generatedCommandList.get(), []);
+        });
+
+        it("IdentifyCluster without optional commands", () => {
+            const server = ClusterServer(
+                IdentifyCluster,
+                {
+                    identifyTime: 100,
+                    identifyType: IdentifyType.None
+                },
+                {
+                    identify: async () => { /* dummy */ },
+                }
+            );
+            assert.ok(server);
+            // as any is trick because these attributes are not officially exposed by typings
+            assert.deepEqual((server.attributes as any).featureMap.get(), {});
+            assert.deepEqual((server.attributes as any).attributeList.get(), [new AttributeId(0), new AttributeId(1), new AttributeId(65533), new AttributeId(65532), new AttributeId(65531), new AttributeId(65529), new AttributeId(65528)]);
+            assert.deepEqual((server.attributes as any).acceptedCommandList.get(), [new CommandId(0)]);
             assert.deepEqual((server.attributes as any).generatedCommandList.get(), []);
         });
 
