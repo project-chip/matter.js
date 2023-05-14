9db76f7d00e6210fc7838f3331b83729321e2791
Ingo Fischer <github@fischer-ka.de>
2023-05-14 16:53:19 +0200

    fix tests

--- packages/matter-node.js/test/cluster/ClusterServerTestingUtil.ts
+++ packages/matter-node.js/test/cluster/ClusterServerTestingUtil.ts
@@ -6,7 +6,6 @@
 
 import { StatusCode } from "@project-chip/matter.js/interaction";
 import { SecureSession } from "@project-chip/matter.js/session";
-import { MatterDevice } from "@project-chip/matter.js";
 import { Message } from "@project-chip/matter.js/codec";
 import { Fabric } from "@project-chip/matter.js/fabric";
 import { FabricIndex, VendorId, FabricId, NodeId } from "@project-chip/matter.js/datatype";
@@ -15,14 +14,14 @@ import { Attributes, ClusterServerObj, Commands } from "@project-chip/matter.js/
 import { Endpoint } from "@project-chip/matter.js/device";
 
 // TODO make that nicer
-export async function callCommandOnClusterServer<A extends Attributes, C extends Commands>(clusterServer: ClusterServerObj<A, C>, commandName: string, args: any, endpoint: Endpoint, session?: SecureSession<MatterDevice>, message?: Message): Promise<{ code: StatusCode, responseId: number, response: any }> {
+export async function callCommandOnClusterServer<A extends Attributes, C extends Commands>(clusterServer: ClusterServerObj<A, C>, commandName: string, args: any, endpoint: Endpoint, session?: SecureSession<any>, message?: Message): Promise<{ code: StatusCode, responseId: number, response: any }> {
     const command = (clusterServer._commands as any)[commandName];
     if (command === undefined) throw new Error(`Command ${commandName} not found`);
-    const { code, responseId, response } = await command.invoke(session ?? {} as SecureSession<MatterDevice>, command.requestSchema.encodeTlv(args), message ?? {} as Message, endpoint);
+    const { code, responseId, response } = await command.invoke(session ?? {} as SecureSession<any>, command.requestSchema.encodeTlv(args), message ?? {} as Message, endpoint);
     return { code, responseId, response: command.responseSchema.decodeTlv(response) };
 }
 
 export async function createTestSessionWithFabric() {
     const testFabric = new Fabric(new FabricIndex(1), new FabricId(BigInt(1)), new NodeId(BigInt(1)), new NodeId(BigInt(2)), ByteArray.fromHex("00"), ByteArray.fromHex("00"), { privateKey: ByteArray.fromHex("00"), publicKey: ByteArray.fromHex("00") }, new VendorId(1), ByteArray.fromHex("00"), ByteArray.fromHex("00"), ByteArray.fromHex("00"), ByteArray.fromHex("00"), ByteArray.fromHex("00"), "");
-    return await SecureSession.create({} as MatterDevice, 1, testFabric, new NodeId(BigInt(1)), 1, ByteArray.fromHex("00"), ByteArray.fromHex("00"), false, false, () => { /* */ }, 1, 2);
+    return await SecureSession.create({} as any, 1, testFabric, new NodeId(BigInt(1)), 1, ByteArray.fromHex("00"), ByteArray.fromHex("00"), false, false, () => { /* */ }, 1, 2);
 }
