4fe8258ae077976f2ca3c3e449fc15934760a700
Ingo Fischer <github@fischer-ka.de>
2023-05-24 15:45:05 +0200

    Update ModeSelectCluster.ts and split up

--- packages/matter.js/src/cluster/ModeSelectCluster.ts
+++ packages/matter.js/src/cluster/ModeSelectCluster.ts
@@ -4,7 +4,7 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-import { Attribute, OptionalAttribute, Cluster, Command, TlvNoResponse } from "./Cluster.js";
+import { Attribute, OptionalAttribute, OptionalWritableAttribute, WritableAttrinute, Cluster, ClusterExtend, Command, TlvNoResponse } from "./Cluster.js";
 import { TlvInt16, TlvUInt16, TlvUInt8 } from "../tlv/TlvNumber.js";
 import { MatterApplicationClusterSpecificationV1_0 } from "../spec/Specifications.js";
 import { TlvNullable } from "../tlv/TlvNullable.js";
@@ -12,22 +12,23 @@ import { BitFlag } from "../schema/BitmapSchema.js";
 import { TlvString64max } from "../tlv/TlvString.js";
 import { TlvField, TlvObject } from "../tlv/TlvObject.js";
 import { TlvArray } from "../tlv/TlvArray.js";
+import { VendorId } from "../datatype/VendorId.js";
 
 /** @see {@link MatterApplicationClusterSpecificationV1_0} § 1.8. */
-export const SemanticTag = TlvObject({
-    value: TlvField(0x0, TlvUInt16), // enum16 in Standard, but values not defined "standard" or "Manufacturer sepcific"
-    mfgCode: TlvField(0x1, TlvNullable(TlvUInt16)), // type in standard is "vendor-id" ??
+export const TlvSemanticTag = TlvObject({
+    mfgCode: TlvField(0x0, TlvNullable(TlvVendorId)),
+    value: TlvField(0x1, TlvUInt16), // enum16 in Standard, but values not defined "standard" or "Manufacturer sepcific"
 });
 
 /** @see {@link MatterApplicationClusterSpecificationV1_0} § 1.8.8.1  */
-const ModeOption = TlvObject({
+const TlvModeOption = TlvObject({
     label: TlvField(0x0, TlvString64max),
     mode: TlvField(0x1, TlvUInt8),
-    semanticTags: TlvField(0x2, TlvArray(SemanticTag, { maxLength: 64 })),
+    semanticTags: TlvField(0x2, TlvArray(TlvSemanticTag, { maxLength: 64 })),
 });
 
 /** @see {@link MatterApplicationClusterSpecificationV1_0} § 1.8.6.1 */
-const ChangeToModedRequest = TlvObject({
+const TlvChangeToModeRequest = TlvObject({
     newMode: TlvField(0x0, TlvUInt8),
 });
 
@@ -43,39 +44,50 @@ export const ModeSelectCluster = Cluster({
     name: "ModeSelect",
     revision: 1,
     features: {
-        onOff: BitFlag(0), // Dependency with an On/Off cluster on same endpoint
+        /** Dependency with an On/Off cluster on same endpoint. */
+        onOff: BitFlag(0),
+    },
+    supportedFeatures: {
+        onOff: false
     },
 
     /** @see {@link MatterApplicationClusterSpecificationV1_0} § 1.8.5 */
     attributes: {
-        /** describes the purpose of the server, in readable text */
-        description: Attribute(0x0, TlvString64max),
+        /** Describes the purpose of the server, in readable text. */
+        description: Attribute(0x0, TlvString64max), // TODO Fixed
 
         /** 
           * Indicates a single standard namespace for any standard semantic tag value supported 
           * Each standard namespace and corresponding values and value meanings SHALL be defined in another document
           * http://www.dial-multiscreen.org/dial-registry/namespace-database
           */
-        standardNamespace: Attribute(0x1, TlvNullable(TlvInt16)),
+        standardNamespace: Attribute(0x1, TlvNullable(TlvInt16), { default: null }), // TODO Fixed
 
         /** Supported modes that may be selected for the CurrentMode attribute */
-        supportedModes: Attribute(0x2, TlvArray(ModeOption, { maxLength: 256 })),
+        supportedModes: Attribute(0x2, TlvArray(TlvModeOption, { maxLength: 255 })), // TODO Fixed
 
         /** Represents the current mode */
-        currentMode: OptionalAttribute(0x3, TlvUInt8),
+        currentMode: Attribute(0x3, TlvUInt8, { scene: true, persistent: true }),
 
         /** Indicates the desired startup mode */
-        startUpMode: OptionalAttribute(0x4, TlvNullable(TlvInt16)),
-
-        /**  
-         * Indicates the value of CurrentMode that depends on the state of the On/Off cluster on the same endpoint 
-         * See @see {@link MatterApplicationClusterSpecificationV1_0} § 1.8.6
-         */
-        onMode: OptionalAttribute(0x5, TlvNullable(TlvUInt8)), // TODO - Mandatory if OnOff flag set in features
+        startUpMode: OptionalWritableAttribute(0x4, TlvNullable(TlvInt8), { persistent: true }),
     },
     /** @see {@link MatterApplicationClusterSpecificationV1_0} § 1.8.5.6 */
     commands: {
         /** set the CurrentMode attribute to NewMode  */
-        changeToMode: Command(0x0, ChangeToModedRequest, 0x0, TlvNoResponse),
+        changeToMode: Command(0x0, TlvChangeToModeRequest, 0x0, TlvNoResponse),
+    }
+});
+
+export const ModeSelectClusterWithOnOffDependency = ClusterExtend(ModeSelectCluster, {
+    supportedFeatures: {
+        onOff: true
+    },
+    attributes: {
+        /**  
+         * Indicates the value of CurrentMode that depends on the state of the On/Off cluster on the same endpoint 
+         * See @see {@link MatterApplicationClusterSpecificationV1_0} § 1.8.6
+         */
+        onMode: WritableAttribute(0x5, TlvNullable(TlvUInt8), { persistent: true, default: null }),
     }
 });
