8c5172ca1f9ec8403c0c4fe9ed16fcf3b0fd6841
Mahima Mandhana <mahimaxm@amazon.com>
2023-06-19 10:30:49 -0700

    decode msg for handshake response

--- packages/matter.js/src/codec/BtpCodec.ts
+++ packages/matter.js/src/codec/BtpCodec.ts
@@ -6,6 +6,7 @@
 
 import { ByteArray, Endian } from "../util/ByteArray.js";
 import { DataReader } from "../util/DataReader.js";
+import { DataWriter } from "../util/DataWriter.js";
 
 export interface HandshakeRequestPayload {
     isManagementOpcode: boolean,
@@ -30,14 +31,25 @@ export interface HandshakeRequest {
     payload: HandshakeRequestPayload
 }
 
+export interface HandshakeResponsePayload {
+    isManagementOpcode: boolean,
+    isReserved: boolean,
+    isFinalVersion: boolean,
+    attMtu: number,
+    windowSize: number,
+}
+
+export interface HandshakeResponse {
+    header: HandshakeHeader,
+    payload: HandshakeResponsePayload
+}
+
 export const enum HeaderBits {
-    UNUSED_BIT1 = 0b00000001, //unused bit 1
+    UNUSED_BIT = 0b00000000, //unused bit
     HANDSHAKE_BIT = 0b00000010,
     MANAGEMENT_MESSAGE = 0b00000100,
-    UNUSED_BIT2 = 0b00001000, //unused bit 2
-    ACKNOWLEDGEMENT_MSG = 0b00010000,
+    ACKNOWLEDGEMENT_MSG = 0b00000000,
     ENDING_SEGMENT = 0b00100000,
-    UNUSED_BIT3 = 0b01000000, //unused bit 3
     BEGINNING_SEGMENT = 0b10000000
 }
 
@@ -67,6 +79,13 @@ export class BtpCodec {
 
     }
 
+    static encodeHandshakeResponse({ header, payload }: HandshakeResponse): ByteArray {
+        return ByteArray.concat(
+            this.encodeHeader(header),
+            this.decodeResponsePayload(payload)
+        );
+    }
+
     private static decodeRequestPayload(reader: DataReader<Endian.Little>): HandshakeRequestPayload {
 
         const opcode = reader.readUInt8();
@@ -81,15 +100,39 @@ export class BtpCodec {
     private static decodeHeader(reader: DataReader<Endian.Little>): HandshakeHeader {
 
         const headerBits = reader.readUInt8();
-        const isUnused1 = (headerBits & HeaderBits.UNUSED_BIT1) == 0;
+        const isUnused1 = (headerBits & HeaderBits.UNUSED_BIT) == 0;
         const isHandshake = (headerBits & HeaderBits.HANDSHAKE_BIT) !== 0;
         const isManagement = (headerBits & HeaderBits.MANAGEMENT_MESSAGE) !== 0;
-        const isUnused2 = (headerBits & HeaderBits.UNUSED_BIT2) == 0;
+        const isUnused2 = (headerBits & HeaderBits.UNUSED_BIT) == 0;
         const isAckMsg = (headerBits & HeaderBits.ACKNOWLEDGEMENT_MSG) == 0;
         const isEndingSegment = (headerBits & HeaderBits.ENDING_SEGMENT) !== 0;
-        const isUnused3 = (headerBits & HeaderBits.UNUSED_BIT3) == 0;
+        const isUnused3 = (headerBits & HeaderBits.UNUSED_BIT) == 0;
         const isBeginingSegment = (headerBits & HeaderBits.BEGINNING_SEGMENT) !== 0;
 
         return { isUnused1, isHandshake, isManagement, isUnused2, isAckMsg, isEndingSegment, isUnused3, isBeginingSegment };
+    }
+
+    private static decodeResponsePayload({ isManagementOpcode, isReserved, isFinalVersion, attMtu, windowSize }: HandshakeResponsePayload): ByteArray {
+
+        const writer = new DataWriter(Endian.Little);
+
+        const flags = (isManagementOpcode ? Opcode.MANAGEMENT_OPCODE : 0) | (isReserved ? Reserved.RESERVE_BIT : 0) | (isFinalVersion ? ProtocolVersion.FINAL_PROTOCOL_VERSION : 0) ;
+
+        writer.writeUInt8(flags);
+        writer.writeUInt16(attMtu);
+        writer.writeInt8(windowSize);
+        return writer.toByteArray();
+    }
+
+    private static encodeHeader({ isUnused1, isHandshake, isManagement, isUnused2, isAckMsg, isEndingSegment, isUnused3, isBeginingSegment }: HandshakeHeader): ByteArray {
+
+        const writer = new DataWriter(Endian.Little);
+
+        const header = ( isUnused1 ? HeaderBits.UNUSED_BIT : 0) | (isHandshake ? HeaderBits.HANDSHAKE_BIT : 0) | (isManagement ? HeaderBits.MANAGEMENT_MESSAGE : 0)
+        | (isUnused2 ? HeaderBits.UNUSED_BIT : 0) | (isAckMsg ? HeaderBits.ACKNOWLEDGEMENT_MSG : 0) | (isEndingSegment ? HeaderBits.ENDING_SEGMENT : 0) 
+        | (isUnused3 ? HeaderBits.UNUSED_BIT : 0) | (isBeginingSegment ? HeaderBits.BEGINNING_SEGMENT : 0);
+
+        writer.writeUInt8(header);
+        return writer.toByteArray();
     }
 }
