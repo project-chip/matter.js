4d38f9b7662ec746d6b2f1310ad317e21b9fde97
Mahima Mandhana <mahimaxm@amazon.com>
2023-06-19 18:22:51 -0700

    resloved PR comments

--- packages/matter.js/src/codec/BtpCodec.ts
+++ packages/matter.js/src/codec/BtpCodec.ts
@@ -9,43 +9,35 @@ import { DataReader } from "../util/DataReader.js";
 import { DataWriter } from "../util/DataWriter.js";
 
 export interface HandshakeRequestPayload {
-    isManagementOpcode: boolean,
-    version: number,
+    versions: ByteArray,
     attMtu: number,
     clientWindowSize: number,
 }
 
 export interface Header {
-    isUnused1: boolean,
-    isHandshake: boolean,
-    isManagement: boolean,
-    isUnused2: boolean,
-    isAckMsg: boolean,
-    isEndingSegment: boolean,
-    isUnused3: boolean,
-    isBeginingSegment: boolean
+    handshakeBit: number,
+    managementBit: number,
+    ackMsgBit: number,
+    endSegmentBit: number,
+    beginSegmentBit: number
 }
 
 export interface HandshakeRequest {
-    header: Header,
     payload: HandshakeRequestPayload
 }
 
 export interface HandshakeResponsePayload {
-    isManagementOpcode: boolean,
-    isReserved: boolean,
-    isFinalVersion: boolean,
+    finalVersion: number,
     attMtu: number,
     windowSize: number
 }
 
 export interface HandshakeResponse {
-    header: Header,
+    header: ByteArray,
     payload: HandshakeResponsePayload
 }
 
 export interface PacketPayload {
-    isManagementOpcode: boolean,
     ackNumber?: number,
     sequenceNumber: number,
     msgLength?: number,
@@ -58,27 +50,18 @@ export interface Packet {
 }
 
 export const enum HeaderBits {
-    UNUSED_BIT = 0b00000000, //unused bit
-    HANDSHAKE_BIT = 0b00000010,
-    MANAGEMENT_MESSAGE = 0b00000100,
-    ACKNOWLEDGEMENT_MSG = 0b00000000,
-    ENDING_SEGMENT = 0b00100000,
-    BEGINNING_SEGMENT = 0b10000000
+    HandshakeBit = 0b00000010,
+    ManagementMsg = 0b00000100,
+    AckMsg = 0b00010000,
+    EndSegment = 0b00100000,
+    BeginSegment = 0b10000000
 }
-
-const HEADER = 0b01100101;
 
 export const enum Opcode {
-    MANAGEMENT_OPCODE = 0b01101100, // 0x6c - binary representation
+    HandshakeManagementOpcode = 0x6c,
 }
 
-export const enum Reserved {
-    RESERVE_BIT = 0b00000000,
-}
-
-export const enum ProtocolVersion {
-    FINAL_PROTOCOL_VERSION = 0b00000100,
-}
+const HANDSHAKE_HEADER = 0b01100101;
 
 export class BtpCodec {
 
@@ -86,7 +69,6 @@ export class BtpCodec {
         const reader = new DataReader(data, Endian.Little);
 
         return {
-            header: this.decodeHeader(reader),
             payload: this.decodeRequestPayload(reader)
         };
 
@@ -97,85 +79,69 @@ export class BtpCodec {
 
         return {
             header: this.decodeHeader(reader),
-            payload: this.decodeRacketPayload(reader)
+            payload: this.decodePacketPayload(reader)
         };
 
     }
 
-    static encodeHandshakeResponse({ header, payload }: HandshakeResponse): ByteArray {
-        
+    static encodeHandshakeResponse({ payload }: HandshakeResponse): ByteArray {
+
         return ByteArray.concat(
-            this.encodeHeader(header),
+            new Uint8Array(HANDSHAKE_HEADER),
             this.encodeResponsePayload(payload)
         );
     }
 
-    private static decodeRacketPayload(reader: DataReader<Endian.Little>): PacketPayload {
-
-        const opcode = reader.readUInt8();
-        const isManagementOpcode = (opcode & Opcode.MANAGEMENT_OPCODE) !== 0;
-
-        if (opcode !== Opcode.MANAGEMENT_OPCODE) throw new Error("Handshake Error - Management Opcode is incorrect");
+    private static decodePacketPayload(reader: DataReader<Endian.Little>): PacketPayload {
 
         const ackNumber = reader.readUInt8();
-        const sequenceNumber = reader.readInt8();
-        const msgLength = reader.readInt16();
+        const sequenceNumber = reader.readUInt8();
+        const msgLength = reader.readUInt16();
         const segmentPayload = reader.getRemainingBytes();
 
-        return { isManagementOpcode, ackNumber, sequenceNumber, msgLength, segmentPayload };
+        return { ackNumber, sequenceNumber, msgLength, segmentPayload };
     }
 
 
     private static decodeRequestPayload(reader: DataReader<Endian.Little>): HandshakeRequestPayload {
 
+        const header = reader.readUInt8();
+        if (header !== HANDSHAKE_HEADER) throw new Error("Error in BTP Handhshake Request Headers"); 
+
         const opcode = reader.readUInt8();
-        const isManagementOpcode = (opcode & Opcode.MANAGEMENT_OPCODE) !== 0;
+        const isManagementOpcode = (opcode & Opcode.HandshakeManagementOpcode) !== 0;
 
-        if (opcode !== Opcode.MANAGEMENT_OPCODE) throw new Error("Handshake Error - Management Opcode is incorrect");
-
+        if (isManagementOpcode) throw new Error("Handshake Error - Management Opcode is incorrect");
+        
         const version = reader.readUInt32();
-        const attMtu = reader.readInt16();
-        const clientWindowSize = reader.readInt8();
+        if(version == 0) throw new Error("BTP Handshake Error - Version is 0");
 
-        return { isManagementOpcode, version, attMtu, clientWindowSize };
+        const versions = reader.readByteArray(version);
+        const attMtu = reader.readUInt16();
+        const clientWindowSize = reader.readUInt8();
+
+        return { versions, attMtu, clientWindowSize };
     }
 
     private static decodeHeader(reader: DataReader<Endian.Little>): Header {
 
         const headerBits = reader.readUInt8();
-        const isUnused1 = (headerBits & HeaderBits.UNUSED_BIT) == 0;
-        const isHandshake = (headerBits & HeaderBits.HANDSHAKE_BIT) !== 0;
-        const isManagement = (headerBits & HeaderBits.MANAGEMENT_MESSAGE) !== 0;
-        const isUnused2 = (headerBits & HeaderBits.UNUSED_BIT) == 0;
-        const isAckMsg = (headerBits & HeaderBits.ACKNOWLEDGEMENT_MSG) == 0;
-        const isEndingSegment = (headerBits & HeaderBits.ENDING_SEGMENT) !== 0;
-        const isUnused3 = (headerBits & HeaderBits.UNUSED_BIT) == 0;
-        const isBeginingSegment = (headerBits & HeaderBits.BEGINNING_SEGMENT) !== 0;
+        const handshakeBit = (headerBits & HeaderBits.HandshakeBit) ;
+        const managementBit = (headerBits & HeaderBits.ManagementMsg);
+        const ackMsgBit = (headerBits & HeaderBits.AckMsg);
+        const endSegmentBit = (headerBits & HeaderBits.EndSegment);
+        const beginSegmentBit = (headerBits & HeaderBits.BeginSegment);
 
-        if (headerBits !== HEADER) throw new Error("Error in BTP Handhshake Request Headers");
-
-        return { isUnused1, isHandshake, isManagement, isUnused2, isAckMsg, isEndingSegment, isUnused3, isBeginingSegment };
+        return { handshakeBit, managementBit, ackMsgBit, endSegmentBit, beginSegmentBit };
     }
 
-    private static encodeResponsePayload({ isManagementOpcode, isReserved, isFinalVersion, attMtu, windowSize }: HandshakeResponsePayload): ByteArray {
+    private static encodeResponsePayload({ finalVersion, attMtu, windowSize }: HandshakeResponsePayload): ByteArray {
 
         const writer = new DataWriter(Endian.Little);
-        const flags = (isManagementOpcode ? Opcode.MANAGEMENT_OPCODE : 0) | (isReserved ? Reserved.RESERVE_BIT : 0) | (isFinalVersion ? ProtocolVersion.FINAL_PROTOCOL_VERSION : 0);
 
-        writer.writeUInt8(flags);
+        // how to add finalversion and sereved bits here?
         writer.writeUInt16(attMtu);
         writer.writeInt8(windowSize);
-        return writer.toByteArray();
-    }
-
-    private static encodeHeader({ isUnused1, isHandshake, isManagement, isUnused2, isAckMsg, isEndingSegment, isUnused3, isBeginingSegment }: Header): ByteArray {
-
-        const writer = new DataWriter(Endian.Little);
-        const header = (isUnused1 ? HeaderBits.UNUSED_BIT : 0) | (isHandshake ? HeaderBits.HANDSHAKE_BIT : 0) | (isManagement ? HeaderBits.MANAGEMENT_MESSAGE : 0)
-            | (isUnused2 ? HeaderBits.UNUSED_BIT : 0) | (isAckMsg ? HeaderBits.ACKNOWLEDGEMENT_MSG : 0) | (isEndingSegment ? HeaderBits.ENDING_SEGMENT : 0)
-            | (isUnused3 ? HeaderBits.UNUSED_BIT : 0) | (isBeginingSegment ? HeaderBits.BEGINNING_SEGMENT : 0);
-
-        writer.writeUInt8(header);
         return writer.toByteArray();
     }
 }
