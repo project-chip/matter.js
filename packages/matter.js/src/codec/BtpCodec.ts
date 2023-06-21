8af092b9a619917c84b089e86ed8f74db9f7f8d2
Mahima Mandhana <136842984+mahimamandhanaa@users.noreply.github.com>
2023-06-21 14:40:21 -0700

    BTP Message enconding and decoding (#182)
    * creating a codec file for BTP which supports btp request and response formats
    
    * corrects the name of the methods
    
    * using bit names instead of abbreviations
    
    * change managementOpcode type to number
    
    * decoding request handshake msg
    
    * decode msg for handshake response
    
    * covered edge cases
    
    * added Packet decode method
    
    * resloved PR comments
    
    * removed header and returned payload directly
    
    * refactored the codec
    
    * refactoring and updating version logic in response payload
    
    * renaming interfaces, changed type of versions in req and response
    
    * adds unit test case for decode methods
    
    * btpTest for encoding
    
    * removed extra space and tab
    
    * changed headers type, error msgs, refactored test
    
    * added error test cases
    
    * added test case that supports multiple versions in request body
    
    * updated failing test
    
    * fixed failing error test cases

---
+++ packages/matter.js/src/codec/BtpCodec.ts
@@ -0,0 +1,143 @@
+/**
+ * @license
+ * Copyright 2022-2023 Project CHIP Authors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import { ByteArray, Endian } from "../util/ByteArray.js";
+import { DataReader } from "../util/DataReader.js";
+import { DataWriter } from "../util/DataWriter.js";
+
+export interface BtpHandshakeRequest {
+    versions: number[],
+    attMtu: number,
+    clientWindowSize: number,
+}
+
+export interface BtpHandshakeResponse {
+    version: number,
+    attMtu: number,
+    windowSize: number
+}
+
+export interface BtpPacketPayload {
+    ackNumber?: number,
+    sequenceNumber: number,
+    msgLength?: number,
+    segmentPayload?: ByteArray
+}
+
+export interface BtpHeader {
+    isHandshakeRequest: boolean,
+    hasManagementOpcode: boolean,
+    hasAckNumber: boolean,
+    isEndingSegment: boolean,
+    isBeginningSegment: boolean
+}
+
+export interface BtpPacket {
+    header: BtpHeader,
+    payload: BtpPacketPayload
+}
+
+export const enum BtpHeaderBits {
+    HandshakeBit = 0b01000000,
+    ManagementMsg = 0b00100000,
+    AckMsg = 0b00001000,
+    EndSegment = 0b00000100,
+    BeginSegment = 0b00000001
+}
+
+export const enum BtpOpcode {
+    HandshakeManagementOpcode = 0x6c,
+}
+
+const HANDSHAKE_HEADER = 0b01100101;
+
+export class BtpCodec {
+
+    static decodeBtpHandshakeRequest(data: ByteArray): BtpHandshakeRequest {
+        const reader = new DataReader(data, Endian.Little);
+        return this.decodeRequestPayload(reader);
+    }
+
+    static decodeBtpPacket(data: ByteArray): BtpPacket {
+        const reader = new DataReader(data, Endian.Little);
+
+        return {
+            header: this.decodeHeader(reader),
+            payload: this.decodeBtpPacketPayload(reader)
+        };
+    }
+
+    static encodeBtpHandshakeResponse({ version, attMtu, windowSize }: BtpHandshakeResponse): ByteArray {
+
+        const writer = new DataWriter(Endian.Little);
+        writer.writeUInt8(HANDSHAKE_HEADER);
+        writer.writeUInt8(BtpOpcode.HandshakeManagementOpcode);
+        writer.writeUInt8(version & 0x0f); //reserved bit and final version
+        writer.writeUInt16(attMtu);
+        writer.writeUInt8(windowSize);
+        return writer.toByteArray();
+    }
+
+    private static decodeBtpPacketPayload(reader: DataReader<Endian.Little>): BtpPacketPayload {
+
+        const ackNumber = reader.readUInt8();
+        const sequenceNumber = reader.readUInt8();
+        const msgLength = reader.readUInt16();
+        const segmentPayload = reader.getRemainingBytes();
+
+        return { ackNumber, sequenceNumber, msgLength, segmentPayload };
+    }
+
+    private static decodeRequestPayload(reader: DataReader<Endian.Little>): BtpHandshakeRequest {
+
+        const header = reader.readUInt8();
+        const opcode = reader.readUInt8();
+        let version = reader.readUInt8();
+
+        if (header !== HANDSHAKE_HEADER) throw new Error("BTPHandshake Request Headers is incorrect");
+        if (opcode !== BtpOpcode.HandshakeManagementOpcode) throw new Error("Management Opcode for BTPHandshake Request is incorrect");
+
+        const ver: number[] = [];
+        ver[0] = (version & 0xF0) >> 4;
+        ver[1] = version & 0x0F;
+
+        version = reader.readUInt8();
+        ver[2] = (version & 0xF0) >> 4;
+        ver[3] = version & 0x0F;
+
+        version = reader.readUInt8();
+        ver[4] = (version & 0xF0) >> 4;
+        ver[5] = version & 0x0F;
+
+        version = reader.readUInt8();
+        ver[6] = (version & 0xF0) >> 4;
+        ver[7] = version & 0x0F;
+
+        const versions = ver.filter(v => v !== 0)
+        if (versions.length == 0) throw new Error("No valid version provided");
+
+        const attMtu = reader.readUInt16();
+        const clientWindowSize = reader.readUInt8();
+
+        return { versions, attMtu, clientWindowSize };
+    }
+
+    private static decodeHeader(reader: DataReader<Endian.Little>): BtpHeader {
+
+        const headerBits = reader.readUInt8();
+        const isHandshakeRequest = (headerBits & BtpHeaderBits.HandshakeBit) !== 0;
+        const hasManagementOpcode = (headerBits & BtpHeaderBits.ManagementMsg) !== 0;
+        const hasAckNumber = (headerBits & BtpHeaderBits.AckMsg) !== 0;
+        const isEndingSegment = (headerBits & BtpHeaderBits.EndSegment) !== 0;
+        const isBeginningSegment = (headerBits & BtpHeaderBits.BeginSegment) !== 0;
+
+        const managementOpcode = reader.readUInt8();
+        if (hasManagementOpcode && managementOpcode == 0) throw new Error("Opcode expected but not provided");
+        if (!hasManagementOpcode && managementOpcode !== 0) throw new Error("Opcode not expected but provided");
+
+        return { isHandshakeRequest, hasManagementOpcode, hasAckNumber, isEndingSegment, isBeginningSegment };
+    }
+}
---
+++ packages/matter.js/test/codec/BtpTest.ts
@@ -0,0 +1,100 @@
+/**
+ * @license
+ * Copyright 2022-2023 Project CHIP Authors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import assert from "assert";
+import { BtpCodec } from "../../src/codec/BtpCodec.js";
+import { ByteArray } from "../../src/util/ByteArray.js";
+
+const DECODED_HANDSHAKE_REQUEST = {
+    versions: [4], // 00000040
+    attMtu: 185, // 00b9
+    clientWindowSize: 6,
+}
+
+const DECODED_HANDSHAKE_REQUEST_WITH_MULTIPLE_VERSIONS = {
+    versions: [4, 5, 6],
+    attMtu: 185,
+    clientWindowSize: 6,
+}
+
+const DECODED_HANDSHAKE_RESPONSE = {
+    version: 4,
+    attMtu: 256,
+    windowSize: 6,
+}
+
+const DECODED_PAYLOAD = {
+    header: {
+        isHandshakeRequest: false,
+        hasManagementOpcode: false,
+        hasAckNumber: true,
+        isEndingSegment: true,
+        isBeginningSegment: true
+    },
+    payload: {
+        ackNumber: 0,
+        sequenceNumber: 68, // 4f
+        msgLength: 1024, // 0400
+        segmentPayload: ByteArray.fromHex("00000049b6a902a9a5773dbb8cafd90120a7c7000015300120cb0c120a3499327ddaec4ebe60889df0f1bf80d8a4dea1dd6ffef16ef58ecafe25028e17240300280418"),
+    }
+}
+
+describe("BtpCodec", () => {
+    describe("decode", () => {
+        it("decodes a valid request handshake message", () => {
+            const result = BtpCodec.decodeBtpHandshakeRequest(ByteArray.fromHex("656c04000000b90006"));
+
+            assert.deepEqual(result, DECODED_HANDSHAKE_REQUEST);
+        });
+
+        it("decodes a valid request handshake message with multiple versions", () => {
+            const result = BtpCodec.decodeBtpHandshakeRequest(ByteArray.fromHex("656c04560000b90006"));
+
+            assert.deepEqual(result, DECODED_HANDSHAKE_REQUEST_WITH_MULTIPLE_VERSIONS);
+        });
+
+        it("decodes a valid btp packet PDU", () => {
+            const result = BtpCodec.decodeBtpPacket(ByteArray.fromHex("0d000044000400000049b6a902a9a5773dbb8cafd90120a7c7000015300120cb0c120a3499327ddaec4ebe60889df0f1bf80d8a4dea1dd6ffef16ef58ecafe25028e17240300280418"));
+
+            assert.deepEqual(result, DECODED_PAYLOAD);
+        });
+    });
+
+    describe("encode", () => {
+        it("encodes a valid response handshake message", () => {
+            const result = BtpCodec.encodeBtpHandshakeResponse(DECODED_HANDSHAKE_RESPONSE);
+
+            assert.deepEqual(result, ByteArray.fromHex("656c04000106"));
+        });
+    });
+
+    describe("Errors", () => {
+        it("incorrect headers in handshake request", () => {
+            expect(() => BtpCodec.decodeBtpHandshakeRequest(ByteArray.fromHex("0d6c04000000b90006")))
+                .toThrowError("BTPHandshake Request Headers is incorrect");
+        });
+
+        it("incorrect management opcode in handshake request", () => {
+            expect(() => BtpCodec.decodeBtpHandshakeRequest(ByteArray.fromHex("656d04000000b90006")))
+                .toThrowError("Management Opcode for BTPHandshake Request is incorrect");
+        });
+
+        it("no valid version provided in request handshake", () => {
+            expect(() => BtpCodec.decodeBtpHandshakeRequest(ByteArray.fromHex("656c00000000b90006")))
+                .toThrowError("No valid version provided");
+        });
+
+        it("opcode expected but not provided error in decoding the header", () => {
+            expect(() => BtpCodec.decodeBtpPacket(ByteArray.fromHex("65000044000400000049b6a902a9a5773dbb8cafd90120a7c7000015300120cb0c120a3499327ddaec4ebe60889df0f1bf80d8a4dea1dd6ffef16ef58ecafe25028e17240300280418")))
+                .toThrowError("Opcode expected but not provided");
+        });
+
+        it("opcode not expected but provided error in decoding the header", () => {
+            expect(() => BtpCodec.decodeBtpPacket(ByteArray.fromHex("0d6c0044000400000049b6a902a9a5773dbb8cafd90120a7c7000015300120cb0c120a3499327ddaec4ebe60889df0f1bf80d8a4dea1dd6ffef16ef58ecafe25028e17240300280418")))
+                .toThrowError("Opcode not expected but provided");
+        });
+    });
+});
