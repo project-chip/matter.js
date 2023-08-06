e12d8f3bc6a3b5fdbc3267b9fd66f0dda1054c5c
Ingo Fischer <github@fischer-ka.de>
2023-08-06 23:12:10 +0200

    AttributeDataDecoder should also call methods with Attribute

--- packages/matter.js/src/protocol/interaction/AttributeDataDecoder.ts
+++ packages/matter.js/src/protocol/interaction/AttributeDataDecoder.ts
@@ -15,7 +15,7 @@ import { UnexpectedDataError } from "../../common/MatterError.js";
 
 const logger = Logger.get("AttributeDataDecoder");
 
-export interface DecodedAttributeReportValue {
+export interface DecodedAttributeReportValue<T> {
     path: {
         nodeId?: NodeId,
         endpointId: number,
@@ -24,10 +24,10 @@ export interface DecodedAttributeReportValue {
         attributeName: string
     },
     version: number,
-    value: any,
+    value: T,
 }
 
-export interface DecodedAttributeValue {
+export interface DecodedAttributeValue<T> {
     path: {
         nodeId?: NodeId,
         endpointId: number,
@@ -36,20 +36,20 @@ export interface DecodedAttributeValue {
         attributeName: string
     },
     version?: number,
-    value: any,
+    value: T,
 }
 
-export function normalizeAndDecodeReadAttributeReport(data: TypeFromSchema<typeof TlvAttributeReport>[]): DecodedAttributeReportValue[] {
+export function normalizeAndDecodeReadAttributeReport(data: TypeFromSchema<typeof TlvAttributeReport>[]): DecodedAttributeReportValue<any>[] {
     // TODO Decide how to handle the attribute report status field, right now we ignore it
     const dataValues = data.flatMap(({ attributeData }) => attributeData !== undefined ? attributeData : []);
 
-    return normalizeAndDecodeAttributeData(dataValues) as DecodedAttributeReportValue[]; // dataVersion existing in incoming data, so must also in outgoing data
+    return normalizeAndDecodeAttributeData(dataValues) as DecodedAttributeReportValue<any>[]; // dataVersion existing in incoming data, so must also in outgoing data
 }
 
 export function normalizeAttributeData(data: TypeFromSchema<typeof TlvAttributeData>[], acceptWildcardPaths = false): TypeFromSchema<typeof TlvAttributeData>[][] {
     // Fill in missing path elements when tag compression is used
     let lastPath: { nodeId?: NodeId, endpointId: number, clusterId: number, attributeId: number } | undefined;
-    data.forEach((value) => {
+    data.forEach(value => {
         if (value === undefined) return;
         const { path } = value;
         if (path.enableTagCompression) {
@@ -67,7 +67,7 @@ export function normalizeAttributeData(data: TypeFromSchema<typeof TlvAttributeD
 
     // Put all returned values into a map to group by path
     const responseList = new Map<string, TypeFromSchema<typeof TlvAttributeData>[]>(); // TODO CHECK
-    data.forEach((value) => {
+    data.forEach(value => {
         if (!value) return;
         const { path: { nodeId, endpointId, clusterId, attributeId } } = value;
         const mapId = `${nodeId}-${endpointId}-${clusterId}-${attributeId}`;
@@ -79,10 +79,10 @@ export function normalizeAttributeData(data: TypeFromSchema<typeof TlvAttributeD
     return Array.from(responseList.values());
 }
 
-export function normalizeAndDecodeAttributeData(data: TypeFromSchema<typeof TlvAttributeData>[]): DecodedAttributeValue[] {
+export function normalizeAndDecodeAttributeData(data: TypeFromSchema<typeof TlvAttributeData>[]): DecodedAttributeValue<any>[] {
 
     const responseList = normalizeAttributeData(data);
-    const result: DecodedAttributeValue[] = [];
+    const result = new Array<DecodedAttributeValue<any>>();
     responseList.forEach(values => {
         const { path: { nodeId, endpointId, clusterId, attributeId }, dataVersion } = values[0];
 
@@ -120,10 +120,10 @@ export function decodeValueForAttribute<A extends Attribute<any, any>>(attribute
         return conformanceValue;
     }
 
-    return decodeValueForSchema(schema, values);
+    return decodeAttributeValueWithSchema(schema, values);
 }
 
-export function decodeValueForSchema<T>(schema: TlvSchema<T>, values: TypeFromSchema<typeof TlvAttributeData>[], defaultValue?: T): T | undefined {
+export function decodeAttributeValueWithSchema<T>(schema: TlvSchema<T>, values: TypeFromSchema<typeof TlvAttributeData>[], defaultValue?: T): T | undefined {
     // No values, so use default value if available
     if (!values.length) {
         return defaultValue;
@@ -141,8 +141,7 @@ export function decodeValueForSchema<T>(schema: TlvSchema<T>, values: TypeFromSc
     return schema.decodeFromChunkedArray(values.map(({ data, path: { listIndex } }) => ({ listIndex, element: data }))) as T;
 }
 
-
-export function structureReadDataToClusterObject(data: DecodedAttributeReportValue[]) {
+export function structureReadAttributeDataToClusterObject(data: DecodedAttributeReportValue<any>[]) {
     const structure: { [key: number]: { [key: number]: { [key: string]: any } } } = {};
     for (const { path: { endpointId, clusterId, attributeName }, value } of data) {
         if (structure[endpointId] === undefined) {
