7ff550b821c67821f945b9e2a494d00512b46068
Ingo Fischer <github@fischer-ka.de>
2023-08-14 21:55:11 +0200

    fix tests

--- packages/matter.js/test/log/LoggerTest.ts
+++ packages/matter.js/test/log/LoggerTest.ts
@@ -222,7 +222,7 @@ describe("Logger", () => {
             const result = logTestLine({ format: Format.ANSI });
 
             expect(result?.message).toBe(
-                "\u001b[37m\u001b[2mxxxx-xx-xx xx:xx:xx.xxx DEBUG\u001b[0m \u001b[37m\u001b[1mUnitTest            \u001b[0m \u001b[37mtest\u001b[0m",
+                "\u001b[90m\u001b[2mxxxx-xx-xx xx:xx:xx.xxx DEBUG\u001b[0m \u001b[90m\u001b[1mUnitTest            \u001b[0m \u001b[90mtest\u001b[0m",
             );
         });
 
@@ -230,7 +230,7 @@ describe("Logger", () => {
             const result = logTestDict({ format: Format.ANSI });
 
             expect(result?.message).toBe(
-                "\u001b[37m\u001b[2mxxxx-xx-xx xx:xx:xx.xxx DEBUG\u001b[0m \u001b[37m\u001b[1mUnitTest            \u001b[0m \u001b[37mdict test \u001b[34mfoo:\u001b[37m bar \u001b[34mbiz:\u001b[37m 1\u001b[0m",
+                "\u001b[90m\u001b[2mxxxx-xx-xx xx:xx:xx.xxx DEBUG\u001b[0m \u001b[90m\u001b[1mUnitTest            \u001b[0m \u001b[90mdict test \u001b[34mfoo:\u001b[90m bar \u001b[34mbiz:\u001b[90m 1\u001b[0m",
             );
         });
 
@@ -241,7 +241,7 @@ describe("Logger", () => {
                 logger.debug("test");
             });
             expect(result?.message).toBe(
-                "\u001b[37m\u001b[2mxxxx-xx-xx xx:xx:xx.xxx DEBUG\u001b[0m \u001b[37m\u001b[1mThisIsAFac~yLongName\u001b[0m \u001b[37mtest\u001b[0m",
+                "\u001b[90m\u001b[2mxxxx-xx-xx xx:xx:xx.xxx DEBUG\u001b[0m \u001b[90m\u001b[1mThisIsAFac~yLongName\u001b[0m \u001b[90mtest\u001b[0m",
             );
         });
     });
