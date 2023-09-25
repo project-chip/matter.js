78d50d665c12cd09b982537b7951e8760037cc18
Ingo Fischer <github@fischer-ka.de>
2023-09-25 16:38:29 +0200

    Implement CLuster Server Handlers according to specs
    This PR implements AdministratorCommissioning, GeneralCommissioning and OperationalCredentials cluster server handlers as define dby specs. Where applicable infos from Specs are added as comments and some TODOs are still in.

--- packages/matter.js/src/cluster/server/AdministratorCommissioningServer.ts
+++ packages/matter.js/src/cluster/server/AdministratorCommissioningServer.ts
@@ -4,88 +4,206 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-import { MatterFlowError, NotImplementedError } from "../../common/MatterError.js";
+import { FabricIndex } from "../../datatype/FabricIndex.js";
+import { VendorId } from "../../datatype/VendorId.js";
 import { MatterDevice } from "../../MatterDevice.js";
-import { SecureChannelProtocol } from "../../protocol/securechannel/SecureChannelProtocol.js";
+import { StatusResponseError } from "../../protocol/interaction/InteractionMessenger.js";
+import { StatusCode } from "../../protocol/interaction/InteractionProtocol.js";
 import { PaseServer } from "../../session/pase/PaseServer.js";
 import { Session } from "../../session/Session.js";
+import { Time, Timer } from "../../time/Time.js";
 import { ByteArray } from "../../util/ByteArray.js";
 import { AdministratorCommissioning } from "../definitions/AdministratorCommissioningCluster.js";
+import { AttributeServer } from "./AttributeServer.js";
 import { ClusterServerHandlers } from "./ClusterServerTypes.js";
 
-async function openCommissioningWindow(
-    secureChannelProtocol: SecureChannelProtocol,
-    pakeVerifier: ByteArray,
-    discriminator: number,
-    iterations: number,
-    salt: ByteArray,
-    commissioningTimeout: number,
-    session: Session<MatterDevice>,
-) {
-    //windowStatus.set(CommissioningWindowStatus.EnhancedWindowOpen);
-    secureChannelProtocol.updatePaseCommissioner(PaseServer.fromVerificationValue(pakeVerifier, { iterations, salt }));
-    await session.getContext().openCommissioningModeWindow(2, discriminator, commissioningTimeout);
-}
+export const MAXIMUM_COMMISSIONING_TIMEOUT_S = 15 * 60; // 900 seconds/15 minutes
+export const MINIMUM_COMMISSIONING_TIMEOUT_S = 3 * 60; // 180 seconds/3 minutes
 
-function revokeCommissioning() {
-    // TODO: implement this
-    throw new NotImplementedError("Not implemented");
-}
+// General:
+// TODO If any format or validity errors related to the PAKEPasscodeVerifier, Iterations or Salt arguments arise, this command SHALL fail with a cluster specific status code of PAKEParameterError.
+// TODO In case of any other parameter error, this command SHALL fail with a status code of COMMAND_INVALID.
 
-export const AdministratorCommissioningHandler: (
-    secureChannelProtocol: SecureChannelProtocol,
-) => ClusterServerHandlers<typeof AdministratorCommissioning.Cluster> = secureChannelProtocol => ({
-    openCommissioningWindow: async function ({
-        request: { pakePasscodeVerifier: pakeVerifier, discriminator, iterations, salt, commissioningTimeout },
-        session /* attributes: { windowStatus } */,
-    }) {
-        await openCommissioningWindow(
-            secureChannelProtocol,
-            pakeVerifier,
-            discriminator,
-            iterations,
-            salt,
-            commissioningTimeout,
-            session,
-        );
-    },
+class AdministratorCommissioningManager {
+    private commissioningWindowTimeout?: Timer;
+    private fabricRemoveHandler = () => this.adminFabricIndexAttribute.setLocal(null);
 
-    revokeCommissioning: async function () {
-        revokeCommissioning();
-    },
-});
+    constructor(
+        private readonly windowStatusAttribute: AttributeServer<AdministratorCommissioning.CommissioningWindowStatus>,
+        private readonly adminFabricIndexAttribute: AttributeServer<FabricIndex | null>,
+        private readonly adminVendorIdAttribute: AttributeServer<VendorId | null>,
+    ) {
+        windowStatusAttribute.setLocal(AdministratorCommissioning.CommissioningWindowStatus.WindowNotOpen);
+    }
 
-const Cluster = AdministratorCommissioning.Cluster.with("Basic");
-export const BasicAdminCommissioningHandler: (
-    secureChannelProtocol: SecureChannelProtocol,
-) => ClusterServerHandlers<typeof Cluster> = secureChannelProtocol => ({
-    openCommissioningWindow: async function ({
-        request: { pakePasscodeVerifier: pakeVerifier, discriminator, iterations, salt, commissioningTimeout },
-        session /* attributes: { windowStatus } */,
-    }) {
-        await openCommissioningWindow(
-            secureChannelProtocol,
-            pakeVerifier,
-            discriminator,
-            iterations,
-            salt,
-            commissioningTimeout,
-            session,
-        );
-    },
+    initializeCommissioningWindow(commissioningTimeout: number, session: Session<MatterDevice>) {
+        this.commissioningWindowTimeout = Time.getTimer(commissioningTimeout * 1000, () =>
+            this.closeCommissioningWindow(session),
+        ).start();
 
-    revokeCommissioning: async function () {
-        revokeCommissioning();
-    },
+        this.adminFabricIndexAttribute.setLocal(session.getAssociatedFabric().fabricIndex);
+        this.adminVendorIdAttribute.setLocal(session.getAssociatedFabric().rootVendorId);
+        session.getAssociatedFabric().addRemoveCallback(this.fabricRemoveHandler);
+    }
 
-    openBasicCommissioningWindow: async function ({
-        request: { commissioningTimeout },
-        session /* attributes: { windowStatus } */,
-    }) {
-        const device = session.getContext();
-        if (device.getFabrics().length > 0) {
-            throw new MatterFlowError("Already commissioned"); // is that correct?
+    validateCommissioningWindowRequirements(commissioningTimeout: number, device: MatterDevice) {
+        if (this.commissioningWindowTimeout !== undefined) {
+            throw new StatusResponseError("A commissioning window is already opened.", StatusCode.Failure, 0x02); // TODO Busy: use Enum when generated
         }
-        await session.getContext().openCommissioningModeWindow(1, undefined, commissioningTimeout);
-    },
-});
+
+        if (commissioningTimeout > MAXIMUM_COMMISSIONING_TIMEOUT_S) {
+            throw new StatusResponseError(
+                `Commissioning timeout must not exceed ${MAXIMUM_COMMISSIONING_TIMEOUT_S} seconds.`,
+                StatusCode.InvalidCommand,
+            );
+        }
+        if (commissioningTimeout < MINIMUM_COMMISSIONING_TIMEOUT_S) {
+            throw new StatusResponseError(
+                `Commissioning timeout must not be lower then ${MINIMUM_COMMISSIONING_TIMEOUT_S} seconds.`,
+                StatusCode.InvalidCommand,
+            );
+        }
+
+        if (device.isFailsafeArmed()) {
+            throw new StatusResponseError(
+                "Failsafe timer armed, assume commissioning in progress.",
+                StatusCode.Failure,
+                0x02,
+            ); // TODO Busy: use Enum when generated
+        }
+    }
+
+    async openEnhancedCommissioningWindow(
+        pakeVerifier: ByteArray,
+        discriminator: number,
+        iterations: number,
+        salt: ByteArray,
+        commissioningTimeout: number,
+        session: Session<MatterDevice>,
+    ) {
+        const device = session.getContext();
+
+        this.validateCommissioningWindowRequirements(commissioningTimeout, device);
+
+        await device.allowEnhancedCommissioning(
+            discriminator,
+            PaseServer.fromVerificationValue(pakeVerifier, { iterations, salt }),
+            () => {
+                session.getAssociatedFabric().deleteRemoveCallback(this.fabricRemoveHandler);
+                this.endCommissioning();
+            },
+        );
+        this.windowStatusAttribute.setLocal(AdministratorCommissioning.CommissioningWindowStatus.EnhancedWindowOpen);
+
+        this.initializeCommissioningWindow(commissioningTimeout, session);
+    }
+
+    async openBasicCommissioningWindow(commissioningTimeout: number, session: Session<MatterDevice>) {
+        const device = session.getContext();
+
+        this.validateCommissioningWindowRequirements(commissioningTimeout, device);
+
+        await device.allowBasicCommissioning(() => {
+            session.getAssociatedFabric().deleteRemoveCallback(this.fabricRemoveHandler);
+            this.endCommissioning();
+        });
+        this.windowStatusAttribute.setLocal(AdministratorCommissioning.CommissioningWindowStatus.BasicWindowOpen);
+
+        this.initializeCommissioningWindow(commissioningTimeout, session);
+    }
+
+    private endCommissioning() {
+        if (this.commissioningWindowTimeout !== undefined) {
+            this.commissioningWindowTimeout.stop();
+            this.commissioningWindowTimeout = undefined;
+        }
+        this.windowStatusAttribute.setLocal(AdministratorCommissioning.CommissioningWindowStatus.WindowNotOpen);
+        this.adminFabricIndexAttribute.setLocal(null);
+        this.adminVendorIdAttribute.setLocal(null);
+    }
+
+    closeCommissioningWindow(session: Session<MatterDevice>) {
+        session.getContext().endCommissioning();
+        this.endCommissioning();
+    }
+
+    async revokeCommissioning(session: Session<MatterDevice>) {
+        if (this.commissioningWindowTimeout === undefined) {
+            throw new StatusResponseError(
+                "No commissioning window is opened that could be revoked.",
+                StatusCode.Failure,
+                0x04,
+            ); // TODO WindowNotOpen: use Enum when generated
+        }
+        this.closeCommissioningWindow(session);
+    }
+
+    destroy() {
+        if (this.commissioningWindowTimeout !== undefined) {
+            this.commissioningWindowTimeout.stop();
+            this.commissioningWindowTimeout = undefined;
+        }
+    }
+}
+
+export const AdministratorCommissioningHandler: () => ClusterServerHandlers<
+    typeof AdministratorCommissioning.Cluster
+> = () => {
+    let manager: AdministratorCommissioningManager;
+
+    return {
+        initializeClusterServer: ({ attributes: { windowStatus, adminVendorId, adminFabricIndex } }) => {
+            manager = new AdministratorCommissioningManager(windowStatus, adminFabricIndex, adminVendorId);
+        },
+
+        openCommissioningWindow: async ({
+            request: { pakePasscodeVerifier: pakeVerifier, discriminator, iterations, salt, commissioningTimeout },
+            session /* attributes: { windowStatus } */,
+        }) =>
+            manager.openEnhancedCommissioningWindow(
+                pakeVerifier,
+                discriminator,
+                iterations,
+                salt,
+                commissioningTimeout,
+                session,
+            ),
+
+        revokeCommissioning: async ({ session }) => manager.revokeCommissioning(session),
+
+        destroyClusterServer: () => manager.destroy(),
+    };
+};
+
+const AdministratorCommissioningBasicCluster = AdministratorCommissioning.Cluster.with("Basic");
+export const BasicAdminCommissioningHandler: () => ClusterServerHandlers<
+    typeof AdministratorCommissioningBasicCluster
+> = () => {
+    let manager: AdministratorCommissioningManager;
+
+    return {
+        initializeClusterServer: ({ attributes: { windowStatus, adminVendorId, adminFabricIndex } }) => {
+            manager = new AdministratorCommissioningManager(windowStatus, adminFabricIndex, adminVendorId);
+        },
+
+        openCommissioningWindow: async ({
+            request: { pakePasscodeVerifier: pakeVerifier, discriminator, iterations, salt, commissioningTimeout },
+            session,
+        }) =>
+            manager.openEnhancedCommissioningWindow(
+                pakeVerifier,
+                discriminator,
+                iterations,
+                salt,
+                commissioningTimeout,
+                session,
+            ),
+
+        openBasicCommissioningWindow: async ({ request: { commissioningTimeout }, session }) =>
+            manager.openBasicCommissioningWindow(commissioningTimeout, session),
+
+        revokeCommissioning: async ({ session }) => manager.revokeCommissioning(session),
+
+        destroyClusterServer: () => manager.destroy(),
+    };
+};
--- packages/matter.js/src/cluster/server/GeneralCommissioningServer.ts
+++ packages/matter.js/src/cluster/server/GeneralCommissioningServer.ts
@@ -7,6 +7,7 @@
 import { ImplementationError, MatterFlowError } from "../../common/MatterError.js";
 import { Logger } from "../../log/Logger.js";
 import { assertSecureSession } from "../../session/SecureSession.js";
+import { AdministratorCommissioning } from "../definitions/AdministratorCommissioningCluster.js";
 import { BasicInformationCluster } from "../definitions/BasicInformationCluster.js";
 import { GeneralCommissioning, GeneralCommissioningCluster } from "../definitions/GeneralCommissioningCluster.js";
 import { ClusterServerHandlers } from "./ClusterServerTypes.js";
@@ -20,10 +21,51 @@ export const GeneralCommissioningClusterHandler: (options?: {
     /** If set, only these country codes are allowed to be set when changing country is allowed. */
     countryCodeWhitelist?: string[];
 }) => ClusterServerHandlers<typeof GeneralCommissioningCluster> = options => ({
-    armFailSafe: async ({ request: { breadcrumb: breadcrumbStep }, attributes: { breadcrumb }, session }) => {
-        // TODO Add handling for ExpiryLengthSeconds field and Error handling, see 11.9.7.2
+    initializeClusterServer: ({ attributes: { breadcrumb } }) => {
+        breadcrumb.setLocal(BigInt(0));
+    },
 
-        session.getContext().armFailSafe();
+    armFailSafe: async ({
+        request: { breadcrumb: breadcrumbStep, expiryLengthSeconds },
+        attributes: { breadcrumb, basicCommissioningInfo },
+        session,
+        endpoint,
+    }) => {
+        assertSecureSession(session, "armFailSafe can only be called on a secure session");
+        const device = session.getContext();
+
+        try {
+            // If the fail-safe timer is not currently armed, the commissioning window is open, and the command was
+            // received over a CASE session, the command SHALL leave the current fail-safe state unchanged and immediately
+            // respond with an ArmFailSafeResponse containing an ErrorCode value of BusyWithOtherAdmin. This is done to
+            // allow commissioners, which use PASE connections, the opportunity to use the failsafe during the
+            // relatively short commissioning window.
+            if (
+                !device.isFailsafeArmed() &&
+                endpoint.getClusterServer(AdministratorCommissioning.Cluster)?.getWindowStatusAttribute() !==
+                    AdministratorCommissioning.CommissioningWindowStatus.WindowNotOpen &&
+                !session.isPase()
+            ) {
+                throw new MatterFlowError("Failed to arm failsafe using CASE while commissioning window is opened.");
+            }
+
+            await device.armFailSafe(
+                expiryLengthSeconds,
+                basicCommissioningInfo.getLocal().maxCumulativeFailsafeSeconds,
+                session.getFabric(),
+                endpoint,
+            );
+        } catch (error) {
+            if (error instanceof MatterFlowError) {
+                logger.debug(`Error while arming failSafe timer`, error);
+                return {
+                    errorCode: GeneralCommissioning.CommissioningError.BusyWithOtherAdmin,
+                    debugText: error.message,
+                };
+            } else {
+                throw error;
+            }
+        }
         breadcrumb.setLocal(breadcrumbStep);
         return SuccessResponse;
     },
@@ -101,16 +143,44 @@ export const GeneralCommissioningClusterHandler: (options?: {
     },
 
     commissioningComplete: async ({ session, attributes: { breadcrumb } }) => {
-        // TODO Add error handling as defined in 11.9.7.6
+        const device = session.getContext();
+        if (!device.isFailsafeArmed()) {
+            return { errorCode: GeneralCommissioning.CommissioningError.NoFailSafe, debugText: "FailSafe not armed." };
+        }
 
         assertSecureSession(session, "commissioningComplete can only be called on a secure session");
         const fabric = session.getFabric();
-        if (fabric === undefined)
-            throw new MatterFlowError("commissioningComplete is called but the fabric has not been defined yet");
-        breadcrumb.setLocal(BigInt(0));
-        logger.info(`Commissioning completed on fabric #${fabric.fabricId} as node #${fabric.nodeId}.`);
+        if (fabric === undefined) {
+            return {
+                errorCode: GeneralCommissioning.CommissioningError.InvalidAuthentication,
+                debugText: "No Fabric associated with the session.",
+            };
+        }
+        const failSafeContext = device.getFailSafeContext();
+        if (fabric.fabricIndex !== failSafeContext.associatedFabric?.fabricIndex) {
+            return {
+                errorCode: GeneralCommissioning.CommissioningError.BusyWithOtherAdmin,
+                debugText: `Associated fabric ${fabric.fabricIndex} does not match the one from the failsafe context ${failSafeContext.associatedFabric?.fabricIndex}.`,
+            };
+        }
+        if (failSafeContext.fabricIndex === undefined) {
+            return {
+                errorCode: GeneralCommissioning.CommissioningError.InvalidAuthentication, // ??
+                debugText: "Fabric from the currently open failsafe context is not completed.",
+            };
+        }
 
-        session.getContext().completeCommission();
+        // On successful execution of the CommissioningComplete command the following actions SHALL be undertaken on the Server:
+        // 1. The Fail-Safe timer associated with the current Fail-Safe context SHALL be disarmed.
+        // 2. The commissioning window at the Server SHALL be closed.
+        // 3. Any temporary administrative privileges automatically granted to any open PASE session SHALL be revoked (see Section 6.6.2.8, “Bootstrapping of the Access Control Cluster”).
+        // 4. The Secure Session Context of any PASE session still established at the Server SHALL be cleared.
+        await device.completeCommission();
+
+        // 5. The Breadcrumb attribute SHALL be reset to zero.
+        breadcrumb.setLocal(BigInt(0));
+
+        logger.info(`Commissioning completed on fabric #${fabric.fabricId} as node #${fabric.nodeId}.`);
 
         return SuccessResponse;
     },
