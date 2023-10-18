1b899a955e780339c3bdcc349bf6c3c006142538
Ingo Fischer <github@fischer-ka.de>
2023-10-18 18:07:47 +0200

    fix name of parameter

--- packages/matter-node-shell.js/src/app.ts
+++ packages/matter-node-shell.js/src/app.ts
@@ -73,7 +73,7 @@ async function main() {
                             description: "Enable BLE support",
                             type: "boolean",
                         },
-                        resetStorage: {
+                        "reset-storage": {
                             description: "Reset storage of this node",
                             default: false,
                             type: "boolean",
