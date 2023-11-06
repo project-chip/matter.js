1fcd87177f21be9b25fa3fcfb99d67a5c58b159d
Ingo Fischer <github@fischer-ka.de>
2023-11-06 10:08:50 +0100

    make test reliable again

--- packages/matter-node.js/test/IntegrationTest.ts
+++ packages/matter-node.js/test/IntegrationTest.ts
@@ -1445,7 +1445,7 @@ describe("Integration Test", () => {
 
             assert.equal(commissioningChangedCallsServer.length, 2);
             assert.ok(sessionChangedCallsServer.length >= 7);
-            assert.equal(sessionChangedCallsServer[7].fabricIndex, FabricIndex(2));
+            assert.equal(sessionChangedCallsServer[sessionChangedCallsServer.length - 1].fabricIndex, FabricIndex(2));
             const sessionInfo = commissioningServer.getActiveSessionInformation();
             assert.equal(sessionInfo.length, 2);
             assert.ok(sessionInfo[1].fabric);
