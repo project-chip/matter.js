248a6a29850ae28dbd1b5bbdbcf01bb887902e97
Ingo Fischer <github@fischer-ka.de>
2023-11-30 17:51:59 +0100

    Respect UDP packet maximum size
    and do not process bigger messages

--- packages/matter.js/src/spec/Specifications.ts
+++ packages/matter.js/src/spec/Specifications.ts
@@ -21,3 +21,12 @@ export interface MatterApplicationClusterSpecificationV1_1 {}
 
 /** {@link https://csa-iot.org/developer-resource/specifications-download-request/ Matter Device Library Specification 1.1} */
 export interface MatterDeviceLibrarySpecificationV1_1 {}
+
+/** {@link https://csa-iot.org/developer-resource/specifications-download-request/ Matter Core Specification 1.2} */
+export interface MatterCoreSpecificationV1_2 {}
+
+/** {@link https://csa-iot.org/developer-resource/specifications-download-request/ Matter Application Cluster Specification 1.2} */
+export interface MatterApplicationClusterSpecificationV1_2 {}
+
+/** {@link https://csa-iot.org/developer-resource/specifications-download-request/ Matter Device Library Specification 1.2} */
+export interface MatterDeviceLibrarySpecificationV1_2 {}
