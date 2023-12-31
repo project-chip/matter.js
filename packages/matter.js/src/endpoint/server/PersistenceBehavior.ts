09773da0a519396a91d307ba645a7863bb4a423a
Greg Lauckhart <greg@lauckhart.com>
2023-12-30 18:34:59 -0800

    Squashed commit of the following:
    commit 8854828a515161a9cd37018cbe577a64e793ef8c
    Author: Greg Lauckhart <greg@lauckhart.com>
    Date:   Sat Dec 30 18:22:06 2023 -0800
    
        Storage refactor complete.
    
    commit f7efbec28565cf42e9ac97096a5b38f93ac26d65
    Author: Greg Lauckhart <greg@lauckhart.com>
    Date:   Sat Dec 30 11:23:47 2023 -0800
    
        WIP - fix up rest of matter.js tests
    
    commit 728f7b6a01b46d2fe03bb8263982a81c1c906376
    Author: Greg Lauckhart <greg@lauckhart.com>
    Date:   Fri Dec 29 23:47:31 2023 -0800
    
        Fix AttributeServerTest
    
        Old tests started version numbers at 0, 1 and sometimes 3 so needed to adjust
        assertions to match.  Some of the tests also used post-increment for
        increasing the version.  Logic seemed OK but assertions were asserting
        erronious behavior.  Fixed those too.  Also fixed an actual bug or two in
        test rewrite.
    
    commit 19aa92cfbb47ec46686ef0f8b48d72c676500880
    Author: Greg Lauckhart <greg@lauckhart.com>
    Date:   Fri Dec 29 17:04:47 2023 -0800
    
        WIP
    
        Remove redundant code in several tests, refactor v0.8 API for changes in
        storage management.
    
    commit a8aa7c7d634474fe2a309cb27f540312f6e5c7c5
    Merge: 5e211144 ac956483
    Author: Greg Lauckhart <greg@lauckhart.com>
    Date:   Thu Dec 28 01:50:50 2023 -0800
    
        Merge remote-tracking branch 'chip/device-gen' into device-gen
    
    commit 5e211144482b8e664df622f2e17c5dd4f4db1f06
    Author: Greg Lauckhart <greg@lauckhart.com>
    Date:   Thu Dec 28 01:46:55 2023 -0800
    
        WIP
    
        Refactor storage handling for CommissioningServer, InteractionServer and
        ClusterServer.  This decouples storage from clusters somewhat and allows for
        more flexibility in how NodeServer handles storage.
    
    commit c128816dad1dd430d013e501493233a1ed986af2
    Author: Greg Lauckhart <greg@lauckhart.com>
    Date:   Sat Dec 23 19:27:32 2023 -0800
    
        Bring back initialization options
    
        This decomposes server options and associates them with behaviors.  Also move
        some commissioning related node functionality from the node to a root endpoint
        behavior.
    
    commit 0db5e354582a36669981171b386a07b2222ea319
    Author: Greg Lauckhart <greg@lauckhart.com>
    Date:   Fri Dec 22 12:05:24 2023 -0800
    
        Commissioning WIP
    
    commit 6c22b67e77b2ca9ebb95c6ec2bc382f489c01a77
    Author: Greg Lauckhart <greg@lauckhart.com>
    Date:   Fri Dec 22 02:51:13 2023 -0800
    
        Working through commissioning

--- packages/matter.js/src/endpoint/server/PersistenceBehavior.ts
+++ packages/matter.js/src/endpoint/server/PersistenceBehavior.ts
@@ -5,14 +5,13 @@
  */
 
 import { Behavior } from "../../behavior/Behavior.js";
-import { LifecycleBehavior } from "../../behavior/definitions/lifecycle/LifecycleBehavior.js";
 import { Datasource } from "../../behavior/state/managed/Datasource.js";
 import { Val } from "../../behavior/state/managed/Val.js";
 import { Participant } from "../../behavior/state/transaction/Participant.js";
 import { Transaction } from "../../behavior/state/transaction/Transaction.js";
-import { StorageContext } from "../../storage/StorageContext.js";
-import { SupportedStorageTypes } from "../../storage/StringifyTools.js";
+import { ImplementationError } from "../../common/MatterError.js";
 import { MaybePromise } from "../../util/Type.js";
+import { PartStore } from "../part/PartStore.js";
 import { PartServer } from "./PartServer.js";
 
 /**
@@ -21,87 +20,56 @@ import { PartServer } from "./PartServer.js";
 export class PersistenceBehavior extends Behavior {
     static override readonly id = "persistence";
 
-    declare state: PersistenceBehavior.State;
+    declare internal: PersistenceBehavior.Internal;
 
-    override initialize() {
-        // Create a promise that resolves once the part is installed and we've
-        // loaded state.
-        //
-        // We need to wait for installation because we use the endpoint ID as
-        // the name of the storage context
-        return new Promise<void>((accept, reject) => {
-            const lifecycle = this.agent.get(LifecycleBehavior);
-            if (lifecycle.state.installed) {
-                this.#load().then(accept).catch(reject);
-            } else {
-                const listener = (installed: boolean) => {
-                    if (installed) {
-                        lifecycle.events.installed$Change.off(listener);
-                        this.#load().then(accept).catch(reject);
-                    }
-                };
-                lifecycle.events.installed$Change.on(listener);
-            }
-        });
-    }
+    static override immediate = true;
 
-    forBehavior(type: Behavior.Type) {
-        return this.state.stores?.forBehavior(type);
-    }
-
-    // Note - this doesn't actually need to be async now but I expect it will
-    // in the future so designing for that
-    async #load() {
-        const storage = this.agent.part.owner.stateStorage;
-        if (!storage) {
-            return;
+    /**
+     * Obtain a {@link Datasource.Store} for a specific {@link Behavior.Type}.
+     */
+    storeFor(type: Behavior.Type) {
+        const partStore = this.internal.partStore;
+        if (!partStore) {
+            throw new ImplementationError("Part storage accessed before initialization");
         }
-
-        const context = storage.createContext(`${this.part.id?.toString(16)}`.padStart(4, "0"));
-
-        const stores = {} as Record<string, Datasource.Store>;
-
-        this.state.stores = {
-            forBehavior(type: Behavior.Type): Datasource.Store {
-                let store = stores[type.id];
-                if (store === undefined) {
-                    store = stores[type.id] = DatasourceStore(context.createContext(type.id));
-                }
-                return store;
-            },
-        };
+        let store = this.internal.datasourceStores[type.id];
+        if (!store) {
+            store = this.internal.datasourceStores[type.id] = DatasourceStore(
+                partStore,
+                type.id,
+                partStore.initialValues[type.id]
+            );
+        }
+        return store;
     }
 }
 
 export namespace PersistenceBehavior {
-    export interface Stores {
-        forBehavior(behavior: Behavior.Type): Datasource.Store;
+    export class Internal {
+        partStore?: PartStore;
+        datasourceStores = {} as Record<string, Datasource.Store>;
     }
-
-    export class State {
-        stores?: Stores;
-    }
-}
-
-interface Operation {
-    context: StorageContext;
-    values?: Val.Struct;
 }
 
 interface StorageParticipant extends Participant {
-    journal: Operation[];
+    mutations?: Record<string, Val.Struct>;
 }
 
-function DatasourceStore(storage: StorageContext, initialValues: Val.Struct = {}): Datasource.Store {
+function DatasourceStore(partStore: PartStore, behaviorId: string, initialValues: Val.Struct = {}): Datasource.Store {
     return {
         initialValues,
 
         async set(transaction: Transaction, values: Val.Struct) {
-            participantFor(transaction, storage).journal.push({ context: storage, values });
-        },
-
-        async delete(transaction: Transaction) {
-            participantFor(transaction, storage).journal.push({ context: storage });
+            const participant = participantFor(transaction, partStore);
+            if (!participant.mutations) {
+                participant.mutations = {};
+            }
+            const behaviorMutations = participant.mutations[behaviorId];
+            if (behaviorMutations) {
+                Object.assign(behaviorMutations, values);
+            } else {
+                participant.mutations[behaviorId] = { ...values };
+            }
         },
     };
 }
@@ -110,52 +78,32 @@ function DatasourceStore(storage: StorageContext, initialValues: Val.Struct = {}
  * We create a single participant per storage/transaction pair.  This function
  * handles setup and retrieval of this participant.
  */
-function participantFor(transaction: Transaction, storage: StorageContext) {
-    let participant = transaction.getParticipant(storage) as StorageParticipant;
+function participantFor(transaction: Transaction, partStore: PartStore) {
+    let participant = transaction.getParticipant(partStore) as StorageParticipant | undefined;
     if (participant) {
-        return participant as StorageParticipant;
+        return participant;
     }
 
     participant = {
         description: "Storage",
 
-        role: storage,
+        role: partStore,
 
-        journal: Array<Operation>(),
+        mutations: undefined,
 
         commit1(): MaybePromise<void> {
             // Persistence serves phase one commit; values are added directly to
             // the journal so nothing necessary here
         },
 
-        commit2(): MaybePromise<void> {
-            // Commit the journal.  For real two-phase commit this would be writing
-            // a marker to the journal indicating successful commit, then it we'd
-            // consolidate into the primary store lazily.  For now we just let
-            // StorageContext do it's thing
-            for (const { context, values } of this.journal) {
-                if (values) {
-                    // Patch operation
-                    for (const key in values) {
-                        const value = values[key];
-                        if (value === undefined) {
-                            context.delete(key);
-                        } else {
-                            context.set(key, values[key] as SupportedStorageTypes);
-                        }
-                    }
-                } else {
-                    // Delete operation
-                    context.clear();
-                }
+        async commit2() {
+            if (this.mutations) {
+                await partStore.set(this.mutations);
             }
-
-            this.journal = [];
         },
 
         rollback(): MaybePromise<void> {
-            // For real two-phase commit we'd truncate the journal on disk
-            this.journal = [];
+            delete this.mutations;
         },
     };
 
