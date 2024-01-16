280467c3c311be9e7f258bd1a51ad1af9676f49d
Greg Lauckhart <greg@lauckhart.com>
2024-01-16 02:54:44 -0800

    Better time reset logic
    Integration test leaks the "real" time integration if it crashes...  Rather than add a bunch of finallies I just made
    the test runner ensure MockTime is installed before each suite.

--- packages/matter.js-tools/src/testing/mocks/time.ts
+++ packages/matter.js-tools/src/testing/mocks/time.ts
@@ -74,6 +74,9 @@ export class MockTime {
     reset(time = this.startTimeMs) {
         this.callbacks = [];
         this.timeMs = time;
+
+        // Some tests use "real" time implementation, ensure this can't leak across suites
+        reinstallTime?.();
     }
 
     now(): Date {
@@ -203,7 +206,10 @@ export class MockTime {
     }
 }
 
+let reinstallTime: undefined | (() => void);
+
 export const TheMockTime = new MockTime(0);
 export function timeSetup(Time: { get: () => MockTime }) {
-    Time.get = () => TheMockTime;
+    reinstallTime = () => Time.get = () => TheMockTime;        
+    reinstallTime();
 }
