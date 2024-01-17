875ddebf736c489bb516aba5cb144015251d9e97
Greg Lauckhart <greg@lauckhart.com>
2024-01-16 16:15:03 -0800

    Revert 0.7 APIs
    Now that so little is shared between NodeServer and CommissioningServer it no
    no longer makes sense to have a common base class.  Many of the changes now
    were just moving things around, and there were breaking differences in the API
    that are no longer necessary.
    
    This reverts most of the changes to MatterNode, CommissioningServer,
    CommissioningController and MatterServer.
    
    CommissioningServer still has a number of internal changes as it integrates
    components that do have changes.  But I don't think any of these changes
    affect the public API.

--- packages/matter.js-tools/src/index.ts
+++ packages/matter.js-tools/src/index.ts
@@ -6,4 +6,5 @@
 
 import "./testing/global-declarations.js";
 export * from "./testing/mocharc.cjs";
+export * from "./util/wtf.js";
 export * from "./util/package.js";
--- packages/matter.js/src/CommissioningController.ts
+++ packages/matter.js/src/CommissioningController.ts
@@ -120,14 +120,14 @@ export type NodeCommissioningOptions = CommissioningControllerNodeOptions & {
 };
 
 /** Controller class to commission and connect multiple nodes into one fabric. */
-export class CommissioningController implements MatterNode {
+export class CommissioningController extends MatterNode {
     private started = false;
     private ipv4Disabled?: boolean;
     private readonly listeningAddressIpv4?: string;
     private readonly listeningAddressIpv6?: string;
 
-    #storage?: StorageContext;
-    #mdnsScanner?: MdnsScanner;
+    private storage?: StorageContext;
+    private mdnsScanner?: MdnsScanner;
 
     private controllerInstance?: MatterController;
     private connectedNodes = new Map<NodeId, PairedNode>();
@@ -139,6 +139,7 @@ export class CommissioningController implements MatterNode {
      * @param options The options for the CommissioningController
      */
     constructor(private readonly options: CommissioningControllerOptions) {
+        super();
     }
 
     get nodeId() {
@@ -146,13 +147,13 @@ export class CommissioningController implements MatterNode {
     }
 
     assertIsAddedToMatterServer() {
-        if (this.#mdnsScanner === undefined || this.#storage === undefined) {
+        if (this.mdnsScanner === undefined || this.storage === undefined) {
             throw new ImplementationError("Add the node to the Matter instance before.");
         }
         if (!this.started) {
             throw new ImplementationError("The node needs to be started before interacting with the controller.");
         }
-        return { mdnsScanner: this.#mdnsScanner, storage: this.#storage };
+        return { mdnsScanner: this.mdnsScanner, storage: this.storage };
     }
 
     assertControllerIsStarted(errorText?: string) {
@@ -301,12 +302,30 @@ export class CommissioningController implements MatterNode {
     }
 
     /**
+     * Set the MDNS Scanner instance. Should be only used internally
+     *
+     * @param mdnsScanner MdnsScanner instance
+     */
+    setMdnsScanner(mdnsScanner: MdnsScanner) {
+        this.mdnsScanner = mdnsScanner;
+    }
+
+    /**
+     * Set the MDNS Broadcaster instance. Should be only used internally
+     *
+     * @param _mdnsBroadcaster MdnsBroadcaster instance
+     */
+    setMdnsBroadcaster(_mdnsBroadcaster: MdnsBroadcaster) {
+        // not needed
+    }
+
+    /**
      * Set the Storage instance. Should be only used internally
      *
      * @param storage storage context to use
      */
     setStorage(storage: StorageContext) {
-        this.#storage = storage;
+        this.storage = storage;
     }
 
     /** Returns true if t least one node is commissioned/paired with this controller instance. */
@@ -349,20 +368,8 @@ export class CommissioningController implements MatterNode {
         this.started = false;
     }
 
-    get port(): number | undefined {
+    getPort(): number | undefined {
         return this.options.localPort;
-    }
-
-    set storage(storage: StorageContext) {
-        this.#storage = storage;
-    }
-
-    setMdnsScanner(mdnsScanner: MdnsScanner) {
-        this.#mdnsScanner = mdnsScanner;
-    }
-
-    setMdnsBroadcaster(_mdnsBroadcaster: MdnsBroadcaster) {
-        // Controller does not use a broadcaster
     }
 
     initialize(ipv4Disabled: boolean) {
