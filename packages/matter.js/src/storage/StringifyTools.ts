221e9092a715ee0af6b1295bef6e022c8201b149
Ingo Fischer <github@fischer-ka.de>
2024-01-18 17:10:34 +0100

    Support sparse arrays in storage and restore "undefined" (8)and update)

--- packages/matter.js/src/storage/StringifyTools.ts
+++ packages/matter.js/src/storage/StringifyTools.ts
@@ -6,13 +6,20 @@
 
 import { UnexpectedDataError } from "../common/MatterError.js";
 import { AttributeId } from "../datatype/AttributeId.js";
+import { CaseAuthenticatedTag } from "../datatype/CaseAuthenticatedTag.js";
 import { ClusterId } from "../datatype/ClusterId.js";
 import { CommandId } from "../datatype/CommandId.js";
+import { DataVersion } from "../datatype/DataVersion.js";
+import { DeviceTypeId } from "../datatype/DeviceTypeId.js";
 import { EndpointNumber } from "../datatype/EndpointNumber.js";
+import { EntryIndex } from "../datatype/EntryIndex.js";
 import { EventId } from "../datatype/EventId.js";
+import { EventNumber } from "../datatype/EventNumber.js";
 import { FabricId } from "../datatype/FabricId.js";
 import { FabricIndex } from "../datatype/FabricIndex.js";
+import { FieldId } from "../datatype/FieldId.js";
 import { GroupId } from "../datatype/GroupId.js";
+import { NodeId } from "../datatype/NodeId.js";
 import { VendorId } from "../datatype/VendorId.js";
 import { ByteArray } from "../util/ByteArray.js";
 
@@ -24,14 +31,20 @@ type SupportedStorageBaseTypes =
     | bigint
     | ByteArray
     | AttributeId
+    | CaseAuthenticatedTag
     | ClusterId
     | CommandId
+    | DataVersion
+    | DeviceTypeId
     | EndpointNumber
+    | EntryIndex
     | EventId
+    | EventNumber
     | FabricId
     | FabricIndex
+    | FieldId
     | GroupId
-    // | NodeId -- duplicative of bigint as not currently branded
+    | NodeId
     | VendorId;
 
 /** Supported combined types to stringify the data for the storage that can be used as values. */
@@ -55,6 +68,9 @@ export function toJson(object: SupportedStorageTypes, spaces?: number): string {
     return JSON.stringify(
         object,
         (_key, value) => {
+            if (value === undefined) {
+                return `{"${JSON_SPECIAL_KEY_TYPE}":"Undefined"}`;
+            }
             if (typeof value === "bigint") {
                 return `{"${JSON_SPECIAL_KEY_TYPE}":"BigInt","${JSON_SPECIAL_KEY_VALUE}":"${value.toString()}"}`;
             }
@@ -84,6 +100,8 @@ export function fromJson(json: string): SupportedStorageTypes {
             const data = JSON.parse(value);
             const object = data[JSON_SPECIAL_KEY_TYPE];
             switch (object) {
+                case "Undefined":
+                    return undefined;
                 case "BigInt":
                     return BigInt(data[JSON_SPECIAL_KEY_VALUE]);
                 case "Uint8Array":
@@ -98,14 +116,20 @@ export function fromJson(json: string): SupportedStorageTypes {
 
                 // TODO Remove in the future, leave here for now for backward compatibility?
                 case "AttributeId":
+                case "CaseAuthenticatedTag":
                 case "ClusterId":
                 case "CommandId":
+                case "DataVersion":
+                case "DeviceTypeId":
                 case "EndpointNumber":
+                case "EntryIndex":
                 case "EventId":
                 case "FabricIndex":
+                case "FieldId":
                 case "GroupId":
                 case "VendorId":
                     return data[JSON_SPECIAL_KEY_VALUE];
+                case "EventNumber":
                 case "FabricId":
                 case "NodeId":
                     return BigInt(data[JSON_SPECIAL_KEY_VALUE]);
