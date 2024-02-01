5ac46e465132fac687be796e05b80e46e395160f
Ingo Fischer <github@fischer-ka.de>
2024-02-01 14:57:14 +0100

    Add generic attribute/event/command handling

---
+++ packages/matter-node-shell.js/src/util/String.ts
@@ -0,0 +1,9 @@
+/**
+ * @license
+ * Copyright 2022-2023 Project CHIP Authors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+export function firstLetterToLowerCase(str: string) {
+    return str.charAt(0).toLowerCase() + str.slice(1);
+}
