dacd3b84675553b826a88428096467abce39c389
Ingo Fischer <github@fischer-ka.de>
2024-02-01 14:57:55 +0100

    Optimize Subscription commands in shell

--- packages/matter-node-shell.js/src/shell/cmd_subscribe.ts
+++ packages/matter-node-shell.js/src/shell/cmd_subscribe.ts
@@ -10,47 +10,34 @@ import { MatterNode } from "../MatterNode.js";
 
 export default function commands(theNode: MatterNode) {
     return {
-        command: "subscribe",
-        describe: "Handle attribute and event subscriptions",
-        builder: (yargs: Argv) =>
-            yargs
-                // Pair
-                .command(
-                    "all [node-id]",
-                    "Subscribe to all events and attributes of a node",
-                    yargs => {
-                        return yargs.positional("node-id", {
-                            describe: "node id",
-                            default: undefined,
-                            type: "string",
-                        });
-                    },
-                    async argv => {
-                        const { nodeId } = argv;
-                        const node = (await theNode.connectAndGetNodes(nodeId))[0];
-
-                        await node.subscribeAllAttributesAndEvents({
-                            attributeChangedCallback: ({
-                                path: { nodeId, clusterId, endpointId, attributeName },
-                                value,
-                            }) =>
-                                console.log(
-                                    `${nodeId}: Attribute ${nodeId}/${endpointId}/${clusterId}/${attributeName} changed to ${Logger.toJSON(
-                                        value,
-                                    )}`,
-                                ),
-                            eventTriggeredCallback: ({ path: { nodeId, clusterId, endpointId, eventName }, events }) =>
-                                console.log(
-                                    `${nodeId} Event ${nodeId}/${endpointId}/${clusterId}/${eventName} triggered with ${Logger.toJSON(
-                                        events,
-                                    )}`,
-                                ),
-                        });
-                    },
-                ),
+        command: "subscribe [node-id]",
+        describe: "Subscribe to all events and attributes of a node",
+        builder: (yargs: Argv) => {
+            return yargs.positional("node-id", {
+                describe: "node id",
+                default: undefined,
+                type: "string",
+            });
+        },
 
         handler: async (argv: any) => {
-            argv.unhandled = true;
+            const { nodeId } = argv;
+            const node = (await theNode.connectAndGetNodes(nodeId))[0];
+
+            await node.subscribeAllAttributesAndEvents({
+                attributeChangedCallback: ({ path: { nodeId, clusterId, endpointId, attributeName }, value }) =>
+                    console.log(
+                        `${nodeId}: Attribute ${nodeId}/${endpointId}/${clusterId}/${attributeName} changed to ${Logger.toJSON(
+                            value,
+                        )}`,
+                    ),
+                eventTriggeredCallback: ({ path: { nodeId, clusterId, endpointId, eventName }, events }) =>
+                    console.log(
+                        `${nodeId} Event ${nodeId}/${endpointId}/${clusterId}/${eventName} triggered with ${Logger.toJSON(
+                            events,
+                        )}`,
+                    ),
+            });
         },
     };
 }
