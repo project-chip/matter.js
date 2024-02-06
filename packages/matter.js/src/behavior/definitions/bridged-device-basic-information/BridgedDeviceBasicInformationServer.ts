f994f4538f2a44e8aa2f65f044e2435c6e62f23c
Greg Lauckhart <greg@lauckhart.com>
2024-02-05 21:23:39 -0800

    Bridged nodes emit redundant reachability changes

--- packages/matter.js/src/behavior/definitions/bridged-device-basic-information/BridgedDeviceBasicInformationServer.ts
+++ packages/matter.js/src/behavior/definitions/bridged-device-basic-information/BridgedDeviceBasicInformationServer.ts
@@ -4,12 +4,27 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-/*** THIS FILE WILL BE REGENERATED IF YOU DO NOT REMOVE THIS MESSAGE ***/
-
 import { BridgedDeviceBasicInformationBehavior } from "./BridgedDeviceBasicInformationBehavior.js";
 
 /**
  * This is the default server implementation of BridgedDeviceBasicInformationBehavior.
+ * 
+ * All attributes are optional except for the "reachable" attribute.
  */
 export class BridgedDeviceBasicInformationServer extends BridgedDeviceBasicInformationBehavior {
+    override initialize() {
+        this.reactTo(this.events.reachable$Change, this.#emitReachableChange);
+    }
+
+    #emitReachableChange(reachable: boolean) {
+        // Per the specification.  Not sure what this adds vs. subscribing to attribute changes
+        this.events.reachableChanged.emit({ reachableNewValue: reachable }, this.context);
+    }
+}
+
+export namespace BridgedDeviceBasicInformationServer {
+    export class State extends BridgedDeviceBasicInformationBehavior.State {
+        // TODO - need real-world feedback to determine best default
+        override reachable = false;
+    }
 }
