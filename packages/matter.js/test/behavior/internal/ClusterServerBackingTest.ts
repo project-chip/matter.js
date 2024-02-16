c16a07b06d15bba6e6cca7c0f3f0da93e4dbb5b0
Greg Lauckhart <greg@lauckhart.com>
2024-02-15 16:12:37 -0800

    Finish fabric-filtered read

--- packages/matter.js/test/behavior/internal/ClusterServerBackingTest.ts
+++ packages/matter.js/test/behavior/internal/ClusterServerBackingTest.ts
@@ -16,8 +16,13 @@ import { FabricIndex } from "../../../src/datatype/FabricIndex.js";
 import { VendorId } from "../../../src/datatype/VendorId.js";
 import { NodeId } from "../../../src/datatype/NodeId.js";
 import { ByteArray } from "../../../src/util/ByteArray.js";
-import { TlvWriteRequest } from "../../../src/protocol/interaction/InteractionProtocol.js";
+import { TlvReadRequest, TlvWriteRequest } from "../../../src/protocol/interaction/InteractionProtocol.js";
 import { TypeFromSchema } from "../../../src/tlv/TlvSchema.js";
+import { TlvField, TlvObject } from "../../../src/tlv/TlvObject.js";
+import { TlvEnum } from "../../../src/tlv/TlvNumber.js";
+import { TlvNullable } from "../../../src/tlv/TlvNullable.js";
+import { TlvArray } from "../../../src/tlv/TlvArray.js";
+import { TlvSubjectId } from "../../../src/datatype/SubjectId.js";
 
 const ROOT_CERT = ByteArray.fromHex(
     "153001010024020137032414001826048012542826058015203b37062414001824070124080130094104d89eb7e3f3226d0918f4b85832457bb9981bca7aaef58c18fb5ec07525e472b2bd1617fb75ee41bd388f94ae6a6070efc896777516a5c54aff74ec0804cdde9d370a3501290118240260300414e766069362d7e35b79687161644d222bdde93a68300514e766069362d7e35b79687161644d222bdde93a6818300b404e8fb06526f0332b3e928166864a6d29cade53fb5b8918a6d134d0994bf1ae6dce6762dcba99e80e96249d2f1ccedb336b26990f935dba5a0b9e5b4c9e5d1d8f1818181824ff0118",
@@ -39,7 +44,11 @@ function createFabric(index = 1) {
     return builder.build(FabricIndex(index));
 }
 
-async function performWrite(node: MockServerNode, fabric: Fabric, request: TypeFromSchema<typeof TlvWriteRequest>["writeRequests"][number]) {
+async function performWrite(
+    node: MockServerNode,
+    fabric: Fabric,
+    request: TypeFromSchema<typeof TlvWriteRequest>["writeRequests"][number]
+) {
     const exchange = await node.createExchange({ fabric });
 
     const interactionServer = await node.act(agent => agent.network.internal.runtime.interactionServer);
@@ -58,16 +67,60 @@ async function performWrite(node: MockServerNode, fabric: Fabric, request: TypeF
     );
 }
 
-async function writeAcl(node: MockServerNode, fabric: Fabric, acl: TypeFromSchema<typeof AccessControl.TlvAccessControlEntryStruct>) {
+async function performRead(
+    node: MockServerNode,
+    fabric: Fabric,
+    isFabricFiltered: boolean,
+    request: Exclude<TypeFromSchema<typeof TlvReadRequest>["attributeRequests"], undefined>[number]
+) {
+    const exchange = await node.createExchange({ fabric });
+
+    const interactionServer = await node.act(agent => agent.network.internal.runtime.interactionServer);
+
+    const result = await interactionServer.handleReadRequest(
+        exchange,
+        {
+            interactionModelRevision: INTERACTION_MODEL_REVISION,
+            attributeRequests: [
+                request
+            ],
+            isFabricFiltered: isFabricFiltered,
+        },
+        {
+            packetHeader: { sessionType: SessionType.Unicast },
+        } as Message,
+    );
+
+    return result.attributeReportsPayload?.[0]?.attributeData?.payload;
+}
+
+// This is AccessControl.AccessControlEntryStruct but not sure how to remove fabric index from payload so just
+// redefining for now
+const AcesWithoutFabric = TlvObject({
+    privilege: TlvField(1, TlvEnum<AccessControl.AccessControlEntryPrivilege>()),
+    authMode: TlvField(2, TlvEnum<AccessControl.AccessControlEntryAuthMode>()),
+    subjects: TlvField(3, TlvNullable(TlvArray(TlvSubjectId))),
+    targets: TlvField(4, TlvNullable(TlvArray(AccessControl.TlvAccessControlTargetStruct))),
+});
+
+async function writeAcl(node: MockServerNode, fabric: Fabric, acl: TypeFromSchema<typeof AcesWithoutFabric>) {
     await performWrite(node, fabric, {
         path: {
             endpointId: EndpointNumber(0),
             clusterId: ClusterId(AccessControl.Cluster.id),
             attributeId: AttributeId(AccessControl.Cluster.attributes.acl.id),
         },
-        data: AccessControl.Cluster.attributes.acl.schema.encodeTlv([
+        data: TlvArray(AcesWithoutFabric).encodeTlv([
             acl
         ]),
+    });
+}
+
+async function readAcls(node: MockServerNode, fabric: Fabric, isFabricFiltered: boolean) {
+    return await performRead(node, fabric, isFabricFiltered, {
+        endpointId: EndpointNumber(0),
+        clusterId: ClusterId(AccessControl.Cluster.id),
+        attributeId: AttributeId(AccessControl.Cluster.attributes.acl.id),
     });
 }
 
@@ -83,7 +136,6 @@ describe("ClusterServerBacking", () => {
             authMode: 1,
             subjects: null,
             targets: null,
-            fabricIndex: FabricIndex(1),
         });
 
         await writeAcl(node, fabric2, {
@@ -91,12 +143,29 @@ describe("ClusterServerBacking", () => {
             authMode: 1,
             subjects: null,
             targets: null,
-            fabricIndex: FabricIndex(2),
         });
 
         expect(node.state.accessControl.acl).deep.equals([
             { privilege: 5, authMode: 1, subjects: null, targets: null, fabricIndex: 1 },
             { privilege: 5, authMode: 1, subjects: null, targets: null, fabricIndex: 2 },
+        ]);
+
+        const fabric1Acls = await readAcls(node, fabric1, true);
+
+        expect(fabric1Acls).deep.equals([
+            { privilege: 5, authMode: 1, subjects: null, targets: null, fabricIndex: 1 },
+        ]);
+
+        const fabric2Acls = await readAcls(node, fabric2, true);
+        expect(fabric2Acls).deep.equals([
+            { privilege: 5, authMode: 1, subjects: null, targets: null, fabricIndex: 2 },
+        ]);
+
+        // ACLs are fabric sensitive so fabricFiltered = false should have no effect
+        const allAcls = await readAcls(node, fabric1, false);
+
+        expect(allAcls).deep.equals([
+            { privilege: 5, authMode: 1, subjects: null, targets: null, fabricIndex: 1 },
         ]);
 
         await node.destroy();
