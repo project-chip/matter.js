2cb307467c3a7db16f3fe2fd6fe5226094892848
Ingo Fischer <github@fischer-ka.de>
2024-02-25 20:38:01 +0100

    Finalize SessionsBehavior

--- packages/matter.js/src/behavior/system/sessions/SessionsBehavior.ts
+++ packages/matter.js/src/behavior/system/sessions/SessionsBehavior.ts
@@ -4,9 +4,10 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-import { FabricIndex } from "../../../datatype/FabricIndex.js";
 import { NodeId } from "../../../datatype/NodeId.js";
+import { ExposedFabricInformation } from "../../../fabric/Fabric.js";
 import type { ServerNode } from "../../../node/ServerNode.js";
+import { SecureSession } from "../../../session/SecureSession.js";
 import { SessionManager } from "../../../session/SessionManager.js";
 import { EventEmitter, Observable } from "../../../util/Observable.js";
 import { Behavior } from "../../Behavior.js";
@@ -31,19 +32,53 @@ export class SessionsBehavior extends Behavior {
         this.reactTo(sessionManagerEvents.deleted, this.#enterOfflineMode);
     }
 
-    #enterOnlineMode(sessions: SessionManager<any>) {
-        this.reactTo(sessions.sessionOpened, session => {
-            this.state.sessions[session.id] = {
-                id: session.id,
-                type: session.isPase ? SessionsBehavior.SessionType.Pase : SessionsBehavior.SessionType.Case,
-                fabricIndex: session.fabric?.fabricIndex,
-                peerNodeId: session.peerNodeId,
-            };
-        });
+    #convertToExposedSession(session: SecureSession<any>): SessionsBehavior.Session {
+        return {
+            name: session.name,
+            nodeId: session.nodeId,
+            peerNodeId: session.peerNodeId,
+            fabric: session.fabric?.externalInformation,
+            isPeerActive: session.isPeerActive(),
+            lastInteractionTimestamp: session.timestamp,
+            lastActiveTimestamp: session.activeTimestamp,
+            numberOfActiveSubscriptions: session.numberOfActiveSubscriptions,
+        };
+    }
 
-        this.reactTo(sessions.sessionClosed, session => {
-            delete this.state.sessions[session.id];
-        });
+    #enterOnlineMode(sessions: SessionManager<any>) {
+        this.reactTo(sessions.sessionOpened, this.#sessionOpened);
+
+        this.reactTo(sessions.sessionClosed, this.#sessionClosed);
+
+        this.reactTo(sessions.subscriptionsChanged, this.#subscriptionsChanged);
+    }
+
+    #sessionOpened(session: SecureSession<any>) {
+        if (session.isPase) {
+            return;
+        }
+        const exposedSession = this.#convertToExposedSession(session);
+        this.state.sessions[session.id] = exposedSession;
+        this.events.opened.emit(exposedSession);
+    }
+
+    #sessionClosed(session: SecureSession<any>) {
+        delete this.state.sessions[session.id];
+        this.events.closed.emit(this.#convertToExposedSession(session));
+    }
+
+    #subscriptionsChanged(session: SecureSession<any>) {
+        if (session.isPase) {
+            return;
+        }
+        const sessionEntry = this.state.sessions[session.id];
+        if (sessionEntry === undefined) {
+            return;
+        }
+
+        sessionEntry.numberOfActiveSubscriptions = session.numberOfActiveSubscriptions;
+        this.state.sessions[session.id] = sessionEntry;
+        this.events.subscriptionsChanged.emit(sessionEntry);
     }
 
     #enterOfflineMode() {
@@ -52,24 +87,24 @@ export class SessionsBehavior extends Behavior {
 }
 
 export namespace SessionsBehavior {
-    export enum SessionType {
-        Pase = "PASE",
-        Case = "CASE",
-    }
-
     export interface Session {
-        id: number;
-        type: SessionType;
-        fabricIndex?: FabricIndex;
+        name: string;
+        nodeId: NodeId;
         peerNodeId: NodeId;
+        fabric?: ExposedFabricInformation;
+        isPeerActive: boolean;
+        lastInteractionTimestamp: number | undefined;
+        lastActiveTimestamp: number | undefined;
+        numberOfActiveSubscriptions: number;
     }
 
     export class State {
-        sessions = {} as Record<number, Session>;
+        sessions: Record<number, Session> = {};
     }
 
     export class Events extends EventEmitter {
-        opened = Observable<[session: Session]>;
-        closed = Observable<[session: Session]>;
+        opened = Observable<[session: Session]>();
+        closed = Observable<[session: Session]>();
+        subscriptionsChanged = Observable<[session: Session]>();
     }
 }
