86f11d507f4ea723fe00f0b91e6b686ea76dee3f
Giorgi Kavrelishvili <giorgi.kavrelishvili@pm.me>
2024-02-26 11:17:51 +0400

    Update MdnsServer.ts
    I think there is an issue with this, how can one message type be both at the same time?

--- packages/matter.js/src/mdns/MdnsServer.ts
+++ packages/matter.js/src/mdns/MdnsServer.ts
@@ -92,7 +92,7 @@ export class MdnsServer {
         const message = DnsCodec.decode(messageBytes);
         if (message === undefined) return; // The message cannot be parsed
         const { transactionId, messageType, queries, answers: knownAnswers } = message;
-        if (messageType !== DnsMessageType.Query && messageType !== DnsMessageType.TruncatedQuery) return;
+        if (messageType !== DnsMessageType.Query || messageType !== DnsMessageType.TruncatedQuery) return;
         if (queries.length === 0) return; // No queries to answer, can happen in a TruncatedQuery, let's ignore for now
         for (const portRecords of records.values()) {
             let answers = queries.flatMap(query => this.queryRecords(query, portRecords));
