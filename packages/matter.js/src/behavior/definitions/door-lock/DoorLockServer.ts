1286226bb5a1f485189c14f8f37bc3e55f57383f
Tu-Anh Hoang | Grey <73174241+GreydonDesu@users.noreply.github.com>
2024-04-09 10:50:34 +0200

    Update by recommendation
    Co-authored-by: Ingo Fischer <github@fischer-ka.de>

--- packages/matter.js/src/behavior/definitions/door-lock/DoorLockServer.ts
+++ packages/matter.js/src/behavior/definitions/door-lock/DoorLockServer.ts
@@ -4,7 +4,7 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-import { DoorLock } from "../../../cluster/definitions/index.js";
+import { DoorLock } from "../../../cluster/definitions/DoorLockCluster.js";
 import { DoorLockBehavior } from "./DoorLockBehavior.js";
 import LockState = DoorLock.LockState;
 
