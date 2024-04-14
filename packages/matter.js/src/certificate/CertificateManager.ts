7d9d44a4bfb1131d7c798418c8b1e6592a2d65a9
Ingo Fischer <github@fischer-ka.de>
2024-04-14 18:31:07 +0200

    Generalize Matter cert definitions

--- packages/matter.js/src/certificate/CertificateManager.ts
+++ packages/matter.js/src/certificate/CertificateManager.ts
@@ -132,218 +132,194 @@ export const VendorId_Matter = GenericMatterAttCertObject<VendorId>(1, uInt16To4
 /** matter-oid-pid = ASN.1 OID 1.3.6.1.4.1.37244.2.2 */
 export const ProductId_Matter = GenericMatterAttCertObject<number>(2, uInt16To4Chars);
 
-export const TlvRootCertificate = TlvObject({
-    serialNumber: TlvField(1, TlvByteString.bound({ maxLength: 20 })),
-    signatureAlgorithm: TlvField(2, TlvUInt8),
-    issuer: TlvField(
-        3,
-        TlvTaggedList({
-            issuerRcacId: TlvOptionalField(20, TlvUInt64),
-        }),
-    ),
-    notBefore: TlvField(4, TlvUInt32),
-    notAfter: TlvField(5, TlvUInt32),
-    subject: TlvField(
-        6,
-        TlvTaggedList({
-            rcacId: TlvField(20, TlvUInt64),
-        }),
-    ),
-    publicKeyAlgorithm: TlvField(7, TlvUInt8),
-    ellipticCurveIdentifier: TlvField(8, TlvUInt8),
-    ellipticCurvePublicKey: TlvField(9, TlvByteString),
-    extensions: TlvField(
-        10,
-        TlvTaggedList({
-            basicConstraints: TlvField(
-                1,
-                TlvObject({
-                    isCa: TlvField(1, TlvBoolean),
-                    pathLen: TlvOptionalField(2, TlvUInt8),
-                }),
-            ),
-            keyUsage: TlvField(2, TlvUInt16),
-            extendedKeyUsage: TlvOptionalField(3, TlvArray(TlvUInt8)),
-            subjectKeyIdentifier: TlvField(4, TlvByteString.bound({ length: 20 })),
-            authorityKeyIdentifier: TlvField(5, TlvByteString.bound({ length: 20 })),
-            futureExtension: TlvOptionalField(6, TlvByteString),
-        }),
-    ),
-    signature: TlvField(11, TlvByteString),
+const DefaultIssuerMatterFields: TlvFields = {
+    nodeId: TlvOptionalField(17, TlvNodeId),
+    firmwareSigningId: TlvOptionalField(18, TlvUInt32),
+    icacId: TlvOptionalField(19, TlvUInt64),
+    rcacId: TlvOptionalField(20, TlvUInt64),
+    fabricId: TlvOptionalField(21, TlvFabricId),
+    caseAuthenticatedTags: TlvOptionalRepeatedField(22, TlvCaseAuthenticatedTag, { maxLength: 3 }),
+};
+
+/**
+ * TLV schema for a generic subject or issuer field in a certificate. We handle all fields as optional here for the TLV
+ * parsing and check required fields in the logic to make sure we return the correct errors.
+ */
+const TlvGenericMatterSubjectOrIssuerTaggedList = <T extends TlvFields>(matterFields: T) => {
+    const fields = {
+        // Standard DNs
+        commonName: TlvOptionalField(1, TlvString),
+        sureName: TlvOptionalField(2, TlvString),
+        serialNum: TlvOptionalField(3, TlvString),
+        countryName: TlvOptionalField(4, TlvString),
+        localityName: TlvOptionalField(5, TlvString),
+        stateOrProvinceName: TlvOptionalField(6, TlvString),
+        orgName: TlvOptionalField(7, TlvString),
+        orgUnitName: TlvOptionalField(8, TlvString),
+        title: TlvOptionalField(9, TlvString),
+        name: TlvOptionalField(10, TlvString),
+        givenName: TlvOptionalField(11, TlvString),
+        initials: TlvOptionalField(12, TlvString),
+        genQualifier: TlvOptionalField(13, TlvString),
+        dnQualifier: TlvOptionalField(14, TlvString),
+        pseudonym: TlvOptionalField(15, TlvString),
+        domainComponent: TlvOptionalField(16, TlvString),
+
+        // Matter specific DNs
+        ...matterFields,
+
+        // Standard DNs when encoded as Printable String
+        commonNamePs: TlvOptionalField(129, TlvString),
+        sureNamePs: TlvOptionalField(130, TlvString),
+        serialNumPs: TlvOptionalField(131, TlvString),
+        countryNamePs: TlvOptionalField(132, TlvString),
+        localityNamePs: TlvOptionalField(133, TlvString),
+        stateOrProvinceNamePs: TlvOptionalField(134, TlvString),
+        orgNamePs: TlvOptionalField(135, TlvString),
+        orgUnitNamePs: TlvOptionalField(136, TlvString),
+        titlePs: TlvOptionalField(137, TlvString),
+        namePs: TlvOptionalField(138, TlvString),
+        givenNamePs: TlvOptionalField(139, TlvString),
+        initialsPs: TlvOptionalField(140, TlvString),
+        genQualifierPs: TlvOptionalField(141, TlvString),
+        dnQualifierPs: TlvOptionalField(142, TlvString),
+        pseudonymPs: TlvOptionalField(143, TlvString),
+    };
+    return TlvTaggedList(fields);
+};
+
+const ExtensionKeyUsageBitmap = {
+    digitalSignature: BitFlag(0),
+    nonRepudiation: BitFlag(1),
+    keyEncipherment: BitFlag(2),
+    dataEncipherment: BitFlag(3),
+    keyAgreement: BitFlag(4),
+    keyCertSign: BitFlag(5),
+    cRLSign: BitFlag(6),
+    encipherOnly: BitFlag(7),
+    decipherOnly: BitFlag(8),
+};
+const ExtensionKeyUsageSchema = BitmapSchema(ExtensionKeyUsageBitmap);
+
+const BaseMatterCertificate = (matterFields?: { subject?: TlvFields; issuer?: TlvFields }) =>
+    TlvObject({
+        serialNumber: TlvField(1, TlvByteString.bound({ maxLength: 20 })),
+        signatureAlgorithm: TlvField(2, TlvUInt8),
+        issuer: TlvField(
+            3,
+            TlvGenericMatterSubjectOrIssuerTaggedList(matterFields?.issuer ?? DefaultIssuerMatterFields),
+        ),
+        notBefore: TlvField(4, TlvUInt32),
+        notAfter: TlvField(5, TlvUInt32),
+        subject: TlvField(
+            6,
+            TlvGenericMatterSubjectOrIssuerTaggedList(matterFields?.subject ?? DefaultIssuerMatterFields),
+        ),
+        publicKeyAlgorithm: TlvField(7, TlvUInt8),
+        ellipticCurveIdentifier: TlvField(8, TlvUInt8),
+        ellipticCurvePublicKey: TlvField(9, TlvByteString),
+        extensions: TlvField(
+            10,
+            TlvTaggedList({
+                basicConstraints: TlvField(
+                    1,
+                    TlvObject({
+                        isCa: TlvField(1, TlvBoolean),
+                        pathLen: TlvOptionalField(2, TlvUInt8),
+                    }),
+                ),
+                keyUsage: TlvField(2, TlvBitmap(TlvUInt16, ExtensionKeyUsageBitmap)),
+                extendedKeyUsage: TlvOptionalField(3, TlvArray(TlvUInt8)),
+                subjectKeyIdentifier: TlvField(4, TlvByteString.bound({ length: 20 })),
+                authorityKeyIdentifier: TlvField(5, TlvByteString.bound({ length: 20 })),
+                futureExtension: TlvOptionalField(6, TlvByteString),
+            }),
+        ),
+        signature: TlvField(11, TlvByteString),
+    });
+
+export const TlvRootCertificate = BaseMatterCertificate({
+    subject: {
+        rcacId: TlvField(20, TlvUInt64),
+        fabricId: TlvOptionalField(21, TlvFabricId),
+    },
 });
 
-export const TlvOperationalCertificate = TlvObject({
-    serialNumber: TlvField(1, TlvByteString.bound({ maxLength: 20 })),
-    signatureAlgorithm: TlvField(2, TlvUInt8),
-    issuer: TlvField(
-        3,
-        TlvTaggedList({
-            issuerRcacId: TlvOptionalField(20, TlvUInt64),
-        }),
-    ),
-    notBefore: TlvField(4, TlvUInt32),
-    notAfter: TlvField(5, TlvUInt32),
-    subject: TlvField(
-        6,
-        TlvTaggedList({
-            fabricId: TlvField(21, TlvFabricId),
-            nodeId: TlvField(17, TlvNodeId),
-            caseAuthenticatedTags: TlvOptionalRepeatedField(22, TlvCaseAuthenticatedTag, { maxLength: 3 }),
-        }),
-    ),
-    publicKeyAlgorithm: TlvField(7, TlvUInt8),
-    ellipticCurveIdentifier: TlvField(8, TlvUInt8),
-    ellipticCurvePublicKey: TlvField(9, TlvByteString),
-    extensions: TlvField(
-        10,
-        TlvTaggedList({
-            basicConstraints: TlvField(
-                1,
-                TlvObject({
-                    isCa: TlvField(1, TlvBoolean),
-                    pathLen: TlvOptionalField(2, TlvUInt8),
-                }),
-            ),
-            keyUsage: TlvField(2, TlvUInt16),
-            extendedKeyUsage: TlvOptionalField(3, TlvArray(TlvUInt8)),
-            subjectKeyIdentifier: TlvField(4, TlvByteString.bound({ length: 20 })),
-            authorityKeyIdentifier: TlvField(5, TlvByteString.bound({ length: 20 })),
-            futureExtension: TlvOptionalField(6, TlvByteString),
-        }),
-    ),
-    signature: TlvField(11, TlvByteString),
+export const TlvOperationalCertificate = BaseMatterCertificate({
+    subject: {
+        nodeId: TlvField(17, TlvNodeId),
+        fabricId: TlvField(21, TlvFabricId),
+        caseAuthenticatedTags: TlvOptionalRepeatedField(22, TlvCaseAuthenticatedTag, { maxLength: 3 }),
+    },
 });
 
-// TODO: Add also ASN1 encoder and validator for intermediate certificate
-export const TlvIntermediateCertificate = TlvObject({
-    serialNumber: TlvField(1, TlvByteString.bound({ maxLength: 20 })),
-    signatureAlgorithm: TlvField(2, TlvUInt8),
-    issuer: TlvField(
-        3,
-        TlvTaggedList({
-            issuerRcacId: TlvOptionalField(20, TlvUInt64),
-        }),
-    ),
-    notBefore: TlvField(4, TlvUInt32),
-    notAfter: TlvField(5, TlvUInt32),
-    subject: TlvField(
-        6,
-        TlvTaggedList({
-            fabricId: TlvOptionalField(21, TlvFabricId),
-            icacId: TlvField(19, TlvUInt64),
-        }),
-    ),
-    publicKeyAlgorithm: TlvField(7, TlvUInt8),
-    ellipticCurveIdentifier: TlvField(8, TlvUInt8),
-    ellipticCurvePublicKey: TlvField(9, TlvByteString),
-    extensions: TlvField(
-        10,
-        TlvTaggedList({
-            basicConstraints: TlvField(
-                1,
-                TlvObject({
-                    isCa: TlvField(1, TlvBoolean),
-                }),
-            ),
-            keyUsage: TlvField(2, TlvUInt16),
-            subjectKeyIdentifier: TlvField(4, TlvByteString.bound({ length: 20 })),
-            authorityKeyIdentifier: TlvField(5, TlvByteString.bound({ length: 20 })),
-        }),
-    ),
-    signature: TlvField(11, TlvByteString),
+export const TlvIntermediateCertificate = BaseMatterCertificate({
+    subject: {
+        icacId: TlvField(19, TlvUInt64),
+        fabricId: TlvOptionalField(21, TlvFabricId),
+    },
 });
 
-export interface DeviceAttestationCertificate {
+const TlvBaseCertificate = BaseMatterCertificate();
+
+interface AttestationCertificateBase {
     serialNumber: ByteArray;
     signatureAlgorithm: number;
+    issuer: {};
+    notBefore: number;
+    notAfter: number;
+    subject: {};
+    publicKeyAlgorithm: number;
+    ellipticCurveIdentifier: number;
+    ellipticCurvePublicKey: ByteArray;
+    extensions: {
+        basicConstraints: {
+            isCa: boolean;
+            pathLen?: number;
+        };
+        keyUsage: TypeFromPartialBitSchema<typeof ExtensionKeyUsageBitmap>;
+        extendedKeyUsage?: number[];
+        subjectKeyIdentifier: ByteArray;
+        authorityKeyIdentifier: ByteArray;
+        futureExtension?: ByteArray;
+    };
+    signature: ByteArray;
+}
+
+export interface DeviceAttestationCertificate extends AttestationCertificateBase {
     issuer: {
         commonName: string;
         productId?: number;
         vendorId: VendorId;
     };
-    notBefore: number;
-    notAfter: number;
     subject: {
         commonName: string;
         productId: number;
         vendorId: VendorId;
     };
-    publicKeyAlgorithm: number;
-    ellipticCurveIdentifier: number;
-    ellipticCurvePublicKey: ByteArray;
-    extensions: {
-        basicConstraints: {
-            isCa: boolean;
-            pathLen?: number;
-        };
-        keyUsage: number;
-        extendedKeyUsage?: number[];
-        subjectKeyIdentifier: ByteArray;
-        authorityKeyIdentifier: ByteArray;
-        futureExtension?: ByteArray;
-    };
-    signature: ByteArray;
 }
 
-export interface ProductAttestationIntermediateCertificate {
-    serialNumber: ByteArray;
-    signatureAlgorithm: number;
+export interface ProductAttestationIntermediateCertificate extends AttestationCertificateBase {
     issuer: {
         commonName: string;
         vendorId?: VendorId;
     };
-    notBefore: number;
-    notAfter: number;
     subject: {
         commonName: string;
         productId?: number;
         vendorId: VendorId;
     };
-    publicKeyAlgorithm: number;
-    ellipticCurveIdentifier: number;
-    ellipticCurvePublicKey: ByteArray;
-    extensions: {
-        basicConstraints: {
-            isCa: boolean;
-            pathLen?: number;
-        };
-        keyUsage: number;
-        extendedKeyUsage?: number[];
-        subjectKeyIdentifier: ByteArray;
-        authorityKeyIdentifier: ByteArray;
-        futureExtension?: ByteArray;
-    };
-    signature: ByteArray;
 }
 
-export interface ProductAttestationAuthorityCertificate {
-    serialNumber: ByteArray;
-    signatureAlgorithm: number;
+export interface ProductAttestationAuthorityCertificate extends AttestationCertificateBase {
     issuer: {
         commonName: string;
         vendorId?: VendorId;
     };
-    notBefore: number;
-    notAfter: number;
     subject: {
         commonName: string;
         vendorId?: VendorId;
     };
-    publicKeyAlgorithm: number;
-    ellipticCurveIdentifier: number;
-    ellipticCurvePublicKey: ByteArray;
-    extensions: {
-        basicConstraints: {
-            isCa: boolean;
-            pathLen?: number;
-        };
-        keyUsage: number;
-        extendedKeyUsage?: number[];
-        subjectKeyIdentifier: ByteArray;
-        authorityKeyIdentifier?: ByteArray;
-        futureExtension?: ByteArray;
-    };
-    signature: ByteArray;
 }
 
 export const TlvCertificationDeclaration = TlvObject({
@@ -364,6 +340,7 @@ export const TlvCertificationDeclaration = TlvObject({
     ),
 });
 
+export type BaseCertificate = TypeFromSchema<typeof TlvBaseCertificate>;
 export type RootCertificate = TypeFromSchema<typeof TlvRootCertificate>;
 export type IntermediateCertificate = TypeFromSchema<typeof TlvIntermediateCertificate>;
 export type OperationalCertificate = TypeFromSchema<typeof TlvOperationalCertificate>;
