413ef3c1c0f5a5bc312a12a276f2536d62e4930c
Ingo Fischer <github@fischer-ka.de>
2024-04-26 20:39:26 +0200

    Enhance WindowCovering overrides
    * adds constrain checks for LiftValue and TiltValue
    * adds feature specific conformance information to Type/´and EndProductType attributes as per 1.2 specs

--- models/src/local/WindowCoveringOverrides.ts
+++ models/src/local/WindowCoveringOverrides.ts
@@ -76,6 +76,62 @@ LocalMatter.children.push(
                 ],
             },
 
+            // Add Conformance field to the Type attribute
+            {
+                tag: "attribute",
+                id: 0,
+                name: "Type",
+                children: [
+                    { tag: "field", name: "Rollershade", id: 0, conformance: "LF & !TL" },
+                    { tag: "field", name: "Rollershade2Motor", id: 1, conformance: "LF & !TL" },
+                    { tag: "field", name: "RollershadeExterior", id: 2, conformance: "LF & !TL" },
+                    { tag: "field", name: "RollershadeExterior2Motor", id: 3, conformance: "LF & !TL" },
+                    { tag: "field", name: "Drapery", id: 4, conformance: "LF & !TL" },
+                    { tag: "field", name: "Awning", id: 5, conformance: "LF & !TL" },
+                    // see https://github.com/project-chip/connectedhomeip/issues/33135
+                    { tag: "field", name: "Shutter", id: 6, conformance: "LF | TL" },
+                    { tag: "field", name: "TiltBlindTiltOnly", id: 7, conformance: "!LF & TL" },
+                    { tag: "field", name: "TiltBlindLift", id: 8, conformance: "LF & TL" },
+                    { tag: "field", name: "ProjectorScreen", id: 9, conformance: "LF & !TL" },
+                    { tag: "field", name: "Unknown", id: 255, conformance: "O" },
+                ],
+            },
+
+            // Add Conformance field to the EndProductType attribute
+            // Unclear if "LF" actually means "LF &!TL" and such
+            {
+                tag: "attribute",
+                id: 13,
+                name: "EndProductType",
+                children: [
+                    { tag: "field", name: "RollerShade", id: 0, conformance: "LF" },
+                    { tag: "field", name: "RomanShade", id: 1, conformance: "LF" },
+                    { tag: "field", name: "BalloonShade", id: 2, conformance: "LF" },
+                    { tag: "field", name: "WovenWood", id: 3, conformance: "LF" },
+                    { tag: "field", name: "PleatedShade", id: 4, conformance: "LF" },
+                    { tag: "field", name: "CellularShade", id: 5, conformance: "LF" },
+                    { tag: "field", name: "LayeredShade", id: 6, conformance: "LF" },
+                    { tag: "field", name: "LayeredShade2D", id: 7, conformance: "LF" },
+                    { tag: "field", name: "SheerShade", id: 8, conformance: "LF & TL" },
+                    { tag: "field", name: "TiltOnlyInteriorBlind", id: 9, conformance: "TL" },
+                    { tag: "field", name: "InteriorBlind", id: 10, conformance: "LF & TL" },
+                    { tag: "field", name: "VerticalBlindStripCurtain", id: 11, conformance: "LF & TL" },
+                    { tag: "field", name: "InteriorVenetianBlind", id: 12, conformance: "LF & TL" },
+                    { tag: "field", name: "ExteriorVenetianBlind", id: 13, conformance: "LF & TL" },
+                    { tag: "field", name: "LateralLeftCurtain", id: 14, conformance: "LF" },
+                    { tag: "field", name: "LateralRightCurtain", id: 15, conformance: "LF" },
+                    { tag: "field", name: "CentralCurtain", id: 16, conformance: "LF" },
+                    { tag: "field", name: "RollerShutter", id: 17, conformance: "LF" },
+                    { tag: "field", name: "ExteriorVerticalScreen", id: 18, conformance: "LF" },
+                    { tag: "field", name: "AwningTerrace", id: 19, conformance: "LF" },
+                    { tag: "field", name: "AwningVerticalScreen", id: 20, conformance: "LF" },
+                    { tag: "field", name: "TiltOnlyPergola", id: 21, conformance: "LF | TL" },
+                    { tag: "field", name: "SwingingShutter", id: 22, conformance: "LF | TL" },
+                    { tag: "field", name: "SlidingShutter", id: 23, conformance: "LF | TL" },
+                    { tag: "field", name: "Unknown", id: 255, conformance: "O" },
+                ],
+            },
+
             // Set the name, type and description of the OperationalStatus config
             // fields.  Otherwise name would be a placeholder, type would be a bare
             // number and the description would be overly-verbose bit-level
@@ -110,6 +166,24 @@ LocalMatter.children.push(
                 ],
             },
 
+            // Adjust the constraints for the GoToLiftValue command parameter to allow automatic validation
+            {
+                tag: "command",
+                name: "GoToLiftValue",
+                id: 4,
+                children: [
+                    {
+                        tag: "field",
+                        name: "LiftValue",
+                        id: 0,
+                        type: "uint16",
+                        conformance: "M",
+                        constraint: "InstalledOpenLimitLift to InstalledClosedLimitLift",
+                    },
+                ],
+            },
+
+            // Fix a Spec bug that listed two parameters but only one is relevant
             {
                 tag: "command",
                 name: "GoToLiftPercentage",
@@ -126,6 +200,25 @@ LocalMatter.children.push(
                     },
                 ],
             },
+
+            // Adjust the constraints for the GoToTiltValue command parameter to allow automatic validation
+            {
+                tag: "command",
+                name: "GoToTiltValue",
+                id: 7,
+                children: [
+                    {
+                        tag: "field",
+                        name: "TiltValue",
+                        id: 0,
+                        type: "uint16",
+                        conformance: "M",
+                        constraint: "InstalledOpenLimitTilt to InstalledClosedLimitTilt",
+                    },
+                ],
+            },
+
+            // Fix a Spec bug that listed two parameters but only one is relevant
             {
                 tag: "command",
                 name: "GoToTiltPercentage",
