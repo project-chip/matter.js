94d2b5aede72b6c99562456939932ddee58579cd
Ingo Fischer <github@fischer-ka.de>
2024-06-14 17:05:10 +0200

    Add "react-native-ble" as Ble wrapper

---
+++ packages/matter.js-react-native-ble/src/ble/BleReactNative.ts
@@ -0,0 +1,42 @@
+/**
+ * @license
+ * Copyright 2022-2024 Matter.js Authors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import { Ble } from "@project-chip/matter.js/ble";
+import { ImplementationError, InstanceBroadcaster, Scanner, TransportInterface } from "@project-chip/matter.js/common";
+import { NetInterface } from "@project-chip/matter.js/net";
+import { BleScanner } from "./BleScanner.js";
+import { NobleBleCentralInterface } from "./ReactNativeBleChannel.js";
+import { ReactNativeBleClient } from "./ReactNativeBleClient.js";
+
+export class BleReactNative extends Ble {
+    private bleCentral: ReactNativeBleClient | undefined;
+
+    constructor() {
+        super();
+    }
+
+    getBleCentralInterface(): NetInterface {
+        if (this.bleCentral === undefined) {
+            this.bleCentral = new ReactNativeBleClient();
+        }
+        return new NobleBleCentralInterface();
+    }
+
+    getBleScanner(): Scanner {
+        if (this.bleCentral === undefined) {
+            this.bleCentral = new ReactNativeBleClient();
+        }
+        return new BleScanner(this.bleCentral);
+    }
+
+    getBlePeripheralInterface(): TransportInterface {
+        throw new ImplementationError("React Native can only act as a central device, not a peripheral.");
+    }
+
+    getBleBroadcaster(): InstanceBroadcaster {
+        throw new ImplementationError("React Native can only act as a central device, not a broadcaster.");
+    }
+}
