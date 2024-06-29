ebdcb9ceff4883a10e1325c02d469dceb05f9fda
Ingo Fischer <github@fischer-ka.de>
2024-06-29 12:43:02 +0200

    Fix prettifier

--- packages/matter.js/src/model/logic/index.ts
+++ packages/matter.js/src/model/logic/index.ts
@@ -4,14 +4,14 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-export * from "./ClusterVariance.js";
-export * from "./DefaultValue.js";
-export * from "./MergedModel.js";
-export * from "./ModelVariantTraversal.js";
-export * from "./ValidateModel.js";
 export * from "./cluster-variance/FeatureBitmap.js";
 export * from "./cluster-variance/IllegalFeatureCombinations.js";
 export * from "./cluster-variance/InferredComponents.js";
 export * from "./cluster-variance/NamedComponents.js";
 export * from "./cluster-variance/VarianceCondition.js";
+export * from "./ClusterVariance.js";
+export * from "./DefaultValue.js";
+export * from "./MergedModel.js";
+export * from "./ModelVariantTraversal.js";
+export * from "./ValidateModel.js";
 import "./definition-validation/index.js";
--- packages/matter.js/src/session/export.ts
+++ packages/matter.js/src/session/export.ts
@@ -12,10 +12,10 @@ export * from "./case/CaseMessenger.js";
 export * from "./case/CaseServer.js";
 // Export PaseSession classes
 export * from "./InsecureSession.js";
-export * from "./SecureSession.js";
-export * from "./Session.js";
-export * from "./SessionManager.js";
 export * from "./pase/PaseClient.js";
 export * from "./pase/PaseMessages.js";
 export * from "./pase/PaseMessenger.js";
 export * from "./pase/PaseServer.js";
+export * from "./SecureSession.js";
+export * from "./Session.js";
+export * from "./SessionManager.js";
