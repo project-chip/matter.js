4b773c5ad9992f5a78e3d3e130443b049c9b6f6c
Dan Cunningham <dan@digitaldan.com>
2024-08-23 14:54:18 -0700

    Use existing options, set ignoreInitialTriggers to false
    Signed-off-by: Dan Cunningham <dan@digitaldan.com>

--- packages/matter.js/src/device/PairedNode.ts
+++ packages/matter.js/src/device/PairedNode.ts
@@ -374,12 +374,7 @@ export class PairedNode {
             updateTimeoutHandler: async () => {
                 logger.info(`Node ${this.nodeId}: Subscription update not received ...`);
                 try {
-                    const { attributeChangedCallback, eventTriggeredCallback } = this.options;
-                    await this.subscribeAllAttributesAndEvents({
-                        ignoreInitialTriggers: true,
-                        attributeChangedCallback: data => attributeChangedCallback?.(this.nodeId, data),
-                        eventTriggeredCallback: data => eventTriggeredCallback?.(this.nodeId, data),
-                    });
+                    await this.subscribeAllAttributesAndEvents({ ...options, ignoreInitialTriggers: false });
                 } catch (error) {
                     logger.info(`Node ${this.nodeId}: Error resubscribing to all attributes and events`, error);
                     // TODO resume logic right now retries and discovers for 60s .. prolong this but without command repeating
