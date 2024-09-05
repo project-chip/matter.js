e8fe2ba5236e8bc6cc7fd9dad0c855633543cc44
Ingo Fischer <github@fischer-ka.de>
2024-09-05 20:45:10 +0200

    NoResponse is also a ResubmissionError
    (just from the other side, but we can handle comparable)

--- packages/matter.js/src/util/Stream.ts
+++ packages/matter.js/src/util/Stream.ts
@@ -4,9 +4,10 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 import { MatterError } from "../common/MatterError.js";
+import { RetransmissionLimitReachedError } from "../protocol/MessageExchange.js";
 
 export class EndOfStreamError extends MatterError {}
-export class NoResponseTimeoutError extends MatterError {}
+export class NoResponseTimeoutError extends RetransmissionLimitReachedError {}
 
 export interface Stream<T> {
     read(): Promise<T>;
