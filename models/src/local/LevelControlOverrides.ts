39b23c9c6b58b8a19fd126ba1b7e070c61575c76
Ingo Fischer <github@fischer-ka.de>
2024-11-07 11:27:02 +0100

    Add model override for MoveToLevel

--- models/src/local/LevelControlOverrides.ts
+++ models/src/local/LevelControlOverrides.ts
@@ -11,10 +11,22 @@ LocalMatter.children.push({
     name: "LevelControl",
 
     children: [
-        // Docmentation for "on/off" variants is in prose.  They have the same structure as non-on/off commands
+        // Documentation for "on/off" variants is in prose.  They have the same structure as non-on/off commands
         { tag: "command", id: 4, name: "MoveToLevelWithOnOff", type: "MoveToLevel" },
         { tag: "command", id: 5, name: "MoveWithOnOff", type: "Move" },
         { tag: "command", id: 6, name: "StepWithOnOff", type: "Step" },
         { tag: "command", id: 7, name: "StopWithOnOff", type: "Stop" },
+
+        // Temporarily declare OptionsMask as optional fields because current Google Firmwares do not send them.
+        // Will be adjusted back once fixed by Google
+        {
+            tag: "command",
+            id: 0,
+            name: "MoveToLevel",
+            children: [
+                { tag: "field", id: 2, name: "OptionsMask", conformance: "O" },
+                { tag: "field", id: 3, name: "OptionsOverride", conformance: "O" },
+            ],
+        },
     ],
 });
