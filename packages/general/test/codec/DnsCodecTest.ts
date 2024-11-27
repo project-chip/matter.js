8fcea5703ee8f5ba01dc3967f501718b2220e5c9
Ingo Fischer <github@fischer-ka.de>
2024-11-27 11:36:33 +0100

    QName tests

--- packages/general/test/codec/DnsCodecTest.ts
+++ packages/general/test/codec/DnsCodecTest.ts
@@ -14,7 +14,8 @@ import {
     SrvRecord,
     TxtRecord,
 } from "#codec/DnsCodec.js";
-import { Bytes } from "#util/Bytes.js";
+import { Bytes, Endian } from "#util/Bytes.js";
+import { DataReader } from "#util/index.js";
 
 const DNS_RESPONSE: DnsMessage = {
     transactionId: 0,
@@ -87,6 +88,71 @@ const DNS_DECODED = {
     ],
 };
 
+const QNameTestData: { testCase: string; data: string; offset?: number; result?: string; error?: string }[] = [
+    { testCase: "one item", data: "\x04test\x00", result: "test" },
+    { testCase: "two items", data: "\x04test\x03com\x00", result: "test.com" },
+    { testCase: "multiple items", data: "\x04this\x02is\x01a\x04test\x00", result: "this.is.a.test" },
+    {
+        testCase: "multiple items with pointers",
+        data: "\x03abc\x02is\x01a\x04test\x00\x04this\xc0\x04",
+        offset: 15,
+        result: "this.is.a.test",
+    },
+    {
+        testCase: "multiple items with pointers and loop",
+        data: "\x04this\xc0\x00\x03abc\x02is\x01a\x04test\xc0\x00",
+        offset: 7,
+        error: "QNAME pointer loop detected. Index 0 visited twice.",
+    },
+    {
+        testCase: "multiple items with pointers and loop",
+        data: "\x04this\xc0\x05\x03abc\x02is\x01a\x04test\xc0\x00",
+        offset: 7,
+        error: "QNAME pointer loop detected. Index 5 visited twice.",
+    },
+    {
+        testCase: "data truncated before the end #1",
+        data: "\x04test",
+        error: "QNAME item length 4 exceeds remaining bytes.",
+    },
+    {
+        testCase: "data truncated before the end #2",
+        data: "\x02",
+        error: "QNAME item length 2 exceeds remaining bytes.",
+    },
+    {
+        testCase: "data truncated before the end #3",
+        data: "\xc0",
+        error: "QNAME pointer exceeds remaining bytes.",
+    },
+    {
+        testCase: "Reference to 0 byte length string", // Truncated before the end (but seemingly valid in case of error)
+        data: "\x00\xc0\x00",
+        offset: 1,
+        result: undefined,
+    },
+    {
+        testCase: "Infinite recursion",
+        data: "\x04test\xc0\x00",
+        error: "QNAME pointer loop detected. Index 0 visited twice.",
+    },
+    {
+        testCase: "Infinite recursion by referencing own element (inside the stream)",
+        data: "\x04test\xc0\x05",
+        error: "QNAME pointer loop detected. Index 5 visited twice.",
+    },
+    {
+        testCase: "Infinite recursion by referencing own element at the start",
+        data: "\xc0\x00",
+        error: "QNAME pointer loop detected. Index 0 visited twice.",
+    },
+    {
+        testCase: "Reference that goes forward instead of backward",
+        data: "\x04test\xc0\x07",
+        error: "Invalid compressed QNAME pointer pointing to out of bounds index.",
+    },
+];
+
 describe("DnsCodec", () => {
     describe("decode", () => {
         it("decodes a message", () => {
@@ -118,5 +184,23 @@ describe("DnsCodec", () => {
 
             expect(decoded).deep.equal(DNS_DECODED);
         });
+    });
+
+    describe("decode QName", () => {
+        for (const { testCase, data, offset, result, error } of QNameTestData) {
+            it(`decodes ${testCase}`, () => {
+                const dataBytes = Uint8Array.from(data, x => x.charCodeAt(0));
+                const reader = new DataReader(dataBytes, Endian.Big);
+                if (offset !== undefined) {
+                    reader.offset = offset;
+                }
+                if (result !== undefined) {
+                    const decoded = DnsCodec.decodeQName(reader, dataBytes);
+                    expect(decoded).equal(result);
+                } else if (error !== undefined) {
+                    expect(() => DnsCodec.decodeQName(reader, dataBytes)).to.throw(error);
+                }
+            });
+        }
     });
 });
