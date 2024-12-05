0a25ae10ce0f3b37d64bd1a24c9ec52c9ec5bec3
Ingo Fischer <github@fischer-ka.de>
2024-12-05 21:16:35 +0100

    Adjust Pump Config override
    in Matter 1.3 ist is ID 0x22 and not 0x1d ... so fix that

--- models/src/local/PumpConfigurationAndControlOverrides.ts
+++ models/src/local/PumpConfigurationAndControlOverrides.ts
@@ -11,34 +11,11 @@ LocalMatter.children.push({
     name: "PumpConfigurationAndControl",
 
     children: [
-        // The spec only defines long-form names for these bits in a non-standard table we don't bother parsing because
-        // it contains nothing else useful.
-        //
-        // This attribute is actually marked as Zigbee-only so we treat as deprecated.  So we may not even need these
         {
             tag: "attribute",
-            id: 0x1d,
+            id: 0x22,
+            type: "uint16", // matches to map16
             name: "AlarmMask",
-            children: [
-                {
-                    tag: "field",
-                    constraint: "0",
-                    name: "InitializationFailure",
-                    details: "The device failed to complete initialization at powerup",
-                },
-                {
-                    tag: "field",
-                    constraint: "1",
-                    name: "HardwareFailure",
-                    details: "The device has detected a hardware fault",
-                },
-                {
-                    tag: "field",
-                    constraint: "2",
-                    name: "SelfCalibrationFailure",
-                    details: "The device was unable to complete self calibration",
-                },
-            ],
         },
     ],
 });
