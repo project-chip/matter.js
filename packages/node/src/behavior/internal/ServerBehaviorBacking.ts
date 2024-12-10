dc561311bf28b5cd073dd67fed15be823983d8c8
Greg Lauckhart <greg@lauckhart.com>
2024-12-10 14:14:02 -0800

    Extract protocol init from node init
    Fixes a mistake in the implementation of node initialization.
    
    Before this commit, ClusterBehaviors were backed by a backing that managed the cluster server.  This intermingled protocol setup with node initialization.
    
    Now there is still a ServerBacking but creation of ClusterServers is handled independently when the node is brought online.
    
    Primary motivation for fixing this now is that it allows for behaviors to independently contribute components used by the network runtime, which we will use in future commits.

--- packages/node/src/behavior/internal/ServerBehaviorBacking.ts
+++ packages/node/src/behavior/internal/ServerBehaviorBacking.ts
@@ -4,19 +4,27 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
+import { ClusterBehavior } from "#behavior/cluster/ClusterBehavior.js";
+import { GlobalAttributeState } from "#behavior/cluster/ClusterState.js";
+import { ValidatedElements } from "#behavior/cluster/ValidatedElements.js";
+import type { SupportedElements } from "#endpoint/index.js";
 import { camelize } from "#general";
 import { FieldValue } from "#model";
 import { ServerNodeStore } from "#node/storage/ServerNodeStore.js";
+import { ClusterType, TlvNoResponse } from "#types";
 import { Behavior } from "../Behavior.js";
 import { Val } from "../state/Val.js";
 import { Datasource } from "../state/managed/Datasource.js";
 import { BehaviorBacking } from "./BehaviorBacking.js";
+
+const NoElements = new Set<string>();
 
 /**
  * This class backs the server implementation of a behavior.
  */
 export class ServerBehaviorBacking extends BehaviorBacking {
     #store?: Datasource.Store;
+    #elements?: SupportedElements;
 
     override get store() {
         if (!this.#store) {
@@ -27,9 +35,23 @@ export class ServerBehaviorBacking extends BehaviorBacking {
         return this.#store;
     }
 
+    get elements() {
+        return this.#elements;
+    }
+
     protected override invokeInitializer(behavior: Behavior, options?: Behavior.Options) {
         const finalizeState = () => {
             this.#applyTransitiveDefaults(behavior.state);
+
+            if (behavior instanceof ClusterBehavior) {
+                this.#configureElements(behavior);
+            } else {
+                this.#elements = {
+                    attributes: NoElements,
+                    commands: NoElements,
+                    events: NoElements,
+                };
+            }
 
             // State must now conform to the schema.  Validate the behavior's state rather than internal state
             // because the behavior likely has uncommitted changes
@@ -71,5 +93,34 @@ export class ServerBehaviorBacking extends BehaviorBacking {
                 }
             }
         }
+    }
+
+    #configureElements(behavior: ClusterBehavior) {
+        // Validate
+        const validation = new ValidatedElements(behavior.constructor as ClusterBehavior.Type, behavior);
+        validation.report();
+
+        const globals = behavior.state as GlobalAttributeState;
+
+        // Update attribute list
+        const attributeDefs = behavior.cluster.attributes as ClusterType.ElementSet<ClusterType.Attribute>;
+        globals.attributeList = [...validation.attributes].map(name => attributeDefs[name].id);
+
+        // Update accepted & generated command lists
+        const commandDefs = behavior.cluster.commands as ClusterType.ElementSet<ClusterType.Command>;
+        const commands = [...validation.commands].map(name => commandDefs[name]);
+        globals.acceptedCommandList = commands.map(command => command.requestId);
+        globals.generatedCommandList = [
+            ...new Set(
+                commands.filter(command => command.responseSchema !== TlvNoResponse).map(command => command.responseId),
+            ),
+        ];
+
+        // Load public API
+        this.#elements = {
+            attributes: validation.attributes,
+            commands: validation.commands,
+            events: validation.events,
+        };
     }
 }
