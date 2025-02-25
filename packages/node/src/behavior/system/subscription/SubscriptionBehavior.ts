27d11fadb0b83ce4be5c952057cd03f9097967d7
Ingo Fischer <github@fischer-ka.de>
2025-02-25 23:28:23 +0100

    Reduce async-ness
    ... only when needed

--- packages/node/src/behavior/system/subscription/SubscriptionBehavior.ts
+++ packages/node/src/behavior/system/subscription/SubscriptionBehavior.ts
@@ -4,7 +4,7 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-import { deepCopy, isIpNetworkChannel, Logger, MatterError, ServerAddressIp } from "#general";
+import { deepCopy, isIpNetworkChannel, Logger, MatterError, MaybePromise, ServerAddressIp } from "#general";
 import { DatatypeModel, FieldElement } from "#model";
 import { TransactionalInteractionServer } from "#node/index.js";
 import {
@@ -177,17 +177,21 @@ export class SubscriptionBehavior extends Behavior {
         this.state.subscriptions.push(peerSubscription);
     }
 
-    async #subscriptionCancelled(subscription: Subscription) {
+    #subscriptionCancelled(subscription: Subscription): MaybePromise {
         if (subscription.isCanceledByPeer) {
             if (this.state.persistenceEnabled === false) return;
             const { id } = subscription;
             const subscriptionIndex = this.state.subscriptions.findIndex(({ subscriptionId }) => id === subscriptionId);
             if (subscriptionIndex !== -1) {
-                await this.context.transaction.begin();
-                this.state.subscriptions.splice(subscriptionIndex, 1);
-                await this.context.transaction.commit();
+                return this.#removeSubscriptionIndex(subscriptionIndex);
             }
         }
+    }
+
+    async #removeSubscriptionIndex(index: number) {
+        await this.context.transaction.begin();
+        this.state.subscriptions.splice(index, 1);
+        await this.context.transaction.commit();
     }
 
     async reestablishFormerSubscriptions(interactionServer: TransactionalInteractionServer) {
