c4771a8694b466ec34143bd1f1a04f4c6513a927
Ingo Fischer <github@fischer-ka.de>
2025-03-21 09:30:31 +0100

    Add Quiter guards for PowerSource cluster

--- packages/node/src/behaviors/power-source/PowerSourceServer.ts
+++ packages/node/src/behaviors/power-source/PowerSourceServer.ts
@@ -5,13 +5,38 @@
  */
 
 import { DescriptorServer } from "#behaviors/descriptor";
+import { ClusterType } from "@matter/types";
+import { PowerSource } from "@matter/types/clusters/power-source";
 import { PowerSourceBehavior } from "./PowerSourceBehavior.js";
+
+const PowerSourceLevelBase = PowerSourceBehavior.with(PowerSource.Feature.Battery, PowerSource.Feature.Rechargeable);
 
 /**
  * This is the default server implementation of {@link PowerSourceBehavior}.
  */
-export class PowerSourceServer extends PowerSourceBehavior {
+export class PowerSourceServerLogic extends PowerSourceLevelBase {
     override async initialize() {
         (await this.agent.load(DescriptorServer)).addDeviceTypes("PowerSource");
+
+        // According to specs changes to these attributes should not occur more often than every 10 seconds
+        if (this.events.batPercentRemaining$Changed !== undefined) {
+            this.events.batPercentRemaining$Changed.quiet.config = {
+                minimumEmitIntervalMs: 10_000,
+            };
+        }
+        if (this.events.batTimeRemaining$Changed !== undefined) {
+            this.events.batTimeRemaining$Changed.quiet.config = {
+                minimumEmitIntervalMs: 10_000,
+            };
+        }
+        if (this.events.batTimeToFullCharge$Changed !== undefined) {
+            this.events.batTimeToFullCharge$Changed.quiet.config = {
+                minimumEmitIntervalMs: 10_000,
+            };
+        }
     }
 }
+
+// We had turned on some more features to provide the default implementation, but export the cluster with default
+// Features again.
+export class PowerSourceServer extends PowerSourceServerLogic.for(ClusterType(PowerSource.Base)) {}
