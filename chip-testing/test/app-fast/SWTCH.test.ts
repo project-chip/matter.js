7a49a631f42c6caad494600f4702a09e8e68624c
Ingo Fischer <github@fischer-ka.de>
2025-04-04 23:07:33 +0200

    Add info for edit

--- chip-testing/test/app-fast/SWTCH.test.ts
+++ chip-testing/test/app-fast/SWTCH.test.ts
@@ -10,6 +10,8 @@ describe("SWTCH", () => {
     // We need to patch the test a bit
     before(async () => {
         await chip.testFor("SWTCH-run3").edit(
+            // Test steps are overlapping here currently, so add a reset in between
+            // Remove when https://github.com/project-chip/connectedhomeip/pull/38262 was merged
             edit.lines({
                 after: / {8}self.step\("7b"\)/,
                 before: / */,
