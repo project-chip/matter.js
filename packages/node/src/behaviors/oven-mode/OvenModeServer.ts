b331df03e2eade54bea663d3d42d3af878bc2107
Ingo Fischer <github@fischer-ka.de>
2025-04-05 12:28:37 +0200

    Validations for all *Mode clusters

--- packages/node/src/behaviors/oven-mode/OvenModeServer.ts
+++ packages/node/src/behaviors/oven-mode/OvenModeServer.ts
@@ -4,11 +4,29 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-/*** THIS FILE WILL BE REGENERATED IF YOU DO NOT REMOVE THIS MESSAGE ***/
-
+import { ModeBaseUtils } from "#behaviors/mode-base";
+import { ModeBase } from "#clusters/mode-base";
 import { OvenModeBehavior } from "./OvenModeBehavior.js";
 
 /**
  * This is the default server implementation of {@link OvenModeBehavior}.
  */
-export class OvenModeServer extends OvenModeBehavior {}
+export class OvenModeServer extends OvenModeBehavior {
+    override initialize() {
+        ModeBaseUtils.assertSupportedModes(this.state.supportedModes);
+        ModeBaseUtils.assertMode(this.state.supportedModes, this.state.currentMode);
+        this.reactTo(this.events.currentMode$Changing, this.#assertMode);
+    }
+
+    #assertMode(newMode: number) {
+        ModeBaseUtils.assertMode(this.state.supportedModes, newMode);
+    }
+
+    override changeToMode({ newMode }: ModeBase.ChangeToModeRequest): ModeBase.ChangeToModeResponse {
+        const result = ModeBaseUtils.assertModeChange(this.state.supportedModes, this.state.currentMode, newMode);
+        if (result.status === ModeBase.ModeChangeStatus.Success) {
+            this.state.currentMode = newMode;
+        }
+        return result;
+    }
+}
