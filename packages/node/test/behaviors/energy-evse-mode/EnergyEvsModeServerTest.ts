4d1ba94bb21d010576e3e11561f9f51c86bec640
lauckhart <greg@lauckhart.com>
2025-04-09 23:51:53 -0700

    Fix for command handling in derived clusters (#1885)
    * ClusterModel properties for attributes, commands, events and datatypes now include all members in scope
    
    * CommandModel direction helpers isRequest and isResponse now use effective (scoped) direction
    
    * Regenerate clusters and endpoints
    
    * (unrelated) Group vscode launch configs

--- packages/node/test/behaviors/energy-evse-mode/EnergyEvsModeServerTest.ts
+++ packages/node/test/behaviors/energy-evse-mode/EnergyEvsModeServerTest.ts
@@ -7,38 +7,64 @@
 import { EnergyEvseModeServer } from "#behaviors/energy-evse-mode";
 import { EnergyEvseMode } from "#clusters/energy-evse-mode";
 import { OnOffPlugInUnitDevice } from "#devices/on-off-plug-in-unit";
+import { Endpoint } from "#endpoint/Endpoint.js";
+import { NotImplementedError } from "#general";
 import { MockServerNode } from "../../node/mock-server-node.js";
 
-describe("EnergyEvseModeServer", () => {
-    it("accepts local mode tags", async () => {
-        const node = await MockServerNode.create();
-        const DeviceType = OnOffPlugInUnitDevice.with(EnergyEvseModeServer);
-        await node.add(DeviceType, {
-            energyEvseMode: {
-                currentMode: 0,
-                supportedModes: [
+const DeviceType = OnOffPlugInUnitDevice.with(EnergyEvseModeServer);
+
+async function createNode(options?: Endpoint.Options<typeof DeviceType>) {
+    const node = await MockServerNode.create();
+    if (!options) {
+        options = {};
+    }
+    const energyEvseMode = options.energyEvseMode ?? {};
+    options.energyEvseMode = {
+        currentMode: 0,
+        supportedModes: [
+            {
+                label: "Manual",
+                mode: 0,
+                modeTags: [
                     {
-                        label: "Manual",
-                        mode: 0,
-                        modeTags: [
-                            {
-                                value: EnergyEvseMode.ModeTag.Manual,
-                            },
-                        ],
-                    },
-                    {
-                        label: "Quick",
-                        mode: 1,
-                        modeTags: [
-                            {
-                                value: EnergyEvseMode.ModeTag.TimeOfUse,
-                            },
-                        ],
+                        value: EnergyEvseMode.ModeTag.Manual,
                     },
                 ],
             },
-        });
+            {
+                label: "Quick",
+                mode: 1,
+                modeTags: [
+                    {
+                        value: EnergyEvseMode.ModeTag.TimeOfUse,
+                    },
+                ],
+            },
+        ],
+
+        ...energyEvseMode,
+    };
+
+    const endpoint = await node.add(DeviceType, options);
+
+    return { node, endpoint };
+}
+
+describe("EnergyEvseModeServer", () => {
+    it("accepts local mode tags", async () => {
+        const { node } = await createNode();
         await node.start();
         await node.close();
+    });
+
+    it("supports inherited commands", async () => {
+        const { endpoint } = await createNode();
+        await endpoint.act(agent => {
+            const { energyEvseMode } = agent;
+            expect(typeof energyEvseMode.changeToMode === "function");
+            expect(() => energyEvseMode.changeToMode({ newMode: EnergyEvseMode.ModeTag.LowEnergy })).throws(
+                NotImplementedError,
+            );
+        });
     });
 });
--- packages/types/src/clusters/rvc-operational-state.ts
+++ packages/types/src/clusters/rvc-operational-state.ts
@@ -10,6 +10,8 @@ import { MutableCluster } from "../cluster/mutation/MutableCluster.js";
 import {
     Attribute,
     OptionalAttribute,
+    Command,
+    TlvNoResponse,
     OptionalCommand,
     Event,
     EventPriority,
@@ -219,8 +221,6 @@ export namespace RvcOperationalState {
     export interface ErrorStateStruct extends TypeFromSchema<typeof TlvErrorStateStruct> {}
 
     /**
-     * Input to the RvcOperationalState operationalCommandResponse command
-     *
      * @see {@link MatterSpecification.v14.Cluster} § 7.4.5
      */
     export const TlvOperationalCommandResponse = TlvObject({
@@ -235,8 +235,6 @@ export namespace RvcOperationalState {
     });
 
     /**
-     * Input to the RvcOperationalState operationalCommandResponse command
-     *
      * @see {@link MatterSpecification.v14.Cluster} § 7.4.5
      */
     export interface OperationalCommandResponse extends TypeFromSchema<typeof TlvOperationalCommandResponse> {}
@@ -361,6 +359,16 @@ export namespace RvcOperationalState {
         },
 
         commands: {
+            /**
+             * @see {@link MatterSpecification.v14.Cluster} § 7.4.5
+             */
+            pause: Command(0x0, TlvNoArguments, 0x0, TlvNoResponse),
+
+            /**
+             * @see {@link MatterSpecification.v14.Cluster} § 7.4.5
+             */
+            resume: Command(0x3, TlvNoArguments, 0x3, TlvNoResponse),
+
             /**
              * On receipt of this command, the device shall start seeking the charging dock, if possible in the current
              * state of the device.
