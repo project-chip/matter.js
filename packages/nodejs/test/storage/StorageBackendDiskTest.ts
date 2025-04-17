564dfe2266425811e1d649fa3ad2a49db5e82ab8
Ingo Fischer <github@fischer-ka.de>
2025-04-17 12:36:18 +0200

    Remove Sync Storage  and other dependency cleanups (#1915)
    * Clear Sync storage including deps
    
    * changelog
    
    * remove curves deps where not needed
    
    * tell mergify to also update
    
    * Address Review Feedback
    
    * Address Review Feedback

--- packages/nodejs/test/storage/StorageBackendDiskTest.ts
+++ packages/nodejs/test/storage/StorageBackendDiskTest.ts
@@ -4,80 +4,71 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-import { StorageBackendDisk } from "#storage/StorageBackendDisk.js";
-import { LocalStorage } from "node-localstorage";
+import { StorageBackendDisk } from "#storage/index.js";
 import * as assert from "node:assert";
-import { readFile, stat } from "node:fs/promises";
+import { readFile, rm, stat } from "node:fs/promises";
 import { tmpdir } from "node:os";
 import { resolve } from "node:path";
 
-const TEST_STORAGE_LOCATION = resolve(tmpdir(), "matterjs-test-storage");
+const TEST_STORAGE_LOCATION = resolve(tmpdir(), "matterjs-test-storage-async");
 
 const CONTEXTx1 = ["context"];
 const CONTEXTx2 = [...CONTEXTx1, "subcontext"];
 const CONTEXTx3 = [...CONTEXTx2, "subsubcontext"];
 
-describe("Storage node-localstorage", () => {
-    beforeEach(() => {
-        const localStorage = new LocalStorage(TEST_STORAGE_LOCATION);
-        localStorage.clear();
+describe("StorageBackendDiskAsync", () => {
+    let storage: StorageBackendDisk;
+
+    beforeEach(async () => {
+        await rm(TEST_STORAGE_LOCATION, { recursive: true, force: true });
+
+        storage = new StorageBackendDisk(TEST_STORAGE_LOCATION);
+        await storage.initialize();
     });
 
     it("write and read success", async () => {
-        const storage = new StorageBackendDisk(TEST_STORAGE_LOCATION);
+        await storage.set(CONTEXTx1, "key", "value");
 
-        storage.set(CONTEXTx1, "key", "value");
-
-        const value = storage.get(CONTEXTx1, "key");
+        const value = await storage.get(CONTEXTx1, "key");
         assert.equal(value, "value");
     });
 
     it("multi-write and read success", async () => {
-        const storage = new StorageBackendDisk(TEST_STORAGE_LOCATION);
+        await storage.set(CONTEXTx1, { key: "value", key2: "value2" });
 
-        storage.set(CONTEXTx1, { key: "value", key2: "value2" });
-
-        const value = storage.get(CONTEXTx1, "key");
+        const value = await storage.get(CONTEXTx1, "key");
         assert.equal(value, "value");
-        const value2 = storage.get(CONTEXTx1, "key2");
+        const value2 = await storage.get(CONTEXTx1, "key2");
         assert.equal(value2, "value2");
     });
 
     it("multi-write and values read success", async () => {
-        const storage = new StorageBackendDisk(TEST_STORAGE_LOCATION);
+        await storage.set(CONTEXTx1, { key: "value", key2: "value2" });
 
-        storage.set(CONTEXTx1, { key: "value", key2: "value2" });
-
-        const values = storage.values(CONTEXTx1);
+        const values = await storage.values(CONTEXTx1);
         assert.deepEqual(values, { key: "value", key2: "value2" });
     });
 
     it("write and delete success", async () => {
-        const storage = new StorageBackendDisk(TEST_STORAGE_LOCATION);
+        await storage.set(CONTEXTx1, "key", "value");
+        await storage.delete(CONTEXTx1, "key");
 
-        storage.set(CONTEXTx1, "key", "value");
-        storage.delete(CONTEXTx1, "key");
-
-        const value = storage.get(CONTEXTx1, "key");
+        const value = await storage.get(CONTEXTx1, "key");
         assert.equal(value, undefined);
     });
 
     it("write and clear success", async () => {
-        const storage = new StorageBackendDisk(TEST_STORAGE_LOCATION);
+        await storage.set(CONTEXTx1, "key", "value");
+        await storage.clear();
 
-        storage.set(CONTEXTx1, "key", "value");
-        storage.clear();
-
-        const value = storage.get(CONTEXTx1, "key");
+        const value = await storage.get(CONTEXTx1, "key");
         assert.equal(value, undefined);
     });
 
     it("write and read success with multiple context levels", async () => {
-        const storage = new StorageBackendDisk(TEST_STORAGE_LOCATION);
+        await storage.set(CONTEXTx3, "key", "value");
 
-        storage.set(CONTEXTx3, "key", "value");
-
-        const value = storage.get(CONTEXTx3, "key");
+        const value = await storage.get(CONTEXTx3, "key");
         assert.equal(value, "value");
 
         const dirStat = await stat(TEST_STORAGE_LOCATION);
@@ -89,12 +80,10 @@ describe("Storage node-localstorage", () => {
     });
 
     it("write and read success with multiple context levels and special chars", async () => {
-        const storage = new StorageBackendDisk(TEST_STORAGE_LOCATION);
-
         const location = [...CONTEXTx3, "it'a/slash"];
-        storage.set(location, "key", "value");
+        await storage.set(location, "key", "value");
 
-        const value = storage.get(location, "key");
+        const value = await storage.get(location, "key");
         assert.equal(value, "value");
 
         const dirStat = await stat(TEST_STORAGE_LOCATION);
@@ -107,71 +96,69 @@ describe("Storage node-localstorage", () => {
         assert.equal(keyContent.toString(), `"value"`);
     });
 
-    it("return keys with storage values", () => {
-        const storage = new StorageBackendDisk(TEST_STORAGE_LOCATION);
+    it("return keys with storage values", async () => {
+        await storage.set(CONTEXTx3, "key", "value");
 
-        storage.set(CONTEXTx3, "key", "value");
-
-        const value = storage.keys(CONTEXTx3);
+        const value = await storage.keys(CONTEXTx3);
         expect(value).deep.equal(["key"]);
     });
 
-    it("return keys with storage without subcontexts values", () => {
-        const storage = new StorageBackendDisk(TEST_STORAGE_LOCATION);
+    it("return keys with storage values and special chars", async () => {
+        const location = [...CONTEXTx3, "it'a/slash"];
+        await storage.set(location, "key", "value");
 
-        storage.set(CONTEXTx2, "key", "value");
-        storage.set(CONTEXTx3, "key", "value");
-
-        const value = storage.keys(CONTEXTx2);
+        const value = await storage.keys(location);
         expect(value).deep.equal(["key"]);
     });
 
-    it("return contexts with subcontexts", () => {
-        const storage = new StorageBackendDisk(TEST_STORAGE_LOCATION);
+    it("return keys with storage without subcontexts values", async () => {
+        await storage.set(CONTEXTx2, "key", "value");
+        await storage.set(CONTEXTx3, "key", "value");
 
-        storage.set(CONTEXTx2, "key", "value");
-        storage.set(["context", "subcontext2"], "key", "value");
-        storage.set(CONTEXTx3, "key", "value");
-
-        expect(storage.contexts(CONTEXTx3)).deep.equal([]);
-        expect(storage.contexts(CONTEXTx2)).deep.equal(["subsubcontext"]);
-        expect(storage.contexts(CONTEXTx1)).deep.equal(["subcontext", "subcontext2"]);
-        expect(storage.contexts([])).deep.equal(CONTEXTx1);
+        const value = await storage.keys(CONTEXTx2);
+        expect(value).deep.equal(["key"]);
     });
 
-    it("return contexts with subcontexts with special chars", () => {
-        const storage = new StorageBackendDisk(TEST_STORAGE_LOCATION);
+    it("return contexts with subcontexts", async () => {
+        await storage.set(CONTEXTx2, "key", "value");
+        await storage.set(["context", "subcontext2"], "key", "value");
+        await storage.set(CONTEXTx3, "key", "value");
 
-        storage.set(CONTEXTx2, "key", "value");
-        storage.set(["context", "sub's/fun"], "key", "value");
-        storage.set(CONTEXTx3, "key", "value");
+        expect(await storage.contexts(CONTEXTx3)).deep.equal([]);
+        expect(await storage.contexts(CONTEXTx2)).deep.equal(["subsubcontext"]);
+        expect(await storage.contexts(CONTEXTx1)).deep.equal(["subcontext", "subcontext2"]);
+        expect(await storage.contexts([])).deep.equal(CONTEXTx1);
+    });
+
+    it("return contexts with subcontexts with special chars", async () => {
+        await storage.set(CONTEXTx2, "key", "value");
+        await storage.set(["context", "sub's/fun"], "key", "value");
+        await storage.set(CONTEXTx3, "key", "value");
 
         const storageRead = new StorageBackendDisk(TEST_STORAGE_LOCATION);
+        await storageRead.initialize();
 
-        expect(storageRead.contexts(CONTEXTx3)).deep.equal([]);
-        expect(storageRead.contexts(CONTEXTx2)).deep.equal(["subsubcontext"]);
-        expect(storageRead.contexts(CONTEXTx1)).deep.equal(["subcontext", "sub's/fun"]);
-        expect(storageRead.contexts([])).deep.equal(CONTEXTx1);
+        expect(await storageRead.contexts(CONTEXTx3)).deep.equal([]);
+        expect(await storageRead.contexts(CONTEXTx2)).deep.equal(["subsubcontext"]);
+        expect(await storageRead.contexts(CONTEXTx1)).deep.equal(["sub's/fun", "subcontext"]);
+        expect(await storageRead.contexts([])).deep.equal(CONTEXTx1);
     });
 
-    it("clear all keys with multiple contextes", () => {
-        const storage = new StorageBackendDisk(TEST_STORAGE_LOCATION);
+    it("clear all keys with multiple contextes", async () => {
+        await storage.set(CONTEXTx1, "key1", "value");
+        await storage.set(CONTEXTx2, "key2", "value");
+        await storage.set(CONTEXTx3, "key3", "value");
 
-        storage.set(CONTEXTx1, "key1", "value");
-        storage.set(CONTEXTx2, "key2", "value");
-        storage.set(CONTEXTx3, "key3", "value");
-
-        storage.clearAll(CONTEXTx2);
-        expect(storage.keys(CONTEXTx1)).deep.equal(["key1"]);
-        expect(storage.keys(CONTEXTx2)).deep.equal([]);
-        expect(storage.keys(CONTEXTx3)).deep.equal([]);
+        await storage.clearAll(CONTEXTx2);
+        expect(await storage.keys(CONTEXTx1)).deep.equal(["key1"]);
+        expect(await storage.keys(CONTEXTx2)).deep.equal([]);
+        expect(await storage.keys(CONTEXTx3)).deep.equal([]);
     });
 
-    it("Throws error when context is empty on set", () => {
-        assert.throws(
-            () => {
-                const storage = new StorageBackendDisk(TEST_STORAGE_LOCATION);
-                storage.set([""], "key", "value");
+    it("Rejects with error when context is empty on set", async () => {
+        await assert.rejects(
+            async () => {
+                await storage.set([""], "key", "value");
             },
             {
                 message: "Context must not be an empty and not contain dots.",
@@ -179,11 +166,10 @@ describe("Storage node-localstorage", () => {
         );
     });
 
-    it("Throws error when key is empty on set", () => {
-        assert.throws(
-            () => {
-                const storage = new StorageBackendDisk(TEST_STORAGE_LOCATION);
-                storage.set(CONTEXTx1, "", "value");
+    it("Rejects with error when key is empty on set", async () => {
+        await assert.rejects(
+            async () => {
+                await storage.set(CONTEXTx1, "", "value");
             },
             {
                 message: "Key must not be an empty string.",
@@ -191,11 +177,10 @@ describe("Storage node-localstorage", () => {
         );
     });
 
-    it("Throws error when context is empty on get", () => {
-        assert.throws(
-            () => {
-                const storage = new StorageBackendDisk(TEST_STORAGE_LOCATION);
-                storage.get([""], "key");
+    it("Rejects with error when context is empty on get", async () => {
+        await assert.rejects(
+            async () => {
+                await storage.get([""], "key");
             },
             {
                 message: "Context must not be an empty and not contain dots.",
@@ -203,11 +188,10 @@ describe("Storage node-localstorage", () => {
         );
     });
 
-    it("Throws error when context is empty on get with subcontext", () => {
-        assert.throws(
-            () => {
-                const storage = new StorageBackendDisk(TEST_STORAGE_LOCATION);
-                storage.get(["ok", ""], "key");
+    it("Rejects with error when context is empty on get with subcontext", async () => {
+        await assert.rejects(
+            async () => {
+                await storage.get(["ok", ""], "key");
             },
             {
                 message: "Context must not be an empty and not contain dots.",
@@ -215,11 +199,10 @@ describe("Storage node-localstorage", () => {
         );
     });
 
-    it("Throws error when key is empty on get", () => {
-        assert.throws(
-            () => {
-                const storage = new StorageBackendDisk(TEST_STORAGE_LOCATION);
-                storage.get(CONTEXTx1, "");
+    it("Rejects with error when key is empty on get", async () => {
+        await assert.rejects(
+            async () => {
+                await storage.get(CONTEXTx1, "");
             },
             {
                 message: "Key must not be an empty string.",
@@ -227,8 +210,7 @@ describe("Storage node-localstorage", () => {
         );
     });
 
-    after(() => {
-        const localStorage = new LocalStorage(TEST_STORAGE_LOCATION);
-        localStorage.clear();
+    after(async () => {
+        await rm(TEST_STORAGE_LOCATION, { recursive: true, force: true });
     });
 });
