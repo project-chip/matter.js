663b130a82b41e4b47ced0dcd67b287c9e94798c
Ingo Fischer <github@fischer-ka.de>
2025-04-18 19:26:11 +0200

    ok needs more excludes

--- chip-testing/test/core/ACE.test.ts
+++ chip-testing/test/core/ACE.test.ts
@@ -8,6 +8,7 @@ describe("ACE", () => {
     chip("ACE/*").exclude(
         // test excluded till we adjusted for https://github.com/project-chip/connectedhomeip/pull/38263
         "ACE/1.2",
+        "ACE/1.3",
         // Our group management server is too limited to pass this test
         "ACE/1.6",
     );
