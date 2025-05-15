4f068c72ace98719985a2af6c1b61d4407e0e30b
Ingo Fischer <github@fischer-ka.de>
2025-05-15 10:56:38 +0200

    Re-enable ACL checks because we are now in sync with SDK
    But we also patch ACE 2.2 for now becauseof https://github.com/project-chip/connectedhomeip/issues/33735

--- chip-testing/test/core/ACL.test.ts
+++ chip-testing/test/core/ACL.test.ts
@@ -6,13 +6,6 @@
 
 describe("ACL", () => {
     chip("ACL/*").exclude(
-        // test excluded till we adjusted for https://github.com/project-chip/connectedhomeip/pull/38263
-        "ACL/2.3",
-        "ACL/2.4",
-        "ACL/2.5",
-        "ACL/2.6",
-        "ACL/2.8",
-
         // Requires ARLs which we don't implement
         "ACL/2.11",
     );
