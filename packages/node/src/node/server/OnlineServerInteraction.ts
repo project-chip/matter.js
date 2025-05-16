ceba41ae9ef7a483d44ef6a80bf43a01dd5fd200
Ingo Fischer <github@fischer-ka.de>
2025-05-16 13:43:00 +0200

    Add Invoke for OnlineServerInteraction
    We need to use a slightly different style here because the OnlineContext.act interferes with the types

--- packages/node/src/node/server/OnlineServerInteraction.ts
+++ packages/node/src/node/server/OnlineServerInteraction.ts
@@ -1,7 +1,6 @@
 import { OnlineContext } from "#behavior/index.js";
 import { NotImplementedError } from "#general";
 import {
-    Interactable,
     Invoke,
     InvokeResult,
     NodeProtocol,
@@ -14,7 +13,7 @@ import {
     WriteResult,
 } from "#protocol";
 
-export class OnlineServerInteraction implements Interactable<OnlineContext.Options> {
+export class OnlineServerInteraction {
     readonly #interaction: ServerInteraction;
 
     constructor(node: NodeProtocol) {
@@ -40,7 +39,21 @@ export class OnlineServerInteraction implements Interactable<OnlineContext.Optio
         return OnlineContext(context).act(session => this.#interaction.write(request, session));
     }
 
-    invoke<T extends Invoke>(_request: T, _context: OnlineContext.Options): InvokeResult<T> {
-        throw new NotImplementedError("invoke not implemented");
+    invoke(
+        request: Invoke,
+        context: OnlineContext.Options,
+        responseData?: (chunk: InvokeResult.Chunk) => Promise<void>,
+    ) {
+        return OnlineContext({ ...context, command: true }).act(async session => {
+            if (request.suppressResponse) {
+                return this.#interaction.invoke(request, session) as unknown as Promise<void>;
+            } else {
+                for await (const chunk of this.#interaction.invoke(request, session)) {
+                    if (responseData !== undefined) {
+                        await responseData(chunk);
+                    }
+                }
+            }
+        });
     }
 }
