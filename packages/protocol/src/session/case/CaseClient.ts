2adb7a768f262e58f33034e26ede6d7b43a1fe00
Ingo Fischer <github@fischer-ka.de>
2025-05-27 09:07:03 +0200

    fixes

--- packages/protocol/src/session/case/CaseClient.ts
+++ packages/protocol/src/session/case/CaseClient.ts
@@ -68,7 +68,7 @@ export class CaseClient {
             const initiatorResumeMic = Crypto.encrypt(resumeKey, new Uint8Array(0), RESUME1_MIC_NONCE);
             sigma1Bytes = await messenger.sendSigma1({
                 initiatorSessionId,
-                destinationId: await fabric.currentDestinationIdFor(peerNodeId, initiatorRandom),
+                destinationId: await fabric.getDestinationId(peerNodeId, initiatorRandom),
                 initiatorEcdhPublicKey,
                 initiatorRandom,
                 resumptionId,
@@ -78,7 +78,7 @@ export class CaseClient {
         } else {
             sigma1Bytes = await messenger.sendSigma1({
                 initiatorSessionId,
-                destinationId: await fabric.currentDestinationIdFor(peerNodeId, initiatorRandom),
+                destinationId: await fabric.getDestinationId(peerNodeId, initiatorRandom),
                 initiatorEcdhPublicKey,
                 initiatorRandom,
                 initiatorSessionParams: this.#sessions.sessionParameters,
