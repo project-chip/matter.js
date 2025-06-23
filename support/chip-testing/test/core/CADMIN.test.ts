5248dde34714b1b1a1dc1abc1b11b81ea9efe2ee
Ingo Fischer <github@fischer-ka.de>
2025-06-23 13:53:33 +0200

    Adjust tests because of adjustments in chip (#2184)
    * Adjust tests because of adjustments in chip
    
    * try reactivate all CADMIN etsts for now

--- support/chip-testing/test/core/CADMIN.test.ts
+++ support/chip-testing/test/core/CADMIN.test.ts
@@ -4,7 +4,9 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
+import { OperationalCredentialsBehavior } from "@matter/main/behaviors";
 import { edit } from "@matter/testing";
+import { NodeTestInstance } from "../../src/NodeTestInstance.js";
 
 describe("CADMIN", () => {
     // CADMIN 1.5 sleeps for 190 seconds to wait for commissioning timeout.  We've forced timeout to 1 second so we can
@@ -37,29 +39,28 @@ describe("CADMIN", () => {
     // encoding for ConstraintError (which is just 0x87, so presumably 0x500 is a bit prefix)
     before(() => chip.testFor("CADMIN/1.19").edit(edit.sed("s/0x0000000B/0x00000587/")));
 
-    // For CADMIN/1.24 we reduce window timeout (see equivalent in SC/4.1)
-    before(() => chip.testFor("CADMIN/1.24").edit(edit.sed("s/180/1/")));
+    // For CADMIN/1.22 we reduce window timeout (see equivalent in SC/4.1)
+    before(() => chip.testFor("CADMIN/1.22").edit(edit.sed("s/180/1/")));
 
     // Since our timeout is artificially low (1 s.) we need to reduce the timeout in the "discovery window too short"
     // test (see equivalent in Discovery.test.ts)
-    before(() => chip.testFor("CADMIN/1.24").edit(edit.sed("s/timeout=179/timeout=0/")));
+    before(() => chip.testFor("CADMIN/1.22").edit(edit.sed("s/timeout=179/timeout=0/")));
 
-    chip("CADMIN/1.5");
-    // chip("CADMIN/*").exclude(
-    //     // Handled below
-    //     "CADMIN/1.19",
+    chip("CADMIN/*").exclude(
+        // Handled below
+        "CADMIN/1.19",
 
-    //     // TODO - we fail 1.24 because we open a second commissioning window.  Unsure why we shouldn't, requires more
-    //     // research
-    //     "CADMIN/1.24",
-    // );
+        // TODO - we fail 1.22 because we open a second commissioning window.  Unsure why we shouldn't, requires more
+        //  research
+        // "CADMIN/1.22",
+    );
 
-    // chip("CADMIN/1.19").beforeTest(subject => {
-    //     const node = NodeTestInstance.nodeOf(subject);
+    chip("CADMIN/1.19").beforeTest(subject => {
+        const node = NodeTestInstance.nodeOf(subject);
 
-    //     // CHIP has a hard-coded limit via CHIP_CONFIG_MAX_FABRICS macro which defaults to 16.  TC_ADMIN_1_19 fails
-    //     // when we exhaust this space with our default fabric limit of 254.  Including a bug which lets the test
-    //     // fail if limit is not 16, so set to 16 to be ok for now.
-    //     return node.setStateOf(OperationalCredentialsBehavior, { supportedFabrics: 16 });
-    // });
+        // CHIP has a hard-coded limit via CHIP_CONFIG_MAX_FABRICS macro which defaults to 16.  TC_ADMIN_1_19 fails
+        // when we exhaust this space with our default fabric limit of 254.  Including a bug which lets the test
+        // fail if limit is not 16, so set to 16 to be ok for now.
+        return node.setStateOf(OperationalCredentialsBehavior, { supportedFabrics: 16 });
+    });
 });
