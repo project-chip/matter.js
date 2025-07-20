413e6f98ec2aa68ed4666ef8cfbff576184334eb
Ingo Fischer <github@fischer-ka.de>
2025-07-20 19:27:40 +0200

    Add enum values for Namespace override and use it
    Fills the enum values for the namespace datatype and use it in mode select

--- packages/model/src/standard/resources/namespace.resource.ts
+++ packages/model/src/standard/resources/namespace.resource.ts
@@ -10,5 +10,50 @@ import { Resource } from "#models/Resource.js";
 
 Resource.add({
     tag: "datatype", name: "namespace", description: "Namespace", xref: "core§7.19.2.43",
-    details: "The Namespace type identifies the namespace used for a semantic tag."
+    details: "The Namespace type identifies the namespace used for a semantic tag.",
+
+    children: [
+        {
+            tag: "field", name: "Closure",
+            details: "Tags which are useful in describing things related to closing and opening"
+        },
+        {
+            tag: "field", name: "CompassDirection",
+            details: "Tags which are useful in describing things related to compass direction"
+        },
+        {
+            tag: "field", name: "CompassLocation",
+            details: "Tags which are useful in describing things related to compass location"
+        },
+        { tag: "field", name: "Direction", details: "Tags which are useful in describing things related to direction" },
+        { tag: "field", name: "Level", details: "Tags which are useful in describing things related to level" },
+        { tag: "field", name: "Location", details: "Tags which are useful in describing things related to location" },
+        { tag: "field", name: "Number", details: "Tags which are useful in describing things related to numbering" },
+        { tag: "field", name: "Position", details: "Tags which are useful in describing things related to position" },
+        {
+            tag: "field", name: "ElectricalMeasurement",
+            details: "Tags which are useful in describing electrical loads"
+        },
+        { tag: "field", name: "Laundry", details: "Tags which are useful with laundry device types" },
+        { tag: "field", name: "PowerSource", details: "Tags which are useful with power source device types" },
+        {
+            tag: "field", name: "AreaNamespace",
+            details: "Tags which are useful in describing things related to home areas"
+        },
+        {
+            tag: "field", name: "LandmarkNamespace",
+            details: "Tags which are useful in describing things related to home landmarks"
+        },
+        {
+            tag: "field", name: "RelativePosition",
+            details: "Tags which are useful in describing things related to position relative to a reference external to " +
+                "the device"
+        },
+        { tag: "field", name: "Refrigerator", details: "Tags which are useful with refrigeration device types" },
+        {
+            tag: "field", name: "RoomAirConditioner",
+            details: "Tags which are useful with Room Air Conditioner device types"
+        },
+        { tag: "field", name: "Switches", details: "Tags which are useful with switch device types" }
+    ]
 });
---
+++ packages/types/src/globals/Namespace.ts
@@ -0,0 +1,101 @@
+/**
+ * @license
+ * Copyright 2022-2025 Matter.js Authors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+/*** THIS FILE IS GENERATED, DO NOT EDIT ***/
+
+/**
+ * Namespace
+ *
+ * The Namespace type identifies the namespace used for a semantic tag.
+ *
+ * @see {@link MatterSpecification.v141.Core} § 7.19.2.43
+ */
+export enum Namespace {
+    /**
+     * Tags which are useful in describing things related to closing and opening
+     */
+    Closure = 1,
+
+    /**
+     * Tags which are useful in describing things related to compass direction
+     */
+    CompassDirection = 2,
+
+    /**
+     * Tags which are useful in describing things related to compass location
+     */
+    CompassLocation = 3,
+
+    /**
+     * Tags which are useful in describing things related to direction
+     */
+    Direction = 4,
+
+    /**
+     * Tags which are useful in describing things related to level
+     */
+    Level = 5,
+
+    /**
+     * Tags which are useful in describing things related to location
+     */
+    Location = 6,
+
+    /**
+     * Tags which are useful in describing things related to numbering
+     */
+    Number = 7,
+
+    /**
+     * Tags which are useful in describing things related to position
+     */
+    Position = 8,
+
+    /**
+     * Tags which are useful in describing electrical loads
+     */
+    ElectricalMeasurement = 10,
+
+    /**
+     * Tags which are useful with laundry device types
+     */
+    Laundry = 14,
+
+    /**
+     * Tags which are useful with power source device types
+     */
+    PowerSource = 15,
+
+    /**
+     * Tags which are useful in describing things related to home areas
+     */
+    AreaNamespace = 16,
+
+    /**
+     * Tags which are useful in describing things related to home landmarks
+     */
+    LandmarkNamespace = 17,
+
+    /**
+     * Tags which are useful in describing things related to position relative to a reference external to the device
+     */
+    RelativePosition = 18,
+
+    /**
+     * Tags which are useful with refrigeration device types
+     */
+    Refrigerator = 65,
+
+    /**
+     * Tags which are useful with Room Air Conditioner device types
+     */
+    RoomAirConditioner = 66,
+
+    /**
+     * Tags which are useful with switch device types
+     */
+    Switches = 67
+}
