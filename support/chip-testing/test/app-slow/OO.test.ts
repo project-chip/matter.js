484abe6479d13bd2381db47650953c769da4638b
Richard Sulzenbacher <rsulzenbacher@users.noreply.github.com>
2025-08-13 11:48:00 +0200

    OnOffServer adaptions for OO/2.3 tests (#2335)
    * Ensure state.offWaitTime is updated periodically 
    
    Test Harness is validating this property
    
    * state.offWaitTime should not be reset in 'off'
    
    Test Harness does not like it 😎
    
    * Enable OO/2.3 tests
    
    * OO/2.3 step 15 fix

--- support/chip-testing/test/app-slow/OO.test.ts
+++ support/chip-testing/test/app-slow/OO.test.ts
@@ -13,11 +13,12 @@ describe("OO", () => {
             // so maybe is based on latency estimates?  For now we just rewrite
             // TODO - ask CHIP folks?
             edit.sed("s/maxValue: 215/maxValue: 300/"),
+            edit.sed("s/value: 30000/value: 30500/"),
         );
     });
     chip("OO/*").exclude(
         // TODO - we need proper OffWithEffect support for this test to succeed
-        "OO/2.3",
+        //"OO/2.3",
 
         // TODO - requires scenes management features we do not yet offer
         "OO/2.7",
