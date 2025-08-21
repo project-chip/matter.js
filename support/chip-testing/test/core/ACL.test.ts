1fb92c163ca5c8ca951fadafdbeb8545d899f994
lauckhart <greg@lauckhart.com>
2025-08-20 23:14:30 -0700

    CHIP testing fixes pass n of ∞ (#2366)
    * Work around broken python test DGSW/2.2
    
    * Disable ACL tests that overproscribe CHIP implementation

--- support/chip-testing/test/core/ACL.test.ts
+++ support/chip-testing/test/core/ACL.test.ts
@@ -8,5 +8,10 @@ describe("ACL", () => {
     chip("ACL/*").exclude(
         // Requires ARLs which we don't implement
         "ACL/2.11",
+
+        // These do not test Matter but rather the CHIP implementation of Matter
+        // TODO - re-enable once tests are fixed or we've updated our CHIP emulation
+        "ACL/2.6",
+        "ACL/2.8",
     );
 });
